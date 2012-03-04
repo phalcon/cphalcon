82b32f7d8b201fb6edcbb2e8e82e43e295f658e4
andres.gutierrez <phalcon@loudertechnology.com>
2012-03-03 19:00:39 -0500

    Pre-init autoglobals on unittests

--- unit-tests/InitTest.php
+++ unit-tests/InitTest.php
@@ -21,7 +21,15 @@
 class InitTest extends PHPUnit_Framework_TestCase {
 
 	public function testExtensionLoaded(){
+
+    //Check if phalcon ext was loaded
 		$this->assertTrue(in_array('phalcon', get_loaded_extensions()));
+
+    //Force to init auto-globals
+    count($_SERVER);
+    count($_POST);
+    count($_GET);
+    count($_REQUEST);
 	}
 
 }
