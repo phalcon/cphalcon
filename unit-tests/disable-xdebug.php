142d28dfc0f485ff70034219e9fa43d3c3ff9bc8
andres.gutierrez <phalcon@loudertechnology.com>
2012-03-03 20:11:38 -0500

    Disabling xdebug for Travis-CI

---
+++ unit-tests/disable-xdebug.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+  +------------------------------------------------------------------------+
+  | Phalcon Framework                                                      |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2011-2012 Phalcon Team (http://www.phalconphp.com)       |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file docs/LICENSE.txt.                        |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconphp.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
+  | Authors: Andres Gutierrez <andres@phalconphp.com>                      |
+  |          Eduar Carvajal <eduar@phalconphp.com>                         |
+  +------------------------------------------------------------------------+
+*/
+
+$file = '/home/vagrant/.phpenv/versions/'.PHP_VERSION.'/etc/php.ini';
+if(file_exists($file)){
+	$content = '';
+	foreach(file($file) as $line){
+		if(!preg_match('/zend_extension=/', $line)){
+			$content.=$line;
+		}
+	}
+	file_put_contents($file, $content);
+}
