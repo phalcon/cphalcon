7696f9500d41be86121bb7f9e31463cf256902a4
phalcon <andres@phalconphp.com>
2012-04-12 18:19:32 -0500

    Adding more tags unit-tests

--- unit-tests/TagTest.php
+++ unit-tests/TagTest.php
@@ -168,4 +168,21 @@ class TagTest extends PHPUnit_Framework_TestCase {
 		$params = array('controller/index', 'method' => 'get');
 		$this->assertEquals(Phalcon_Tag::form($params), '<form action="/controller/index/" method= "get" >');
 	}
+
+	public function testStaticLinksRel(){
+
+		//Images
+		$this->assertEquals(Phalcon_Tag::image("img/hello.gif"), '<img  src="/img/hello.gif">'.PHP_EOL);
+		$this->assertEquals(Phalcon_Tag::image(array("img/hello.gif", "alt" => "hello image")), '<img  alt="hello image" src="/img/hello.gif">'.PHP_EOL);
+
+		//CSS stylesheetlinks
+		$this->assertEquals(Phalcon_Tag::stylesheetLink("http://fonts.googleapis.com/css?family=Rosario", false), '<link rel="stylesheet"  href="http://fonts.googleapis.com/css?family=Rosario" type="text/css">'.PHP_EOL);
+		$this->assertEquals(Phalcon_Tag::stylesheetLink("css/style.css"), '<link rel="stylesheet"  href="/css/style.css" type="text/css">'.PHP_EOL);
+
+		//Javascript includes
+		$this->assertEquals(Phalcon_Tag::javascriptInclude('javascript/jquery.min.js'), '<script  src="/javascript/jquery.min.js"  type="text/javascript" ></script>'.PHP_EOL);
+		$this->assertEquals(Phalcon_Tag::javascriptInclude('http://localhost/javascript/jquery.min.js', false), '<script  src="http://localhost/javascript/jquery.min.js""  type="text/javascript" ></script>'.PHP_EOL);
+
+
+	}
 }
