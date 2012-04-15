957e63caf8c45b9f8690dbfedf2b8af3696d2cff
phalcon <andres@phalconphp.com>
2012-04-15 00:22:22 -0500

    Fixing paginator test

--- unit-tests/PaginatorTest.php
+++ unit-tests/PaginatorTest.php
@@ -73,19 +73,19 @@ class PaginatorTest extends PHPUnit_Framework_TestCase {
  		$this->assertEquals($page->current, 50);
  		$this->assertEquals($page->total_pages, 219);
 
- 		//After Last Page
- 		$paginator->setCurrentPage(250);
+ 		//Last Page
+ 		$paginator->setCurrentPage(219);
 
  		$page = $paginator->getPaginate();
  		$this->assertEquals(get_class($page), 'stdClass');
 
- 		$this->assertEquals(count($page->items), 10);
+ 		$this->assertEquals(count($page->items), 1);
 
- 		$this->assertEquals($page->before, 1);
- 		$this->assertEquals((int) $page->next, 219);
+ 		$this->assertEquals($page->before, 218);
+ 		$this->assertEquals((int) $page->next, 218);
  		$this->assertEquals($page->last, 219);
 
- 		$this->assertEquals($page->current, 1);
+ 		$this->assertEquals($page->current, 219);
  		$this->assertEquals($page->total_pages, 219);
 
 	}
