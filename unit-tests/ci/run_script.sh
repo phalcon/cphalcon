58d3d229b157a7eb1457bd7005c66476fc265b85
Phalcon <andres@phalconphp.com>
2012-06-13 19:20:26 -0500

    Generating core dump to screen

--- unit-tests/ci/run_script.sh
+++ unit-tests/ci/run_script.sh
@@ -1,6 +1,12 @@
+ulimit -c unlimited
+echo "/tmp/core" > /proc/sys/kernel/core_pattern
 phpunit --debug -c unit-tests/phpunit.xml
 STATUS=$?
 if [ $STATUS != 0 ]; then
-	cat core
+        if [ -f /tmp/core ]; then
+		gdb -q -batch -x unit-tests/ci/gdb-commands -e php -c /tmp/core 
+	else 
+		echo "No core dump was generated"
+	fi
 fi
 exit $STATUS
