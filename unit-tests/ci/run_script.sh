86b7417a1a56701e0b74f91af806cb0374a42242
phalcon <andres@phalconphp.com>
2012-07-02 13:00:31 -0500

    Running models-manager test

--- unit-tests/ci/run_script.sh
+++ unit-tests/ci/run_script.sh
@@ -1,12 +1,13 @@
 ulimit -c unlimited
 rm -f /tmp/core
 echo "/tmp/core" > /proc/sys/kernel/core_pattern
+php ./unit-tests/manual-unit.php ./unit-tests/ModelsManagerTest.php
 php ./unit-tests/ci/phpunit.php --debug -c unit-tests/phpunit.xml
 STATUS=$?
 if [ $STATUS != 0 ]; then
         if [ -f /tmp/core ]; then
-		gdb -q -batch -x ./unit-tests/ci/gdb-commands -e php -c /tmp/core 
-	else 
+		gdb -q -batch -x ./unit-tests/ci/gdb-commands -e php -c /tmp/core
+	else
 		echo "No core dump was generated"
 	fi
 fi
