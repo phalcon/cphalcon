69fc15ce8f7bdaf3e683e40e98795f747e40d446
phalcon <andres@phalconphp.com>
2012-08-14 12:32:52 -0500

    moving files to mvc/http

--- unit-tests/RouterTest.php
+++ unit-tests/RouterTest.php
@@ -18,109 +18,136 @@
   +------------------------------------------------------------------------+
 */
 
-class RouterRewriteTest extends PHPUnit_Framework_TestCase {
+class RouterRegexTest extends PHPUnit_Framework_TestCase {
 
 	private function _runTest($router, $test){
-		
-		if($test['type']=='handle'){
-			if(isset($test['url'])){
-				$_GET['_url'] = $test['url'];
-			} else {
-				unset($_GET['_url']);
-			}
-			$router->handle();
-		}
-
-		if(isset($test['controller'])){		
-			$this->assertEquals($router->getControllerName(), $test['controller']);		
-		}
-
-		if(isset($test['action'])){		
-			$this->assertEquals($router->getActionName(), $test['action']);		
-		}
+		$router->handle($test['uri']);
+		$this->assertEquals($router->getControllerName(), $test['controller']);
+		$this->assertEquals($router->getActionName(), $test['action']);
+		$this->assertEquals($router->getParams(), $test['params']);
 	}
 
 	public function testRouter(){
 
 		$tests = array(
 			array(
-				'name' => 'No uri route info',
-				'type' => 'handle',
-				'controller' => null,
-				'action' => null,
-				'params' => array()
+				'uri' => '/documentation/index/hellao/aaadpqñda/bbbAdld/cc-ccc',
+				'controller' => 'documentation',
+				'action' => 'index',
+				'params' => array('hellao', 'aaadpqñda', 'bbbAdld', 'cc-ccc')
 			),
 			array(
-				'name' => 'Route info /controller',
-				'type' => 'handle',
-				'url' => 'controller',
-				'controller' => 'controller',
-				'action' => null,
-				'params' => array()
-			),
-			array(
-				'name' => 'Route info /controller/index',
-				'type' => 'handle',
-				'url' => 'controller/index',
-				'controller' => 'controller',
+				'uri' => '/documentation/index/',
+				'controller' => 'documentation',
 				'action' => 'index',
 				'params' => array()
 			),
 			array(
-				'name' => 'Route info /controller/index/p1',
-				'type' => 'handle',
-				'url' => 'controller/index/p1',
-				'controller' => 'controller',
+				'uri' => '/documentation/index',
+				'controller' => 'documentation',
 				'action' => 'index',
-				'params' => array('p1')
-			),
-			array(
-				'name' => 'Route info /controller/index/p1/param2',
-				'type' => 'handle',
-				'url' => 'controller/index/p1/param2',
-				'controller' => 'controller',
-				'action' => 'index',
-				'params' => array('p1', 'param2')
-			),
-			array(
-				'name' => 'Route info /controller/lol',
-				'type' => 'handle',
-				'url' => 'controller/lol',
-				'controller' => 'controller',
-				'action' => 'lol',
 				'params' => array()
 			),
 			array(
-				'name' => 'Route info /controller//p1',
-				'type' => 'handle',
-				'url' => 'controller//p1',
-				'controller' => 'controller',
+				'uri' => '/documentation/',
+				'controller' => 'documentation',
 				'action' => null,
-				'params' => array('p1')
-			),
-			array(
-				'name' => 'Route info ///p1',
-				'type' => 'handle',
-				'url' => '///p1',
-				'controller' => null,
-				'action' => null,
-				'params' => array('p1')
-			),
-			array(
-				'name' => 'Route info /controller0/index1',
-				'type' => 'handle',
-				'url' => 'controller0/index1',
-				'controller' => 'controller0',
-				'action' => 'index1',
 				'params' => array()
 			),
+			array(
+				'uri' => '/documentation',
+				'controller' => 'documentation',
+				'action' => null,
+				'params' => array()
+			),
+			array(
+				'uri' => '/system/admin/a/edit/hellao/aaadp',
+				'controller' => 'admin',
+				'action' => 'edit',
+				'params' => array('hellao', 'aaadp')
+			),
+			array(
+				'uri' => '/es/news',
+				'controller' => 'news',
+				'action' => 'index',
+				'params' => array('language' => 'es')
+			),
+			array(
+				'uri' => '/admin/posts/edit/100',
+				'controller' => 'posts',
+				'action' => 'edit',
+				'params' => array('id' => 100)
+			),
+			array(
+				'uri' => '/posts/2010/02/10/title/content',
+				'controller' => 'posts',
+				'action' => 'show',
+				'params' => array('year' => '2010', 'month' => '02', 'day' => '10', 0 => 'title', 1 => 'content')
+			),
+			array(
+				'uri' => '/manual/en/translate.adapter.html',
+				'controller' => 'manual',
+				'action' => 'show',
+				'params' => array('language' => 'en', 'file' => 'translate.adapter')
+			),
+			array(
+				'uri' => '/posts/1999/s/le-nice-title',
+				'controller' => 'posts',
+				'action' => 'show',
+				'params' => array('year' => '1999', 'title' => 'le-nice-title')
+			),
+			array(
+				'uri' => '/feed/fr/blog/diaporema.json',
+				'controller' => 'feed',
+				'action' => 'get',
+				'params' => array('lang' => 'fr', 'blog' => 'diaporema', 'type' => 'json')
+			)
 		);
 
-		foreach($tests as $test){
-			$router = new Phalcon\Router\Rewrite();
-			$this->_runTest($router, $test);		
-		}	
+		$router = new Phalcon\Router\Regex();
+
+		$router->add('/system/:controller/a/:action/:params', array(
+			'controller' => 1,
+			'action' => 2,
+			'params' => 3,
+		));
+
+		$router->add('/([a-z]{2})/:controller', array(
+			'controller' => 2,
+			'action' => 'index',
+			'language' => 1
+		));
+
+		$router->add('/admin/:controller/:action/:int', array(
+			'controller' => 1,
+			'action' => 2,
+			'id' => 3
+		));
+
+		$router->add('/posts/([0-9]{4})/([0-9]{2})/([0-9]{2})/:params', array(
+			'controller' => 'posts',
+			'action' => 'show',
+			'year' => 1,
+			'month' => 2,
+			'day' => 3,
+			'params' => 4,
+		));
+
+		$router->add('/manual/([a-z]{2})/([a-z\.]+)\.html', array(
+			'controller' => 'manual',
+			'action' => 'show',
+			'language' => 1,
+			'file' => 2
+		));
+
+		$router->add("/feed/{lang:[a-z]+}/blog/{blog:[a-z\-]+}\.{type:[a-z\-]+}", "Feed::get");
+
+		$router->add("/posts/{year:[0-9]+}/s/{title:[a-z\-]+}", "Posts::show");
+
+		foreach($tests as $n => $test){
+			$this->_runTest($router, $test);
+		}
 
 	}
-	
+
 }
