ebd67a0d5f2dd7e7c33294686888f07da60d8219
Nikolaos Dimopoulos <nikos@niden.net>
2012-10-02 15:44:24 -0400

    Fixed headers for Win complilation 	modified:   ext/php_phalcon.h

--- ext/php_phalcon.h
+++ ext/php_phalcon.h
@@ -20,55 +20,49 @@
 #ifndef PHP_PHALCON_H
 #define PHP_PHALCON_H 1
 
-#define PHP_PHALCON_VERSION "0.5.0"
+#define PHALCON_RELEASE 1
+
+#define PHP_PHALCON_VERSION "0.5.1"
 #define PHP_PHALCON_EXTNAME "phalcon"
 
 #define PHALCON_MAX_MEMORY_STACK 48
 
 typedef struct _phalcon_memory_entry {
-	int pointer;
-	zval **addresses[PHALCON_MAX_MEMORY_STACK];
-	struct _phalcon_memory_entry *prev;
-	struct _phalcon_memory_entry *next;
+  int pointer;
+  zval **addresses[PHALCON_MAX_MEMORY_STACK];
+  struct _phalcon_memory_entry *prev;
+  struct _phalcon_memory_entry *next;
 } phalcon_memory_entry;
 
 ZEND_BEGIN_MODULE_GLOBALS(phalcon)
-	phalcon_memory_entry *start_memory;
-	phalcon_memory_entry *active_memory;
+  phalcon_memory_entry *start_memory;
+  phalcon_memory_entry *active_memory;
 #ifndef PHALCON_RELEASE
-	int phalcon_stack_stats;
+  int phalcon_stack_stats;
 #endif
 ZEND_END_MODULE_GLOBALS(phalcon)
-
-#ifdef ZTS
-#include "TSRM.h"
-#endif
 
 ZEND_EXTERN_MODULE_GLOBALS(phalcon)
 
 #ifdef ZTS
-	#define PHALCON_GLOBAL(v) TSRMG(phalcon_globals_id, zend_phalcon_globals *, v)
+  #define PHALCON_GLOBAL(v) TSRMG(phalcon_globals_id, zend_phalcon_globals *, v)
 #else
-	#define PHALCON_GLOBAL(v) (phalcon_globals.v)
+  #define PHALCON_GLOBAL(v) (phalcon_globals.v)
 #endif
 
 extern zend_module_entry phalcon_module_entry;
 #define phpext_phalcon_ptr &phalcon_module_entry
 
-#ifdef PHP_WIN32
-#include "stdafx.h"
-#endif
-
 #endif
 
 #if PHP_VERSION_ID >= 50400
-	#define PHALCON_INIT_FUNCS(class_functions) static const zend_function_entry class_functions[] =
+  #define PHALCON_INIT_FUNCS(class_functions) static const zend_function_entry class_functions[] =
 #else
-	#define PHALCON_INIT_FUNCS(class_functions) static const function_entry class_functions[] =
+  #define PHALCON_INIT_FUNCS(class_functions) static const function_entry class_functions[] =
 #endif
 
 #ifndef PHP_FE_END
-	#define PHP_FE_END { NULL, NULL, NULL, 0, 0 }
+  #define PHP_FE_END { NULL, NULL, NULL, 0, 0 }
 #endif
 
 /** Define FASTCALL */
