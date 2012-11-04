8fbc60ef481cd0b23237491ea7db4afe940ae49c
Nikolaos Dimopoulos <nikos@niden.net>
2012-11-04 14:40:38 -0500

    Corrected Flash tests - refactored and added more due to this 	new file:   unit-tests/tests/Phalcon/Flash/Direct/CustomCSSTest.php 	new file:   unit-tests/tests/Phalcon/Flash/Direct/DefaultCSSTest.php 	new file:   unit-tests/tests/Phalcon/Flash/Direct/EmptyCSSTest.php 	new file:   unit-tests/tests/Phalcon/Flash/Direct/Helper/Unit.php 	deleted:    unit-tests/tests/Phalcon/Flash/Direct/UnitTest.php

---
+++ unit-tests/tests/Phalcon/Flash/Direct/CustomCSSTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * CustomCSSTest.php
+ * Flash_Direct_CustomCSSTest
+ *
+ * Tests the \Phalcon\Flash\Direct component with default CSS
+ *
+ * PhalconPHP Framework
+ *
+ * @copyright (c) 2011-2012 Phalcon Team
+ * @link      http://www.phalconphp.com
+ * @author    Andres Gutierrez <andres@phalconphp.com>
+ * @author    Eduar Carvajal <eduar@phalconphp.com>
+ * @author    Nikolaos Dimopoulos <nikos@niden.net>
+ *
+ * The contents of this file are subject to the New BSD License that is
+ * bundled with this package in the file docs/LICENSE.txt
+ *
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world-wide-web, please send an email to license@phalconphp.com
+ * so that we can send you a copy immediately.
+ */
+
+class Flash_Direct_CustomCSSTest extends Flash_Direct_Helper_Unit
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $classes = array(
+            'error'   => 'alert alert-error',
+            'success' => 'alert alert-success',
+            'notice'  => 'alert alert-notice',
+            'warning' => 'alert alert-warning'
+        );
+
+        $this->setClasses($classes);
+    }
+}
