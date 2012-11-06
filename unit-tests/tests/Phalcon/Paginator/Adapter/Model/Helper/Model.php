11632e779ac2a84d4beb3c711e5245af7297d95f
Nikolaos Dimopoulos <nikos@niden.net>
2012-11-05 21:43:26 -0500

    Refactored Paginator Model tests 	deleted:    unit-tests/tests/Phalcon/Paginator.php 	new file:   unit-tests/tests/Phalcon/Paginator/Adapter/Model/Helper/Model.php 	new file:   unit-tests/tests/Phalcon/Paginator/Adapter/Model/MysqlTest.php 	new file:   unit-tests/tests/Phalcon/Paginator/Adapter/Model/PostgresqlTest.php 	new file:   unit-tests/tests/Phalcon/Paginator/Adapter/Model/SqliteTest.php

---
+++ unit-tests/tests/Phalcon/Paginator/Adapter/Model/Helper/Model.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * Model.php
+ * Paginator_Adapter_Model_Helper_Model
+ *
+ * Tests the \Phalcon\Paginator\Adapter\Model component
+ *
+ * PhalconPHP Framework
+ *
+ * @copyright (c) 2011-2012 Phalcon Team
+ * @link      http://www.phalconphp.com
+ * @author    Andres Gutierrez <andres@phalconphp.com>
+ * @author    Eduar Carvajal <eduar@phalconphp.com>
+ * @author    Nikolaos Dimopoulos <nikos@niden.net>
+ *
+ * The contents of this file are subject to the New BSD License that is
+ * bundled with this package in the file docs/LICENSE.txt
+ *
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world-wide-web, please send an email to license@phalconphp.com
+ * so that we can send you a copy immediately.
+ */
+
+use \Phalcon\Paginator\Adapter\Model as PhPaginatorModel;
+
+class Paginator_Adapter_Model_Helper_Model extends Phalcon_Test_ModelTestCase
+{
+    /**
+     * Tests the returned class
+     *
+     * @author Nikos Dimopoulos <nikos@niden.net>
+     * @since  2012-11-05
+     */
+    public function testReturnedClass()
+    {
+        $persons   = Personnes::find();
+        $params    = array(
+                        'data'  => $persons,
+                        'limit' => 10,
+                        'page'  => 1,
+                     );
+        $paginator = new PhPaginatorModel($params);
+
+        // Get a page
+        $page = $paginator->getPaginate();
+
+        $expected = 'stdClass';
+        $actual   = get_class($page);
+
+        $this->assertEquals($expected, $actual, 'Object returned not stdClass');
+    }
+
+    /**
+     * Tests the count of the paginator items
+     *
+     * @author Nikos Dimopoulos <nikos@niden.net>
+     * @since  2012-11-05
+     */
+    public function testPageCount()
+    {
+        $persons   = Personnes::find();
+        $params    = array(
+                        'data'  => $persons,
+                        'limit' => 10,
+                        'page'  => 1,
+                     );
+        $paginator = new PhPaginatorModel($params);
+
+        // Get a page
+        $page = $paginator->getPaginate();
+
+        $expected = 10;
+        $actual   = count($page->items);
+
+        $this->assertEquals($expected, $actual, 'Item count not correct');
+    }
+
+    /**
+     * Tests the before, next, last, current, total_pages when on first page
+     *
+     * @author Nikos Dimopoulos <nikos@niden.net>
+     * @since  2012-11-05
+     */
+    public function testPageOneBeforeNextLastCurrentPages()
+    {
+        $persons   = Personnes::find();
+        $params    = array(
+                        'data'  => $persons,
+                        'limit' => 10,
+                        'page'  => 1,
+                     );
+        $paginator = new PhPaginatorModel($params);
+
+        // Get a page
+        $page = $paginator->getPaginate();
+
+        $expected = 1;
+        $actual   = $page->before;
+
+        $this->assertEquals($expected, $actual, 'Before not correct');
+
+        $expected = 2;
+        $actual   = $page->next;
+
+        $this->assertEquals($expected, $actual, 'Next not correct');
+
+        $expected = 218;
+        $actual   = $page->last;
+
+        $this->assertEquals($expected, $actual, 'Last not correct');
+
+        $expected = 1;
+        $actual   = $page->current;
+
+        $this->assertEquals($expected, $actual, 'Current not correct');
+
+        $expected = 218;
+        $actual   = $page->total_pages;
+
+        $this->assertEquals($expected, $actual, 'Total Pages not correct');
+    }
+
+    /**
+     * Tests the before, next, last, current, total_pages when on the middle
+     * of the pagination collection
+     *
+     * @author Nikos Dimopoulos <nikos@niden.net>
+     * @since  2012-11-05
+     */
+    public function testPageMiddleBeforeNextLastCurrentPages()
+    {
+        $persons   = Personnes::find();
+        $params    = array(
+                        'data'  => $persons,
+                        'limit' => 10,
+                        'page'  => 1,
+                     );
+        $paginator = new PhPaginatorModel($params);
+
+        $paginator->setCurrentPage(50);
+
+        $page = $paginator->getPaginate();
+
+        $expected = 49;
+        $actual   = $page->before;
+
+        $this->assertEquals($expected, $actual, 'Before not correct');
+
+        $expected = 51;
+        $actual   = $page->next;
+
+        $this->assertEquals($expected, $actual, 'Next not correct');
+
+        $expected = 218;
+        $actual   = $page->last;
+
+        $this->assertEquals($expected, $actual, 'Last not correct');
+
+        $expected = 50;
+        $actual   = $page->current;
+
+        $this->assertEquals($expected, $actual, 'Current not correct');
+
+        $expected = 218;
+        $actual   = $page->total_pages;
+
+        $this->assertEquals($expected, $actual, 'Total Pages not correct');
+
+        //Last Page
+        /*$paginator->setCurrentPage(219);
+
+        $page = $paginator->getPaginate();
+        $this->assertEquals(get_class($page), 'stdClass');
+
+        $this->assertEquals(count($page->items), 1);
+
+        $this->assertEquals($page->before, 218);
+        $this->assertEquals((int) $page->next, 219);
+        $this->assertEquals($page->last, 219);
+
+        $this->assertEquals($page->current, 219);
+        $this->assertEquals($page->total_pages, 219);*/
+   }
+}
---
+++ unit-tests/tests/Phalcon/Paginator/Adapter/Model/SqliteTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * SqliteTest.php
+ * Paginator_Adapter_Model_SqliteTest
+ *
+ * Tests the \Phalcon\Paginator\Adapter\Model component
+ *
+ * PhalconPHP Framework
+ *
+ * @copyright (c) 2011-2012 Phalcon Team
+ * @link      http://www.phalconphp.com
+ * @author    Andres Gutierrez <andres@phalconphp.com>
+ * @author    Eduar Carvajal <eduar@phalconphp.com>
+ * @author    Nikolaos Dimopoulos <nikos@niden.net>
+ *
+ * The contents of this file are subject to the New BSD License that is
+ * bundled with this package in the file docs/LICENSE.txt
+ *
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world-wide-web, please send an email to license@phalconphp.com
+ * so that we can send you a copy immediately.
+ */
+
+class Paginator_Adapter_Model_SqliteTest extends Paginator_Adapter_Model_Helper_Model
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->setDb('sqlite');
+    }
+}
