a1ace3aac72fa245f4b50d589923255382218ac6
Nikolaos Dimopoulos <nikos@niden.net>
2012-11-12 11:28:42 -0500

    Corrected the customer fixture to provide a bit more randomization on results 	modified:   unit-tests/library/Phalcon/Test/Fixtures/Customers.php

--- unit-tests/library/Phalcon/Test/Fixtures/Customers.php
+++ unit-tests/library/Phalcon/Test/Fixtures/Customers.php
@@ -25,29 +25,39 @@ class Phalcon_Test_Fixtures_Customers
 {
     public static function get($records = null)
     {
-        $template = "(%s, '1', '1', '%s', '%s', '%s', '%s', '%s', 'A', "
-                  . "'%s', '0.00', '%s', '0')";
+        $template = "(%s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', "
+                  . "'%s', '%s', '%s', '0')";
 
-        $total = (is_null($records)) ? 100 : intval($records);
+        $total   = (is_null($records)) ? 100 : intval($records);
+        $doc_id  = 1;
+        $cust_id = 1;
 
         for ($i = 1; $i <= $total; $i++)
         {
+            $doc_id  = ($doc_id > 4)  ? 1 : ($doc_id + 1);
+            $cust_id = ($cust_id > 3) ? 1 : ($cust_id + 1);
             $first_name = uniqid('fn');
             $last_name  = uniqid('ln');
             $phone      = uniqid('555');
             $notes      = uniqid('notes', true);
             $birthday   = date('Y-m-d');
+            $status     = ($i % 2) ? 'A' : 'I';
             $now        = date('Y-m-d H:i:s');
+            $credit     = ($i % 2) ? 10.5 : 5.5;
 
             $data[] = sprintf(
                 $template,
                 $i,
+                $cust_id,
+                $doc_id,
                 $first_name,
                 $last_name,
                 $phone,
                 "user{$i}@email.com",
                 $notes,
+                $status,
                 $birthday,
+                $credit,
                 $now
             );
         }
