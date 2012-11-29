4a6e4ad7a3b460dfb9cd457370f1368a2ef8ac11
Nikolaos Dimopoulos <nikos@niden.net>
2012-11-29 14:27:12 -0500

    Added PHP-Tests folder (new test suite) with the relevant ci scripts 	new file:   php-tests/README.md 	new file:   php-tests/ci/gdb-commands 	new file:   php-tests/ci/install_apc 	new file:   php-tests/ci/install_memcached 	new file:   php-tests/ci/install_mongo 	new file:   php-tests/ci/phpunit.php 	new file:   php-tests/ci/run_script.sh 	new file:   php-tests/tests/phpunit.xml

---
+++ php-tests/ci/run_script.sh
@@ -0,0 +1,16 @@
+
+ulimit -c unlimited
+PHP_VERSION=`php -r "echo PHP_VERSION;"`
+PHP_BIN="/home/travis/.phpenv/versions/$PHP_VERSION/bin/php"
+echo $PHP_BIN
+$PHP_BIN ./php-tests/ci/phpunit.php --debug -c php-tests/phpunit.xml
+STATUS=$?
+if [ $STATUS != 0 ]; then
+	if [ -f core ]; then
+		sudo apt-get install gdb
+		gdb -q -batch -x ./php-tests/ci/gdb-commands -e $PHP_BIN -c core
+	else
+		echo "No core dump was generated"
+	fi
+fi
+exit $STATUS
