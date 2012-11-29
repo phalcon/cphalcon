20c34595da63ff70e511243fbc6063d165c2884b
Nikolaos Dimopoulos <nikos@niden.net>
2012-11-29 15:23:47 -0500

    Trying different configuration to run both suites 	modified:   .travis.yml 	modified:   unit-tests/ci/run_script.sh

--- unit-tests/ci/run_script.sh
+++ unit-tests/ci/run_script.sh
@@ -13,4 +13,16 @@ if [ $STATUS != 0 ]; then
 		echo "No core dump was generated"
 	fi
 fi
+
+$PHP_BIN ./php-tests/ci/phpunit.php --debug -c php-tests/tests/phpunit.xml
+STATUS=$?
+if [ $STATUS != 0 ]; then
+	if [ -f core ]; then
+		sudo apt-get install gdb
+		gdb -q -batch -x ./unit-tests/ci/gdb-commands -e $PHP_BIN -c core
+	else
+		echo "No core dump was generated"
+	fi
+fi
+
 exit $STATUS
