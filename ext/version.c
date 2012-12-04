6a38c593bfa79cd9b1a7701ccb6905508fe3c7d6
Nikolaos Dimopoulos <nikos@niden.net>
2012-12-04 14:10:56 -0500

    Setting up version to 0.8.0.ALPHA.1 	modified:   ext/version.c

--- ext/version.c
+++ ext/version.c
@@ -74,10 +74,10 @@ PHP_METHOD(Phalcon_Version, _getVersion){
 	PHALCON_INIT_VAR(version);
 	array_init(version);
 	add_next_index_long(version, 0);
-	add_next_index_long(version, 7);
+	add_next_index_long(version, 8);
 	add_next_index_long(version, 0);
-	add_next_index_long(version, 4);
-	add_next_index_long(version, 0);
+	add_next_index_long(version, 1);
+	add_next_index_long(version, 1);
 	
 	RETURN_CTOR(version);
 }
