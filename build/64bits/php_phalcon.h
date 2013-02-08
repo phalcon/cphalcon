fbbca2048f67278492b73622f7c2c5c2e60cfd92
phalcon <andres@phalconphp.com>
2013-02-07 21:44:10 -0500

    Updating version, fixing metadata adapter test

--- build/64bits/php_phalcon.h
+++ build/64bits/php_phalcon.h
@@ -20,7 +20,7 @@
 #ifndef PHP_PHALCON_H
 #define PHP_PHALCON_H 1
 
-#define PHP_PHALCON_VERSION "0.9.0"
+#define PHP_PHALCON_VERSION "0.9.1"
 #define PHP_PHALCON_EXTNAME "phalcon"
 
 #define PHALCON_RELEASE 1
