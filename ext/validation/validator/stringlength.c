ea7187f3cf6335054d6706ca63605a24397b1446
boston <bost56@gmail.com>
2013-04-30 12:28:24 +0600

    Update stringlength.c

--- ext/validation/validator/stringlength.c
+++ ext/validation/validator/stringlength.c
@@ -1,4 +1,3 @@
-
 /*
   +------------------------------------------------------------------------+
   | Phalcon Framework                                                      |
@@ -46,7 +45,7 @@
  *<code>
  *use Phalcon\Validation\Validator\StringLength as StringLength;
  *
- *$validation->validate('name_last', new StringLength(array(
+ *$validation->add('name_last', new StringLength(array(
  *      'max' => 50,
  *      'min' => 2,
  *      'messageMaximum' => 'We don\'t like really long names',
