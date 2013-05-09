0a55ef5ad53a8cadcdf60a77287615b8498d7c23
xboston <bost56@gmail.com>
2013-05-09 18:16:57 +0600

    fix Phalcon_Events_ManagerInterface

--- ext/events/managerinterface.h
+++ ext/events/managerinterface.h
@@ -24,6 +24,7 @@ PHALCON_INIT_CLASS(Phalcon_Events_ManagerInterface);
 ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_events_managerinterface_attach, 0, 0, 2)
 	ZEND_ARG_INFO(0, eventType)
 	ZEND_ARG_INFO(0, handler)
+	ZEND_ARG_INFO(0, priority)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_events_managerinterface_dettachall, 0, 0, 0)
