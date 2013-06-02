4c15bd7068cd17749b0d841a29598f0fc106e275
phalcon <andres@phalconphp.com>
2013-06-01 19:31:44 -0500

    Manually finding the core dump

--- unit-tests/ci/run_script.sh
+++ unit-tests/ci/run_script.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
+#  Phalcon Framework
+#
+#  Copyright (c) 2011-2013 Phalcon Team (http://www.phalconphp.com)
+#
+#  This source file is subject to the New BSD License that is bundled
+#  with this package in the file docs/LICENSE.txt.
+#
+#  If you did not receive a copy of the license and are unable to
+#  obtain it through the world-wide-web, please send an email
+#  to license@phalconphp.com so we can send you a copy immediately.
+#
+#  Authors: Andres Gutierrez <andres@phalconphp.com>
+#            Eduar Carvajal <eduar@phalconphp.com>
+
+find_core_dump(){
+	find /home/travis -name core
+}
 
 ulimit -c unlimited
 PHP_VERSION=`php -r "echo PHP_VERSION;"`
@@ -11,7 +30,8 @@ if [ $STATUS != 0 ]; then
 		sudo apt-get install gdb
 		gdb -q -batch -x ./unit-tests/ci/gdb-commands -e $PHP_BIN -c /tmp/core
 	else
-		echo "No core dump was generated"
+		find_core_dump
+		echo "No core dump was found"
 	fi
 
 	exit $STATUS
@@ -24,7 +44,8 @@ if [ $STATUS != 0 ]; then
 		sudo apt-get install gdb
 		gdb -q -batch -x ./unit-tests/ci/gdb-commands -e $PHP_BIN -c /tmp/core
 	else
-		echo "No core dump was generated"
+		find_core_dump
+		echo "No core dump was found"
 	fi
 fi
 
