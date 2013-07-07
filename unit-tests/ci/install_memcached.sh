54615110177c7937bd45e4b970d16dd9ad0752ac
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-07-07 04:36:53 +0300

    Hopefully final bug fixes

--- unit-tests/ci/install_memcached.sh
+++ unit-tests/ci/install_memcached.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
-if [ "`php -r 'echo substr(PHP_VERSION, 0, 3);'`" = "5.3" ]; then
-	wget http://pecl.php.net/get/memcache-2.2.7.tgz
+if [ "$(php -r 'echo substr(PHP_VERSION, 0, 3);')" = "5.3" ]; then
+	wget -q http://pecl.php.net/get/memcache-2.2.7.tgz
 	tar -xzf memcache-2.2.7.tgz
-	sh -c "cd memcache-2.2.7 && phpize && ./configure --enable-memcache && make && sudo make install"
+	( cd memcache-2.2.7 && phpize && ./configure --enable-memcache && make && sudo make install )
 else
-	wget http://pecl.php.net/get/memcache-3.0.7.tgz
+	wget -q http://pecl.php.net/get/memcache-3.0.7.tgz
 	tar -xzf memcache-3.0.7.tgz
-	sh -c "cd memcache-3.0.7 && phpize && ./configure --enable-memcache && make && sudo make install"
+	( cd memcache-3.0.7 && phpize && ./configure --enable-memcache && make && sudo make install )
 fi
 
 echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
--- unit-tests/ci/install_mongo.sh
+++ unit-tests/ci/install_mongo.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-wget http://pecl.php.net/get/mongo-1.3.4.tgz
+wget -q http://pecl.php.net/get/mongo-1.3.4.tgz
 tar -xzf mongo-1.3.4.tgz
-sh -c "cd mongo-1.3.4 && phpize && ./configure --enable-memcache && make && sudo make install"
+( cd mongo-1.3.4 && phpize && ./configure --enable-mongo && make && sudo make install )
 
 echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
