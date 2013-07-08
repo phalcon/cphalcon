4138c3196c5b2cb1da05556556285c410f995f5f
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-07-08 05:22:49 +0300

    APCu is beta

--- unit-tests/ci/install_apc.sh
+++ unit-tests/ci/install_apc.sh
@@ -3,7 +3,7 @@
 DIR=$(readlink -enq $(dirname $0))
 
 if [ "$(php -r 'echo substr(PHP_VERSION, 0, 3);')" = "5.5" ]; then
-	pecl install apcu < /dev/null || ( pecl config-set preferred_state beta; pecl install apcu < /dev/null )
+	pecl install apcu || ( pecl config-set preferred_state beta; pecl install apcu )
 	phpenv config-add "$DIR/apcu.ini"
 else
 	phpenv config-add "$DIR/apc.ini"
