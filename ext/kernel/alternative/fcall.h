14be03d28cdb508ea6d47d3b57e2b350f20aa46d
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-07-09 18:46:27 +0300

    'phalcon_alt_call_user_method_ex' declared 'static' but never defined

--- ext/kernel/alternative/fcall.h
+++ ext/kernel/alternative/fcall.h
@@ -27,5 +27,5 @@
 #endif
 
 extern int phalcon_alt_call_user_method(zend_class_entry *ce, zval **object_pp, char *method_name, unsigned int method_len, zval *retval_ptr, zend_uint param_count, zval *params[], unsigned long method_key TSRMLS_DC);
-int phalcon_alt_call_user_method_ex(zend_class_entry *ce, zval **object_pp, char *method_name, unsigned int method_len, zval **retval_ptr_ptr, zend_uint param_count, zval **params[], unsigned long method_key TSRMLS_DC);
+/*int phalcon_alt_call_user_method_ex(zend_class_entry *ce, zval **object_pp, char *method_name, unsigned int method_len, zval **retval_ptr_ptr, zend_uint param_count, zval **params[], unsigned long method_key TSRMLS_DC);*/
 int phalcon_alt_call_method(zend_fcall_info *fci, zend_class_entry *ce, unsigned long hash_key, char *method_name, unsigned int method_len, unsigned long method_key TSRMLS_DC);
