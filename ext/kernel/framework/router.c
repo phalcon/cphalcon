60da6fae16fcecd85246144aeea9153877897e52
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-08-01 07:03:00 +0300

    variable may be used uninitialized in this function

--- ext/kernel/framework/router.c
+++ ext/kernel/framework/router.c
@@ -270,8 +270,8 @@ void phalcon_extract_named_params(zval *return_value, zval *str, zval *matches){
 
 	unsigned int i, j, k, bracket_count = 0, parentheses_count = 0, ch;
 	unsigned int intermediate = 0, length, number_matches = 0, found_pattern;
-	int variable_length, regexp_length = 0, not_valid;
-	char *cursor, *cursor_var, *marker;
+	int variable_length, regexp_length = 0, not_valid = 0;
+	char *cursor, *cursor_var, *marker = NULL;
 	char *item, *variable = NULL, *regexp;
 	smart_str route_str = {0};
 
