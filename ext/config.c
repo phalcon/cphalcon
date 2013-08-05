b1b58cbc948180b8a83d2a70435236efa3d0d7a0
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-08-05 15:42:38 +0300

    Fix #1000 (cherry picked from commit 2722cb6745d05057e6338056d300d4b89bb47c26)

--- ext/config.c
+++ ext/config.c
@@ -322,7 +322,11 @@ static zend_object_value phalcon_config_object_ctor(zend_class_entry* ce TSRMLS_
 	phalcon_config_object* obj = ecalloc(1, sizeof(phalcon_config_object));
 	zend_object_value retval;
 
-	zend_object_std_init(&(obj->obj), ce TSRMLS_CC);
+	zend_object_std_init(&obj->obj, ce TSRMLS_CC);
+#if PHP_VERSION_ID >= 50400
+	object_properties_init(&obj->obj, ce);
+#endif
+
 	ALLOC_HASHTABLE(obj->props);
 	zend_hash_init(obj->props, 0, NULL, ZVAL_PTR_DTOR, 0);
 
