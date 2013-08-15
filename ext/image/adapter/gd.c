cda33d58262d5d05ab9b2e51130827136daa4379
dreamsxin <dreamsxin@126.com>
2013-08-15 11:19:03 +0800

    Updated gd.c

--- ext/image/adapter/gd.c
+++ ext/image/adapter/gd.c
@@ -1273,50 +1273,10 @@ PHP_METHOD(Phalcon_Image_Adapter_GD, _blur){
 		RETURN_MM();
 	}
 
-	if (phalcon_get_intval(radius) <= 1) {
+	r = phalcon_get_intval(radius);
+
+	for (i = 0; i < r; i++) {
 		phalcon_call_func_p2_noret("imagefilter", image, constant);
-	} else {
-		r = phalcon_get_intval(radius);
-
-		PHALCON_INIT_VAR(matrix);
-		array_init_size(matrix, 3);
-
-		PHALCON_INIT_NVAR(item);
-		array_init_size(item, 3);
-
-		phalcon_array_append_long(&item, 1, PH_SEPARATE);
-		phalcon_array_append_long(&item, 2, PH_SEPARATE);
-		phalcon_array_append_long(&item, 1, PH_SEPARATE);
-
-		phalcon_array_append(&matrix, item, PH_SEPARATE);
-
-		PHALCON_INIT_NVAR(item);
-		array_init_size(item, 3);
-
-		phalcon_array_append_long(&item, 2, PH_SEPARATE);
-		phalcon_array_append_long(&item, 4, PH_SEPARATE);
-		phalcon_array_append_long(&item, 2, PH_SEPARATE);
-
-		phalcon_array_append(&matrix, item, PH_SEPARATE);
-
-		PHALCON_INIT_NVAR(item);
-		array_init_size(item, 3);
-
-		phalcon_array_append_long(&item, 1, PH_SEPARATE);
-		phalcon_array_append_long(&item, 2, PH_SEPARATE);
-		phalcon_array_append_long(&item, 1, PH_SEPARATE);
-
-		phalcon_array_append(&matrix, item, PH_SEPARATE);
-
-		PHALCON_INIT_VAR(div);
-		ZVAL_LONG(div, 16);
-
-		PHALCON_INIT_VAR(offset);
-		ZVAL_LONG(offset, 0);
-
-		for (i = 0; i < r; i++) {
-			phalcon_call_func_p4_noret("imageconvolution", image, matrix, div, offset);
-		}
 	}
 
 	PHALCON_MM_RESTORE();
