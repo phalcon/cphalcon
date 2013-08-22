6aa5f4126c009b87c68f5e1a8f091a36182c4701
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-08-22 11:14:01 +0300

    More interned strings

--- ext/interned-strings.h
+++ ext/interned-strings.h
@@ -22,6 +22,7 @@
 
 #include "main/php.h"
 
+extern const char *phalcon_interned_alias;
 extern const char *phalcon_interned_all;
 extern const char *phalcon_interned_arguments;
 extern const char *phalcon_interned_balias;
@@ -29,12 +30,14 @@ extern const char *phalcon_interned_binary_op;
 extern const char *phalcon_interned_column;
 extern const char *phalcon_interned_columns;
 extern const char *phalcon_interned_conditions;
+extern const char *phalcon_interned_delete;
 extern const char *phalcon_interned_distinct;
 extern const char *phalcon_interned_domain;
 extern const char *phalcon_interned_expr;
 extern const char *phalcon_interned_fields;
 extern const char *phalcon_interned_functionCall;
 extern const char *phalcon_interned_group;
+extern const char *phalcon_interned_groupBy;
 extern const char *phalcon_interned_having;
 extern const char *phalcon_interned_joins;
 extern const char *phalcon_interned_left;
@@ -45,17 +48,23 @@ extern const char *phalcon_interned_modelsCache;
 extern const char *phalcon_interned_modelsManager;
 extern const char *phalcon_interned_modelsMetadata;
 extern const char *phalcon_interned_name;
+extern const char *phalcon_interned_ns_alias;
 extern const char *phalcon_interned_number;
 extern const char *phalcon_interned_offset;
 extern const char *phalcon_interned_op;
 extern const char *phalcon_interned_order;
+extern const char *phalcon_interned_orderBy;
 extern const char *phalcon_interned_qualified;
+extern const char *phalcon_interned_qualifiedName;
 extern const char *phalcon_interned_right;
+extern const char *phalcon_interned_select;
+extern const char *phalcon_interned_sort;
 extern const char *phalcon_interned_source;
 extern const char *phalcon_interned_sqlAlias;
 extern const char *phalcon_interned_table;
 extern const char *phalcon_interned_tables;
 extern const char *phalcon_interned_type;
+extern const char *phalcon_interned_update;
 extern const char *phalcon_interned_value;
 extern const char *phalcon_interned_values;
 extern const char *phalcon_interned_where;
