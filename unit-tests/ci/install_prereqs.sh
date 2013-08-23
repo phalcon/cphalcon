8c266af459af9da4658fdb92202af2472df705b2
Ingo Walz <ingo.walz@googlemail.com>
2013-08-24 01:17:07 +0200

    Disable apc via cli setting

--- unit-tests/ci/install_prereqs.sh
+++ unit-tests/ci/install_prereqs.sh
@@ -5,12 +5,9 @@ DIR=$(readlink -enq $(dirname $0))
 
 if [ "$(php -r 'echo substr(PHP_VERSION, 0, 3);')" = "5.5" ]; then
 	( pecl install apcu < /dev/null || ( pecl config-set preferred_state beta; pecl install apcu < /dev/null ) && phpenv config-add "$DIR/apcu.ini" ) &
-	APCEXT="$DIR/apcu.ini"
 else
 	phpenv config-add "$DIR/apc.ini"
-	APCEXT="$DIR/apc.ini"
 fi
-export APCEXT
 
 pecl install igbinary < /dev/null && phpenv config-add $(DIR)/igbinary.ini &
 export DIR
