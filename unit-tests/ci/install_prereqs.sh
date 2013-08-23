76cec9d7757d6fb3cdf17f787182f1b38b8dd096
Ingo Walz <ingo.walz@googlemail.com>
2013-08-24 00:08:29 +0200

    Fixed travis failures

--- unit-tests/ci/install_prereqs.sh
+++ unit-tests/ci/install_prereqs.sh
@@ -2,16 +2,18 @@
 
 DIR=$(readlink -enq $(dirname $0))
 
+
+if [ "$(php -r 'echo substr(PHP_VERSION, 0, 3);')" = "5.5" ]; then
+	( pecl install apcu < /dev/null || ( pecl config-set preferred_state beta; pecl install apcu < /dev/null ) && phpenv config-add "$DIR/apcu.ini" ) &
+	APCEXT=apcu.ini
+else
+	phpenv config-add "$DIR/apc.ini"
+	APCEXT=apc.ini
+fi
+export $APCEXT
+
 pecl install igbinary < /dev/null && phpenv config-add $(DIR)/igbinary.ini &
-php -v
-
-PREVDIR=$(pwd)
-cd ~; svn co svn://svn.lighttpd.net/xcache/trunk xcache-trunk; cd xcache-trunk
-phpize && ./configure --enable-xcache && make && make install
-phpenv config-add xcache.ini
-cd $PREVDIR
-
-php -v
+export $DIR
 
 phpenv config-add $(DIR)/memcache.ini
 phpenv config-add $(DIR)/mongo.ini
