4c97ebafdc055915845422673353b0e4f84efe4d
Ingo Walz <ingo.walz@googlemail.com>
2013-08-24 00:15:25 +0200

    Changed variable export

--- unit-tests/ci/install_prereqs.sh
+++ unit-tests/ci/install_prereqs.sh
@@ -5,15 +5,15 @@ DIR=$(readlink -enq $(dirname $0))
 
 if [ "$(php -r 'echo substr(PHP_VERSION, 0, 3);')" = "5.5" ]; then
 	( pecl install apcu < /dev/null || ( pecl config-set preferred_state beta; pecl install apcu < /dev/null ) && phpenv config-add "$DIR/apcu.ini" ) &
-	APCEXT=apcu.ini
+	APCEXT="apcu.ini"
 else
 	phpenv config-add "$DIR/apc.ini"
-	APCEXT=apc.ini
+	APCEXT="apc.ini"
 fi
-export $APCEXT
+export APCEXT
 
 pecl install igbinary < /dev/null && phpenv config-add $(DIR)/igbinary.ini &
-export $DIR
+export DIR
 
 phpenv config-add $(DIR)/memcache.ini
 phpenv config-add $(DIR)/mongo.ini
