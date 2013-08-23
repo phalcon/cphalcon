1585c8f05588d3f0b2bffa7a863f91a3372018d6
Ingo Walz <ingo.walz@googlemail.com>
2013-08-24 01:05:38 +0200

    Flush memcache after first test

--- unit-tests/ci/install_prereqs.sh
+++ unit-tests/ci/install_prereqs.sh
@@ -5,10 +5,10 @@ DIR=$(readlink -enq $(dirname $0))
 
 if [ "$(php -r 'echo substr(PHP_VERSION, 0, 3);')" = "5.5" ]; then
 	( pecl install apcu < /dev/null || ( pecl config-set preferred_state beta; pecl install apcu < /dev/null ) && phpenv config-add "$DIR/apcu.ini" ) &
-	APCEXT="apcu.ini"
+	APCEXT="$DIR/apcu.ini"
 else
 	phpenv config-add "$DIR/apc.ini"
-	APCEXT="apc.ini"
+	APCEXT="$DIR/apc.ini"
 fi
 export APCEXT
 
