98e40c273052eeba7352fded6467d98587bb41b8
Ingo Walz <ingo.walz@googlemail.com>
2013-08-23 22:21:58 +0200

    Temporary remove apc

--- unit-tests/ci/install_prereqs.sh
+++ unit-tests/ci/install_prereqs.sh
@@ -2,12 +2,6 @@
 
 DIR=$(readlink -enq $(dirname $0))
 
-if [ "$(php -r 'echo substr(PHP_VERSION, 0, 3);')" = "5.5" ]; then
-	( pecl install apcu < /dev/null || ( pecl config-set preferred_state beta; pecl install apcu < /dev/null ) && phpenv config-add "$DIR/apcu.ini" ) &
-else
-	phpenv config-add "$DIR/apc.ini"
-fi
-
 pecl install igbinary < /dev/null && phpenv config-add $(DIR)/igbinary.ini &
 php -v
 
