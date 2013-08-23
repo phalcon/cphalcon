0ce63d33a815f7f75fa0e8f449e1d9b09586f99e
Ingo Walz <ingo.walz@googlemail.com>
2013-08-23 22:10:27 +0200

    Add php -v output after xcache installation

--- unit-tests/ci/install_prereqs.sh
+++ unit-tests/ci/install_prereqs.sh
@@ -17,6 +17,8 @@ phpize && ./configure --enable-xcache && make && make install
 phpenv config-add xcache.ini
 cd $PREVDIR
 
+php -v
+
 phpenv config-add $(DIR)/memcache.ini
 phpenv config-add $(DIR)/mongo.ini
 wait
