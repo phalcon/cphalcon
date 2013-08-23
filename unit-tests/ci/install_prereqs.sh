7e4ba36a8c4fbacdcf8924b2808be9edbb1ddc02
Ingo Walz <ingo.walz@googlemail.com>
2013-08-23 21:59:33 +0200

    Add xcache trunk

--- unit-tests/ci/install_prereqs.sh
+++ unit-tests/ci/install_prereqs.sh
@@ -10,6 +10,12 @@ fi
 
 pecl install igbinary < /dev/null && phpenv config-add $(DIR)/igbinary.ini &
 
+PREVDIR=$(pwd)
+cd ~; svn co svn://svn.lighttpd.net/xcache/trunk xcache-trunk; cd xcache-trunk
+phpize && ./configure --enable-xcache && make && make install
+phpenv config-add xcache.ini
+cd $PREVDIR
+
 phpenv config-add $(DIR)/memcache.ini
 phpenv config-add $(DIR)/mongo.ini
 wait
