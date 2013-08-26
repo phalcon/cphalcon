b2aea4f6b992be544b182accc38acddf89a8f9a9
dreamsxin <dreamsxin@126.com>
2013-08-26 11:55:26 +0800

    add ArrTest

--- unit-tests/ArrTest.php
+++ unit-tests/ArrTest.php
@@ -25,23 +25,67 @@ class ArrTest extends PHPUnit_Framework_TestCase
 
 	public function test()
 	{
-		$color1 = array('red', 'blur', 'green');
-		$color2 = array('while', 'black');
-		$array = array('theme' => array('one' => array('color' => $color1), 'two' => array()));
+		$array = array('theme' => array('one' => array('color' => array('red', 'blur', 'green'), 'fontsize' => '12'), 'two' => array('color' => array(1, 2, 3))), 'path' => 'path1');
 		$noarray = 'array';
+		$keys = array('theme', '*', 'color');
 
-		$ret = \Phalcon\Arr::is_assoc($color1);
+		$ret = \Phalcon\Arr::is_assoc(array('red', 'blur', 'green'));
+		$this->assertTrue(!$ret);
 
 		$ret = \Phalcon\Arr::is_assoc($array);
+		$this->assertTrue($ret);
 
 		$ret = \Phalcon\Arr::is_array($array);
+		$this->assertTrue($ret);
+
+		$ret = \Phalcon\Arr::is_array(new ArrayObject);
+		$this->assertTrue($ret);
 
 		$ret = \Phalcon\Arr::is_array($noarray);
+		$this->assertTrue(!$ret);
+		
+		$ret = \Phalcon\Arr::path($array, $keys);
+		$this->assertEquals($ret, array(array('red', 'blur', 'green'), array(1, 2, 3))); 
 
-		$ret = \Phalcon\Arr::path($array, 'theme.*.color');
+		\Phalcon\Arr::set_path($array, $keys, array('while', 'black'));
+		$this->assertEquals($array, array('theme' => array('one' => array('color' => array('while', 'black'), 'fontsize' => '12'), 'two' => array('color' => array('while', 'black'))), 'path' => 'path1'));
 
-		\Phalcon\Arr::set_path($array, 'theme.*.color', $color2);
+		$ret = \Phalcon\Arr::range(5, 20);
+		$this->assertEquals($ret, array(5 => 5, 10 => 10, 15 => 15, 20 => 20));
 
-		$ret = \Phalcon\Arr::range();
+		$ret = \Phalcon\Arr::get($array, 'theme');
+		$this->assertEquals($ret, array('one' => array('color' => array('while', 'black'), 'fontsize' => '12'), 'two' => array('color' => array('while', 'black'))));
+
+		$ret = \Phalcon\Arr::extract($array, array('theme.one.color', 'path'));
+		$this->assertEquals($ret, array('theme' => array('one' => array('color' => array('while', 'black'))), 'path' => 'path1'));
+
+		$array = array(array('color' => 'green'), array('color' => 'red'));
+		$ret = \Phalcon\Arr::pluck($array, 'color');
+		$this->assertEquals($ret, array('green', 'red'));
+
+		$array = array(5 => "orange", 3 => "banana");
+		$ret = \Phalcon\Arr::unshift($array, 1, array('red', 'blur', 'green'));
+		$this->assertEquals($ret, array(1 => array('red', 'blur', 'green'), 5 => "orange", 3 => "banana"));
+
+		$array = array(' 1 <script></script>', ' 2 xxx ');
+		$ret = \Phalcon\Arr::map(array('strip_tags', 'trim'), $array);
+		$this->assertEquals($ret, array('1', '2 xxx'));
+
+		$john = array('name' => 'john', 'children' => array('fred', 'paul', 'sally', 'jane'));
+		$mary = array('name' => 'mary', 'children' => array('jane', 'myleft'));
+		$ret = \Phalcon\Arr::merge($john, $mary);
+		$this->assertEquals($ret, array('name' => 'mary', 'children' => array('fred', 'paul', 'sally', 'jane', 'myleft')));
+
+		$a1 = array('name' => 'john', 'mood' => 'happy', 'food' => 'bacon');
+		$a2 = array('name' => 'jack', 'food' => 'tacos', 'drink' => 'beer');
+		$ret = \Phalcon\Arr::overwrite($a1, $a2);
+		$this->assertEquals($ret, array('name' => 'jack', 'mood' => 'happy', 'food' => 'tacos'));
+
+		$ret = \Phalcon\Arr::callback('Foo::bar(apple,orange)');
+		$this->assertEquals($ret, array(array('Foo', 'bar'), array('apple', 'orange')));
+
+		$array = array('set' => array('one' => 'something'), 'two' => 'other');
+		$ret = \Phalcon\Arr::flatten($array);
+		$this->assertEquals($ret, array('one' => 'something', 'two' => 'other'));
 	}
 }
