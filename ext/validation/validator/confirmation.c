079449c45c9e77854ec288771154e75f1bdc2663
Viktoras Bezaras <vb@meets-ecommerce.de>
2013-08-30 10:49:01 +0200

    Added code to all Validation\Validators (this must be probably extracted into some parent method)

--- ext/validation/validator/confirmation.c
+++ ext/validation/validator/confirmation.c
@@ -75,7 +75,7 @@ PHP_METHOD(Phalcon_Validation_Validator_Confirmation, validate){
 
 	zval *validator, *attribute, *with, *with_attribute;
 	zval *value, *with_value, *option, *message_str = NULL;
-	zval *type, *message;
+	zval *type, *message, *is_set_code, *code;
 
 	PHALCON_MM_GROW();
 
@@ -106,10 +106,23 @@ PHP_METHOD(Phalcon_Validation_Validator_Confirmation, validate){
 	
 		PHALCON_INIT_VAR(type);
 		ZVAL_STRING(type, "Confirmation", 1);
+
+		// Is code set
+		PHALCON_INIT_NVAR(option);
+		ZVAL_STRING(option, "code", 1);
+
+		PHALCON_INIT_VAR(is_set_code);
+		phalcon_call_method_p1(is_set_code, this_ptr, "issetoption", option);
+		PHALCON_INIT_VAR(code);
+		if (zend_is_true(is_set_code)) {
+			phalcon_call_method_p1(code, this_ptr, "getoption", option);
+		} else {
+			ZVAL_LONG(code, 0);
+		}
 	
 		PHALCON_INIT_VAR(message);
 		object_init_ex(message, phalcon_validation_message_ce);
-		phalcon_call_method_p3_noret(message, "__construct", message_str, attribute, type);
+		phalcon_call_method_p4_noret(message, "__construct", message_str, attribute, type, code);
 	
 		phalcon_call_method_p1_noret(validator, "appendmessage", message);
 		RETURN_MM_FALSE;
