988f6015db70b08c228620a140dfac5a728b0391
dreamsxin <dreamsxin@126.com>
2013-08-31 14:53:08 +0800

    Updated HttpClientTest.php

--- unit-tests/HttpClientTest.php
+++ unit-tests/HttpClientTest.php
@@ -24,22 +24,24 @@ class HttpClientTest extends PHPUnit_Framework_TestCase
 
 	public function test()
 	{		
-		$url = 'http://localhost/upload.php';
+		$url = 'https://github.com';
 		$method = 'POST';
 		$data = array('key' => 'value', 'key1' => 'value1');
-
-		//$client = new \Phalcon\Http\Client($url, 'POST');
-		$client = new \Phalcon\Http\Client;
-		$client->setUrl($url);
-		$client->setMethod($method);
-
-		// $client->setAuthentication('username', 'password');
-		// $client->setDigestAuthentication('username', 'password');
-		// $client->setBasicAuthentication('username', 'password');
-
-		$client->setData($data);
-		//$client->setFiles(array('unit-tests/assets/phalconphp.jpg'));
 		try {
+			//$client = new \Phalcon\Http\Client($url, 'POST');
+			$client = new \Phalcon\Http\Client;
+			$client->setUrl($url);
+			$client->setMethod($method);
+
+			// $client->setAuthentication('username', 'password');
+			// $client->setDigestAuthentication('username', 'password');
+			// $client->setBasicAuthentication('username', 'password');
+
+			// Set post/put/other data
+			$client->setData($data);
+
+			// Upload file
+			//$client->setFiles(array('unit-tests/assets/phalconphp.jpg'));
 			
 			$ret = $client->getUrl();
 			$this->assertEquals($ret, $url);
@@ -52,6 +54,11 @@ class HttpClientTest extends PHPUnit_Framework_TestCase
 
 			$ret = $client->send();
 			$this->assertTrue($ret);
+			
+			// $ret = $client->getResponseHeaders();
+			// $ret = $client->getResponseCookies();
+			// $ret = $client->getResponseBody();
+			// $ret = $client->getResponseStatus();
 		} catch (Exception $ex) {
 		}
 	}
