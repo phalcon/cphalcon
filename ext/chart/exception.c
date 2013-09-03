fdc7bc48efd895ea20619729a8039fd78634d55d
dreamsxin <dreamsxin@126.com>
2013-09-03 12:11:18 +0800

    add ChartTest.php

---
+++ ext/chart/exception.c
@@ -0,0 +1,56 @@
+
+/*
+  +------------------------------------------------------------------------+
+  | Phalcon Framework                                                      |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2011-2013 Phalcon Team (http://www.phalconphp.com)       |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file docs/LICENSE.txt.                        |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconphp.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
+  | Authors: Andres Gutierrez <andres@phalconphp.com>                      |
+  |          Eduar Carvajal <eduar@phalconphp.com>                         |
+  |          ZhuZongXin <dreamsxin@qq.com>                                 |
+  |          Vladimir Kolesnikov <vladimir@free-sevastopol.com>            |
+  +------------------------------------------------------------------------+
+*/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "php.h"
+#include "php_phalcon.h"
+#include "phalcon.h"
+
+#include "Zend/zend_operators.h"
+#include "Zend/zend_exceptions.h"
+#include "Zend/zend_interfaces.h"
+
+#include "kernel/main.h"
+#include "kernel/memory.h"
+
+#include "chart/exception.h"
+
+/**
+ * Phalcon\Chart\Exception
+ *
+ * Exceptions thrown in Phalcon\Chart will use this class
+ *
+ */
+
+
+/**
+ * Phalcon\Chart\Exception initializer
+ */
+PHALCON_INIT_CLASS(Phalcon_Chart_Exception){
+
+	PHALCON_REGISTER_CLASS_EX(Phalcon\\Chart, Exception, chart_exception, phalcon_exception_ce, NULL, 0);
+
+	return SUCCESS;
+}
+
---
+++ unit-tests/ChartTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+	+------------------------------------------------------------------------+
+	| Phalcon Framework                                                      |
+	+------------------------------------------------------------------------+
+	| Copyright (c) 2011-2013 Phalcon Team (http://www.phalconphp.com)       |
+	+------------------------------------------------------------------------+
+	| This source file is subject to the New BSD License that is bundled     |
+	| with this package in the file docs/LICENSE.txt.                        |
+	|                                                                        |
+	| If you did not receive a copy of the license and are unable to         |
+	| obtain it through the world-wide-web, please send an email             |
+	| to license@phalconphp.com so we can send you a copy immediately.       |
+	+------------------------------------------------------------------------+
+	| Authors: Andres Gutierrez <andres@phalconphp.com>                      |
+	|          Eduar Carvajal <eduar@phalconphp.com>                         |
+	+------------------------------------------------------------------------+
+*/
+
+class ChartTest extends PHPUnit_Framework_TestCase
+{
+
+	public function testQRencode()
+	{
+		try {
+			$qr = new \Phalcon\Chart\QRencode('Phalcon is web framework');
+			if ($qr->generate()) {
+				// $data = $qr->render();
+				// $qr->save('qr.png');
+			}
+		} catch (Exception) {
+		}
+	}
+}
