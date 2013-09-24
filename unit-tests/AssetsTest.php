bfae0e2ae623e4c264037e87ead1f861e108e498
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-09-25 02:11:21 +0300

    Fix memory leaks

--- unit-tests/AssetsTest.php
+++ unit-tests/AssetsTest.php
@@ -121,6 +121,8 @@ class AssetsTest extends PHPUnit_Framework_TestCase
 			return $url;
 		});
 
+		$di->set('escaper', function() { return new \Phalcon\Escaper(); });
+
 		//With implicit output
 
 		ob_start();
