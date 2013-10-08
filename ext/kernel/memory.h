f903d98bd3367783308f765b793e120b9f6e9369
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-10-08 04:57:27 +0300

    Fix #1340 (cherry picked from commit 2c7c1f8b9bbb903ae5a33eeb02b70cd8f8e8ce8b)
    Conflicts:
    	ext/kernel/memory.h

--- ext/kernel/memory.h
+++ ext/kernel/memory.h
@@ -139,17 +139,15 @@ extern void PHALCON_FASTCALL phalcon_copy_ctor(zval *destiny, zval *origin);
 	}
 
 #define PHALCON_SEPARATE_PARAM(z) \
-	{\
+	do { \
 		zval *orig_ptr = z;\
-		if (Z_REFCOUNT_P(orig_ptr) > 1) {\
-			phalcon_memory_observe(&z TSRMLS_CC);\
-			ALLOC_ZVAL(z);\
-			*z = *orig_ptr;\
-			zval_copy_ctor(z);\
-			Z_SET_REFCOUNT_P(z, 1);\
-			Z_UNSET_ISREF_P(z);\
-		}\
-	}
+		phalcon_memory_observe(&z TSRMLS_CC);\
+		ALLOC_ZVAL(z);\
+		*z = *orig_ptr;\
+		zval_copy_ctor(z);\
+		Z_SET_REFCOUNT_P(z, 1);\
+		Z_UNSET_ISREF_P(z);\
+	} while (0)
 
 #define PHALCON_SEPARATE_PARAM_NMO(z) { \
 		zval *orig_ptr = z; \
@@ -168,3 +166,4 @@ extern void PHALCON_FASTCALL phalcon_copy_ctor(zval *destiny, zval *origin);
 	} else { \
 		zval_ptr_dtor(&var); \
 	}
+
