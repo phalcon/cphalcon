bf32b51a25bca7d9a41b6523b951c233e6072c57
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-10-09 13:01:58 +0300

    Backported ed3548dd0fb74929124397ae1796999624778b8d

--- ext/kernel/exception.h
+++ ext/kernel/exception.h
@@ -28,6 +28,3 @@ extern void phalcon_throw_exception(zval *object TSRMLS_DC);
 extern void phalcon_throw_exception_string(zend_class_entry *ce, const char *message, zend_uint message_len, int restore_stack TSRMLS_DC);
 extern void phalcon_throw_exception_zval(zend_class_entry *ce, zval *message, int restore_stack TSRMLS_DC);
 extern void phalcon_throw_exception_internal(zval *exception TSRMLS_DC);
-
-/** Catch Exceptions */
-/* extern void phalcon_try_execute(zval *success, zval *return_value, zval *call_object, zval *params, zval **exception TSRMLS_DC); */
