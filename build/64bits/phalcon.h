14283ed4f178fdabed6b49fc56f43c8e1b8b5db1
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-12-12 04:54:00 +0200

    Regenerated build/

--- build/64bits/phalcon.h
+++ build/64bits/phalcon.h
@@ -5194,9 +5194,11 @@ PHALCON_INIT_FUNCS(phalcon_mvc_view_engine_method_entry){
 
 
 zend_class_entry *phalcon_translate_adapter_ce;
+zend_object_handlers phalcon_translate_adapter_object_handlers;
 
 PHALCON_INIT_CLASS(Phalcon_Translate_Adapter);
 
+static PHP_METHOD(Phalcon_Translate_Adapter, __construct);
 static PHP_METHOD(Phalcon_Translate_Adapter, _);
 static PHP_METHOD(Phalcon_Translate_Adapter, offsetSet);
 static PHP_METHOD(Phalcon_Translate_Adapter, offsetExists);
@@ -5226,6 +5228,7 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_translate_adapter_offsetget, 0, 0, 1)
 ZEND_END_ARG_INFO()
 
 PHALCON_INIT_FUNCS(phalcon_translate_adapter_method_entry){
+	PHP_ME(Phalcon_Translate_Adapter, __construct, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
 	PHP_ME(Phalcon_Translate_Adapter, _, arginfo_phalcon_translate_adapter__, ZEND_ACC_PUBLIC) 
 	PHP_ME(Phalcon_Translate_Adapter, offsetSet, arginfo_phalcon_translate_adapter_offsetset, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Translate_Adapter, offsetExists, arginfo_phalcon_translate_adapter_offsetexists, ZEND_ACC_PUBLIC)
@@ -10287,6 +10290,17 @@ PHALCON_INIT_FUNCS(phalcon_forms_element_textarea_method_entry){
 };
 
 
+#ifndef PHALCON_FORMS_ELEMENT_RADIO_H
+#define PHALCON_FORMS_ELEMENT_RADIO_H
+
+#include "php_phalcon.h"
+
+zend_class_entry *phalcon_forms_element_radio_ce;
+
+PHALCON_INIT_CLASS(Phalcon_Forms_Element_Radio);
+
+#endif /* PHALCON_FORMS_ELEMENT_RADIO_H */
+
 
 #define PHALCON_CRYPT_PADDING_DEFAULT        0
 #define PHALCON_CRYPT_PADDING_ANSI_X_923     1
@@ -11425,6 +11439,8 @@ static PHP_METHOD(Phalcon_Queue_Beanstalk, reserve);
 static PHP_METHOD(Phalcon_Queue_Beanstalk, choose);
 static PHP_METHOD(Phalcon_Queue_Beanstalk, watch);
 static PHP_METHOD(Phalcon_Queue_Beanstalk, peekReady);
+static PHP_METHOD(Phalcon_Queue_Beanstalk, peekDelayed);
+static PHP_METHOD(Phalcon_Queue_Beanstalk, peekBuried);
 static PHP_METHOD(Phalcon_Queue_Beanstalk, readStatus);
 static PHP_METHOD(Phalcon_Queue_Beanstalk, read);
 static PHP_METHOD(Phalcon_Queue_Beanstalk, write);
@@ -11465,6 +11481,8 @@ PHALCON_INIT_FUNCS(phalcon_queue_beanstalk_method_entry){
 	PHP_ME(Phalcon_Queue_Beanstalk, choose, arginfo_phalcon_queue_beanstalk_choose, ZEND_ACC_PUBLIC) 
 	PHP_ME(Phalcon_Queue_Beanstalk, watch, arginfo_phalcon_queue_beanstalk_watch, ZEND_ACC_PUBLIC) 
 	PHP_ME(Phalcon_Queue_Beanstalk, peekReady, NULL, ZEND_ACC_PUBLIC) 
+	PHP_ME(Phalcon_Queue_Beanstalk, peekDelayed, NULL, ZEND_ACC_PUBLIC)
+	PHP_ME(Phalcon_Queue_Beanstalk, peekBuried, NULL, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Queue_Beanstalk, readStatus, NULL, ZEND_ACC_PROTECTED) 
 	PHP_ME(Phalcon_Queue_Beanstalk, read, arginfo_phalcon_queue_beanstalk_read, ZEND_ACC_PUBLIC) 
 	PHP_ME(Phalcon_Queue_Beanstalk, write, NULL, ZEND_ACC_PROTECTED) 
@@ -11484,6 +11502,10 @@ static PHP_METHOD(Phalcon_Queue_Beanstalk_Job, __construct);
 static PHP_METHOD(Phalcon_Queue_Beanstalk_Job, getId);
 static PHP_METHOD(Phalcon_Queue_Beanstalk_Job, getBody);
 static PHP_METHOD(Phalcon_Queue_Beanstalk_Job, delete);
+static PHP_METHOD(Phalcon_Queue_Beanstalk_Job, release);
+static PHP_METHOD(Phalcon_Queue_Beanstalk_Job, bury);
+static PHP_METHOD(Phalcon_Queue_Beanstalk_Job, touch);
+static PHP_METHOD(Phalcon_Queue_Beanstalk_Job, kick);
 static PHP_METHOD(Phalcon_Queue_Beanstalk_Job, __wakeup);
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_queue_beanstalk_job___construct, 0, 0, 3)
@@ -11497,6 +11519,10 @@ PHALCON_INIT_FUNCS(phalcon_queue_beanstalk_job_method_entry){
 	PHP_ME(Phalcon_Queue_Beanstalk_Job, getId, NULL, ZEND_ACC_PUBLIC) 
 	PHP_ME(Phalcon_Queue_Beanstalk_Job, getBody, NULL, ZEND_ACC_PUBLIC) 
 	PHP_ME(Phalcon_Queue_Beanstalk_Job, delete, NULL, ZEND_ACC_PUBLIC) 
+	PHP_ME(Phalcon_Queue_Beanstalk_Job, release, NULL, ZEND_ACC_PUBLIC)
+	PHP_ME(Phalcon_Queue_Beanstalk_Job, bury, NULL, ZEND_ACC_PUBLIC)
+	PHP_ME(Phalcon_Queue_Beanstalk_Job, touch, NULL, ZEND_ACC_PUBLIC)
+	PHP_ME(Phalcon_Queue_Beanstalk_Job, kick, NULL, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Queue_Beanstalk_Job, __wakeup, NULL, ZEND_ACC_PUBLIC)
 	PHP_FE_END
 };
