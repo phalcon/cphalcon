ef3055a16879fced6d26c1bf9b04088d615e1174
Cinderella-Man <emanuel1989@gmail.com>
2014-01-12 17:48:58 +0000

    Updates after PR

--- ext/paginator/adapter/querybuilder.c
+++ ext/paginator/adapter/querybuilder.c
@@ -59,6 +59,10 @@ PHP_METHOD(Phalcon_Paginator_Adapter_QueryBuilder, getLimit);
 PHP_METHOD(Phalcon_Paginator_Adapter_QueryBuilder, setQueryBuilder);
 PHP_METHOD(Phalcon_Paginator_Adapter_QueryBuilder, getQueryBuilder);
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_paginator_adapter_querybuilder_setquerybuilder, 0, 0, 1)
+    ZEND_ARG_INFO(0, queryBuilder)
+ZEND_END_ARG_INFO()
+
 static const zend_function_entry phalcon_paginator_adapter_querybuilder_method_entry[] = {
 	PHP_ME(Phalcon_Paginator_Adapter_QueryBuilder, __construct, arginfo_phalcon_paginator_adapterinterface___construct, ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
 	PHP_ME(Phalcon_Paginator_Adapter_QueryBuilder, getPaginate, arginfo_phalcon_paginator_adapterinterface_getcurrentpage, ZEND_ACC_PUBLIC)
@@ -66,7 +70,7 @@ static const zend_function_entry phalcon_paginator_adapter_querybuilder_method_e
 	PHP_ME(Phalcon_Paginator_Adapter_QueryBuilder, getLimit, NULL, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Paginator_Adapter_QueryBuilder, setCurrentPage, arginfo_phalcon_paginator_adapterinterface_setcurrentpage, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Paginator_Adapter_QueryBuilder, getCurrentPage, NULL, ZEND_ACC_PUBLIC)
-	PHP_ME(Phalcon_Paginator_Adapter_QueryBuilder, setQueryBuilder, NULL, ZEND_ACC_PUBLIC)
+	PHP_ME(Phalcon_Paginator_Adapter_QueryBuilder, setQueryBuilder, arginfo_phalcon_paginator_adapter_querybuilder_setquerybuilder, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Paginator_Adapter_QueryBuilder, getQueryBuilder, NULL, ZEND_ACC_PUBLIC)
 	PHP_FE_END
 };
