4cc0079409f1cade48a15faee2a04d3d44938ae1
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2014-01-26 16:19:02 +0200

    Handle null offset in phalcon_update_property_array()

--- ext/kernel/object.c
+++ ext/kernel/object.c
@@ -1012,6 +1012,8 @@ int phalcon_update_property_array(zval *object, char *property, unsigned int pro
 			zend_symtable_update(Z_ARRVAL_P(tmp), Z_STRVAL_P(index), Z_STRLEN_P(index) + 1, &value, sizeof(zval*), NULL);
 		} else if (Z_TYPE_P(index) == IS_LONG) {
 			zend_hash_index_update(Z_ARRVAL_P(tmp), Z_LVAL_P(index), &value, sizeof(zval *), NULL);
+		} else if (Z_TYPE_P(index) == IS_NULL) {
+			zend_hash_next_index_insert(Z_ARRVAL_P(tmp), (void**)&value, sizeof(zval*), NULL);
 		}
 
 		if (separated) {
