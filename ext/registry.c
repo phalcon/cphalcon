87fc89b737f1dfac2e0ece82084e3ce44f6be667
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2014-01-29 01:04:08 +0200

    Added comments

--- ext/registry.c
+++ ext/registry.c
@@ -25,15 +25,61 @@
 #include <ext/standard/php_var.h>
 #include <ext/standard/php_smart_str.h>
 
-#if defined(PHALCON_USE_PHP_JSON) && PHP_VERSION_ID >= 50400
-#	include <ext/json/php_json.h>
-#endif
-
 #include "kernel/main.h"
 #include "kernel/object.h"
 #include "kernel/hash.h"
 #include "kernel/fcall.h"
 
+/**
+ * Phalcon\Registry
+ *
+ * A registry is a container for storing objects and values in the application space.
+ * By storing the value in a registry, the same object is always available throughout
+ * your application.
+ *
+ * <code>
+ * 	$registry = new \Phalcon\Registry();
+ *
+ * 	// Set value
+ * 	$registry->something = 'something';
+ * 	// or
+ * 	$registry['something'] = 'something';
+ *
+ * 	// Get value
+ * 	$value = $registry->something;
+ * 	// or
+ * 	$value = $registry['something'];
+ *
+ * 	// Check if the key exists
+ * 	$exists = isset($registry->something);
+ * 	// or
+ * 	$exists = isset($registry['something']);
+ *
+ * 	// Unset
+ * 	unset($registry->something);
+ * 	// or
+ * 	unset($registry['something']);
+ * </code>
+ *
+ * In addition to ArrayAccess, Phalcon\Registry also implements Countable
+ * (count($registry) will return the number of elements in the registry),
+ * Serializable and IteratorAggregate (you can iterate over the registry
+ * using a foreach loop) interfaces. For PHP 5.4 and higher, JsonSerializable
+ * interface is implemented.
+ *
+ * Phalcon\Registry is very fast (it is typically faster than any userspace
+ * implementation of the registry); however, this comes at a price:
+ * Phalcon\Registry is a final class and cannot be inherited from.
+ *
+ * Though Phalcon\Registry exposes methods like __get(), offsetGet(), count() etc,
+ * it is not recommended to invoke them manually (these method exists mainly to
+ * match the interfaces the registry implements): $registry->__get('property')
+ * is several times slower than $registry->property.
+ *
+ * Internally all the magic methods (and interfaces except JsonSerializable)
+ * are implemented using object handlers or similar techniques: this allows
+ * to bypass relatively slow method calls.
+ */
 zend_class_entry *phalcon_registry_ce;
 
 static zend_object_handlers phalcon_registry_object_handlers;
