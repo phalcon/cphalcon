aa75a378f7f17bbae93f22e359f486c26aafce24
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2014-02-12 17:06:10 +0200

    Clean up Phalcon\DiInterface
    * Remove `setShared()` method because it is a shorthand for
      `set($name, $definition, true)`;
    * Remove `attempt()` method because it is a shorthand for
      `has()` + `set()`
    * Rename `setRaw()` to `setService()` because `getRaw()`
      is actually `getService()` + `$service->getDefinition()`
    * Remove `getRaw()` because it is `getService()` +
      `$service->getDefinition()`
    * Modify `setService($name, $raw_definition)` to
      `setService($raw_definition)` because service name
      can be retrieved from the definition.

--- ext/diinterface.c
+++ ext/diinterface.c
@@ -27,13 +27,10 @@ zend_class_entry *phalcon_diinterface_ce;
 
 static const zend_function_entry phalcon_diinterface_method_entry[] = {
 	PHP_ABSTRACT_ME(Phalcon_DiInterface, set, arginfo_phalcon_diinterface_set)
-	PHP_ABSTRACT_ME(Phalcon_DiInterface, setShared, arginfo_phalcon_diinterface_setshared)
 	PHP_ABSTRACT_ME(Phalcon_DiInterface, remove, arginfo_phalcon_diinterface_remove)
-	PHP_ABSTRACT_ME(Phalcon_DiInterface, attempt, arginfo_phalcon_diinterface_attempt)
 	PHP_ABSTRACT_ME(Phalcon_DiInterface, get, arginfo_phalcon_diinterface_get)
 	PHP_ABSTRACT_ME(Phalcon_DiInterface, getShared, arginfo_phalcon_diinterface_getshared)
-	PHP_ABSTRACT_ME(Phalcon_DiInterface, setRaw, arginfo_phalcon_diinterface_setraw)
-	PHP_ABSTRACT_ME(Phalcon_DiInterface, getRaw, arginfo_phalcon_diinterface_getraw)
+	PHP_ABSTRACT_ME(Phalcon_DiInterface, setService, arginfo_phalcon_diinterface_setservice)
 	PHP_ABSTRACT_ME(Phalcon_DiInterface, getService, arginfo_phalcon_diinterface_getservice)
 	PHP_ABSTRACT_ME(Phalcon_DiInterface, has, arginfo_phalcon_diinterface_has)
 	PHP_ABSTRACT_ME(Phalcon_DiInterface, wasFreshInstance, NULL)
@@ -56,7 +53,7 @@ PHALCON_INIT_CLASS(Phalcon_DiInterface){
 }
 
 /**
- * Registers a service in the services container
+ * Registers a service in the service container
  *
  * @param string $name
  * @param mixed $definition
@@ -66,48 +63,27 @@ PHALCON_INIT_CLASS(Phalcon_DiInterface){
 PHALCON_DOC_METHOD(Phalcon_DiInterface, set);
 
 /**
- * Registers an "always shared" service in the services container
- *
- * @param string $name
- * @param mixed $definition
- * @return Phalcon\DI\ServiceInterface
- */
-PHALCON_DOC_METHOD(Phalcon_DiInterface, setShared);
-
-/**
- * Removes a service in the services container
+ * Removes a service from the service container
  *
  * @param string $name
  */
 PHALCON_DOC_METHOD(Phalcon_DiInterface, remove);
 
 /**
- * Attempts to register a service in the services container
- * Only is successful if a service hasn't been registered previously
- * with the same name
- *
- * @param string $name
- * @param mixed $definition
- * @param boolean $shared
- * @return Phalcon\DI\ServiceInterface
- */
-PHALCON_DOC_METHOD(Phalcon_DiInterface, attempt);
-
-/**
  * Resolves the service based on its configuration
  *
  * @param string $name
  * @param array $parameters
- * @return mixed
+ * @return object
  */
 PHALCON_DOC_METHOD(Phalcon_DiInterface, get);
 
 /**
- * Returns a shared service based on their configuration
+ * Resolves a shared service based on their configuration
  *
  * @param string $name
  * @param array $parameters
- * @return mixed
+ * @return object
  */
 PHALCON_DOC_METHOD(Phalcon_DiInterface, getShared);
 
@@ -118,15 +94,7 @@ PHALCON_DOC_METHOD(Phalcon_DiInterface, getShared);
  * @param Phalcon\DI\ServiceInterface $rawDefinition
  * @return Phalcon\DI\ServiceInterface
  */
-PHALCON_DOC_METHOD(Phalcon_DiInterface, setRaw);
-
-/**
- * Returns a service definition without resolving
- *
- * @param string $name
- * @return mixed
- */
-PHALCON_DOC_METHOD(Phalcon_DiInterface, getRaw);
+PHALCON_DOC_METHOD(Phalcon_DiInterface, setService);
 
 /**
  * Returns the corresponding Phalcon\Di\Service instance for a service
@@ -159,7 +127,7 @@ PHALCON_DOC_METHOD(Phalcon_DiInterface, wasFreshInstance);
 PHALCON_DOC_METHOD(Phalcon_DiInterface, getServices);
 
 /**
- * Set a default dependency injection container to be obtained into static methods
+ * Set the default dependency injection container to be obtained into static methods
  *
  * @param Phalcon\DiInterface $dependencyInjector
  */
