0075fcc230718b1e7a1b3060b3df52de29b0505b
dreamsxin <dreamsxin@126.com>
2014-02-19 15:47:17 +0800

    Updated

--- ext/tag.c
+++ ext/tag.c
@@ -779,13 +779,10 @@ PHP_METHOD(Phalcon_Tag, linkTo){
 
 	if (phalcon_array_isset_string_fetch(&query, params, SS("query"))) {
 		phalcon_array_unset_string(&params, SS("query"), 0);
-
-		PHALCON_INIT_VAR(query_string);
-		phalcon_http_build_query(query_string, query, "&" TSRMLS_CC);
 	}
 	else {
-		PHALCON_INIT_VAR(query_string);
-		ZVAL_EMPTY_STRING(query_string);
+		PHALCON_INIT_VAR(query);
+		ZVAL_NULL(query);
 	}
 
 	if (zend_is_true(z_local) || Z_TYPE_P(params) == IS_ARRAY) {
@@ -793,26 +790,21 @@ PHP_METHOD(Phalcon_Tag, linkTo){
 		PHALCON_CALL_SELF(&url, "geturlservice");
 		
 		PHALCON_INIT_VAR(internal_url);
-		phalcon_call_method_p1(internal_url, url, "get", action);
-		
-		if (Z_TYPE_P(query_string) == IS_STRING && Z_STRLEN_P(query_string)) {
-				if (phalcon_memnstr_str(internal_url, "?", 1)) {
-						PHALCON_SCONCAT_SV(internal_url, "&", query_string);
-				}
-				else {
-						PHALCON_SCONCAT_SV(internal_url, "?", query_string);
-				}
-		}
-
+		phalcon_call_method_p2(internal_url, url, "get", action, query);
 		phalcon_array_update_string(&params, SL("href"), internal_url, PH_COPY);
 	} else {
-		if (Z_TYPE_P(query_string) == IS_STRING && Z_STRLEN_P(query_string)) {
-				if (phalcon_memnstr_str(action, "?", 1)) {
-						PHALCON_SCONCAT_SV(action, "&", query_string);
-				}
-				else {
-						PHALCON_SCONCAT_SV(action, "?", query_string);
-				}
+		if (Z_TYPE_P(query) != IS_NULL) {
+			PHALCON_INIT_VAR(query_string);
+			phalcon_http_build_query(query_string, query, "&" TSRMLS_CC);
+		
+			if (Z_TYPE_P(query_string) == IS_STRING && Z_STRLEN_P(query_string)) {
+					if (phalcon_memnstr_str(action, "?", 1)) {
+							PHALCON_SCONCAT_SV(action, "&", query_string);
+					}
+					else {
+							PHALCON_SCONCAT_SV(action, "?", query_string);
+					}
+			}
 		}
 
 		phalcon_array_update_string(&params, SL("href"), action, PH_COPY);
