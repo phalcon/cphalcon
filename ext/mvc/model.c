96534471556b4a03a9bf323f7909998c75d420c2
dreamsxin <dreamsxin@qq.com>
2014-02-24 19:18:45 +0800

    Add option allow_empty for Model::validate

--- ext/mvc/model.c
+++ ext/mvc/model.c
@@ -2243,16 +2243,34 @@ PHP_METHOD(Phalcon_Mvc_Model, appendMessage){
  */
 PHP_METHOD(Phalcon_Mvc_Model, validate){
 
-	zval *validator, *status = NULL, *messages = NULL, *errors, *new_errors;
+	zval *validator, *allow_empty = NULL, *option = NULL, *field_name = NULL, *value = NULL, *status = NULL, *messages = NULL, *errors, *new_errors;
 
 	PHALCON_MM_GROW();
 
-	phalcon_fetch_params(1, 1, 0, &validator);
+	phalcon_fetch_params(1, 1, 1, &validator, &allow_empty);
 	
 	/** 
 	 * Valid validators are objects
 	 */
 	PHALCON_VERIFY_INTERFACE_EX(validator, phalcon_mvc_model_validatorinterface_ce, phalcon_mvc_model_exception_ce, 1);
+
+	if (!allow_empty) {
+		PHALCON_INIT_VAR(allow_empty);
+		ZVAL_FALSE(allow_empty);
+	}
+
+	if (zend_is_true(allow_empty)) {
+		HALCON_INIT_VAR(option);
+		ZVAL_STRING(option, "field", 1);
+
+		PHALCON_CALL_METHOD(&field_name, validator, "getoption", option);
+
+		PHALCON_CALL_METHOD(&value, this_ptr, "readattribute", field_name);
+
+		if (PHALCON_IS_EMPTY(value)) {
+			RETURN_THIS();
+		}
+	}
 	
 	/** 
 	 * Call the validation, if it returns false we append the messages to the current
