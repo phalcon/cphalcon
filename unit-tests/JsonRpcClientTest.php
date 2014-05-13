68d2bb7e52b43adb028ad76c759f3576dd9cf6b2
dreamsxin <dreamsxin@qq.com>
2014-05-13 20:22:17 +0800

    Updated JsonRpcClientTest.php

--- unit-tests/JsonRpcClientTest.php
+++ unit-tests/JsonRpcClientTest.php
@@ -1,15 +1,40 @@
 <?php
 
+/*
+ * Server demo
+error_reporting(E_ALL);
+
+try {
+        $loader = new \Phalcon\Loader();
+
+        $loader->registerDirs(
+                array(
+                        __DIR__ . '/controllers/',
+                )
+        )->register();
+
+        $di = new \Phalcon\DI\FactoryDefault();
+        $di->set('url', function () use ($config) {
+                $url = new \Phalcon\Mvc\Url();
+                $url->setBaseUri('jsonrpc');
+                return $url;
+        }, true);
+
+        $application = new \Phalcon\Mvc\JsonRpc($di);
+
+        echo $application->handle()->getContent();
+} catch (\Exception $e) {
+        echo $e->getMessage();
+}              
+*/
+
 class JsonRpcClientTest extends PHPUnit_Framework_TestCase
 {
 	public function testClient()
 	{
-		$data = array('message' => 'helloword');
-
-		$client = new Phalcon\Http\Client\Adapter\Stream('http://localhost/edu/php/passport/public/');
+		$client = new Phalcon\Http\Client\Adapter\Stream('http://myleftstudio.com:81/jsonrpc/');
 
 		$rpc = new Phalcon\JsonRpc\Client($client);
-		//$response = $rpc->call('index/index', array(1, 2));
-		//var_dump($response);
+		$response = $rpc->call('index/index', array('message' => 'helloword'));
 	}
 }
