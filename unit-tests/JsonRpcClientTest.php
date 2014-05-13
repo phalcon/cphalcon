3e462b5aa29bfea6ba65a26c7fff258ff85ad0d6
dreamsxin <dreamsxin@qq.com>
2014-05-13 20:00:48 +0800

    Updated JsonRpcClientTest.php

--- unit-tests/JsonRpcClientTest.php
+++ unit-tests/JsonRpcClientTest.php
@@ -9,7 +9,7 @@ class JsonRpcClientTest extends PHPUnit_Framework_TestCase
 		$client = new Phalcon\Http\Client\Adapter\Stream('http://localhost/edu/php/passport/public/');
 
 		$rpc = new Phalcon\JsonRpc\Client($client);
-		$response = $rpc->call('index/index', array(1, 2));
-		var_dump($response);
+		//$response = $rpc->call('index/index', array(1, 2));
+		//var_dump($response);
 	}
 }
