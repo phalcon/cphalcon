b054183a28a9fcae4f8eec9f504550f5118605fa
Wenzel Pünter <wenzel@phelix.me>
2014-06-19 09:18:17 +0200

    Fixed typo

--- ext/mvc/model/query.c
+++ ext/mvc/model/query.c
@@ -4901,7 +4901,7 @@ PHP_METHOD(Phalcon_Mvc_Model_Query, execute){
  *
  * @param array $bindParams
  * @param array $bindTypes
- * @return Ṕhalcon\Mvc\ModelInterface
+ * @return Phalcon\Mvc\ModelInterface
  */
 PHP_METHOD(Phalcon_Mvc_Model_Query, getSingleResult){
 
