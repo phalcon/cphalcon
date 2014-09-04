33371949dfae7aefebf4c6096f9a9bff25ce676f
Nikolaos Dimopoulos <nikos@niden.net>
2014-09-04 13:56:56 -0400

    Moved Security tests to codeception

---
+++ tests/unit/Phalcon/Security/UnitTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * UnitTest.php
+ * \Phalcon\Security
+ *
+ * Tests the \Phalcon\Security component
+ *
+ * PhalconPHP Framework
+ *
+ * @copyright (c) 2011-2014 Phalcon Team
+ * @link      http://www.phalconphp.com
+ * @author    Andres Gutierrez <andres@phalconphp.com>
+ * @author    Nikolaos Dimopoulos <nikos@phalconphp.com>
+ *
+ * The contents of this file are subject to the New BSD License that is
+ * bundled with this package in the file docs/LICENSE.txt
+ *
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world-wide-web, please send an email to license@phalconphp.com
+ * so that we can send you a copy immediately.
+ */
+
+use \Phalcon\Security as PhSecurity;
+use \Codeception\TestCase\Test as CdTest;
+
+class SecurityUnitTest extends CdTest
+{
+    /**
+     * Tests the HMAC computation
+     *
+     * @author Nikolaos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-04
+     */
+    public function testComputeHMAC()
+    {
+        $security = new PhSecurity;
+        $key      = md5('test', true);
+        $keys     = array(
+            substr($key, 0, strlen($key) / 2),
+            $key,
+            $key . $key
+        );
+
+        $data = array();
+        for ($i = 1; $i < 256; ++$i) {
+            $data[] = str_repeat('a', $i);
+        }
+
+        foreach ($keys as $key) {
+            foreach ($data as $text) {
+                $actual   = $security->computeHmac($text, $key, 'md5');
+                $expected = hash_hmac('md5', $text, $key);
+                expect($actual)->equals($expected);
+            }
+        }
+    }
+}
