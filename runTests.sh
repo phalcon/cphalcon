47c9eb40751d01e286f97532047eceb767d4b17f
Nikolaos Dimopoulos <nikos@niden.net>
2014-09-05 09:22:48 -0400

    More modifications to the run script

--- runTests.sh
+++ runTests.sh
@@ -2,7 +2,7 @@
 
 ZEND_DONT_UNLOAD_MODULES=1 $(phpenv which php) ./unit-tests/ci/phpunit.php --debug -c unit-tests/phpunit.xml --testsuite=stable
 
-if [ "$TRAVIS_PHP_VERSION" == "5.3" ] || [ "$TRAVIS_PHP_VERSION" == "5.3.3" ]
+if [ "$(php -r 'echo substr(PHP_VERSION, 0, 3);')" = "5.3" ];
 then
    $(phpenv which php) codecept53.phar build
    ZEND_DONT_UNLOAD_MODULES=1 $(phpenv which php) codecept53.phar run
