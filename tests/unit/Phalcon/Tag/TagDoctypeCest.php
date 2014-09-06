ce80e37182858aac3fabfa336a39c37bfda35fa3
Nikolaos Dimopoulos <nikos@niden.net>
2014-09-05 21:05:21 -0400

    Moved the URL tests under Phalcon Converted Tag:Doctype tests to codeception

--- tests/unit/Phalcon/Tag/TagDoctypeCest.php
+++ tests/unit/Phalcon/Tag/TagDoctypeCest.php
@@ -28,25 +28,291 @@ class TagDoctypeCest
     private $message = "%s does not return proper html element";
 
     /**
-     * Tests the setting the doctype
+     * Tests the setting the doctype 3.2
      *
      * @author Nikolaos Dimopoulos <nikos@phalconphp.com>
      * @since  2014-09-04
      *
      * @param CodeGuy $I
      */
-    public function testDoctypeSet(CodeGuy $I)
+    public function testDoctypeSet32Final(CodeGuy $I)
     {
-        $doctype  = PhTag::HTML5;
-        $expected = "<!DOCTYPE html>" . PHP_EOL;
-
+        $doctype  = PhTag::HTML32;
         PhTag::setDocType($doctype);
-        $actual = PhTag::getDocType();
+        
+        $I->assertEquals(
+            $this->docTypeToString($doctype),
+            PhTag::getDocType(),
+            sprintf($this->message, 'Doctype (3.2 Final)')
+        );
+    }
+
+    /**
+     * Tests the setting the doctype 4.01 Strict
+     *
+     * @author Nikolaos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-04
+     *
+     * @param CodeGuy $I
+     */
+    public function testDoctypeSet401(CodeGuy $I)
+    {
+        $doctype  = PhTag::HTML401_STRICT;
+        PhTag::setDocType($doctype);
+        
+        $I->assertEquals(
+            $this->docTypeToString($doctype),
+            PhTag::getDocType(),
+            sprintf($this->message, 'Doctype (401 Strict)')
+        );
+    }
+
+    /**
+     * Tests the setting the doctype 4.01 Transitional
+     *
+     * @author Nikolaos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-04
+     *
+     * @param CodeGuy $I
+     */
+    public function testDoctypeSet401Transitional(CodeGuy $I)
+    {
+        $doctype  = PhTag::HTML401_TRANSITIONAL;
+        PhTag::setDocType($doctype);
+        
+        $I->assertEquals(
+            $this->docTypeToString($doctype),
+            PhTag::getDocType(),
+            sprintf($this->message, 'Doctype (401 Transitional)')
+        );
+    }
+
+    /**
+     * Tests the setting the doctype 4.01 Frameset
+     *
+     * @author Nikolaos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-04
+     *
+     * @param CodeGuy $I
+     */
+    public function testDoctypeSet401Frameset(CodeGuy $I)
+    {
+        $doctype  = PhTag::HTML401_FRAMESET;
+        PhTag::setDocType($doctype);
 
         $I->assertEquals(
-            $expected,
-            $actual,
-            sprintf($this->message, 'Doctype (HTML5)')
+            $this->docTypeToString($doctype),
+            PhTag::getDocType(),
+            sprintf($this->message, 'Doctype (401 Frameset)')
         );
+    }
+
+    /**
+     * Tests the setting the doctype 5
+     *
+     * @author Nikolaos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-04
+     *
+     * @param CodeGuy $I
+     */
+    public function testDoctypeSet5(CodeGuy $I)
+    {
+        $doctype  = PhTag::HTML5;
+        PhTag::setDocType($doctype);
+
+        $I->assertEquals(
+            $this->docTypeToString($doctype),
+            PhTag::getDocType(),
+            sprintf($this->message, 'Doctype (5)')
+        );
+    }
+
+    /**
+     * Tests the setting the doctype 1.0 Strict
+     *
+     * @author Nikolaos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-04
+     *
+     * @param CodeGuy $I
+     */
+    public function testDoctypeSet10Strict(CodeGuy $I)
+    {
+        $doctype  = PhTag::XHTML10_STRICT;
+        PhTag::setDocType($doctype);
+
+        $I->assertEquals(
+            $this->docTypeToString($doctype),
+            PhTag::getDocType(),
+            sprintf($this->message, 'Doctype (1.0 Strict)')
+        );
+    }
+
+    /**
+     * Tests the setting the doctype 1.0 Transitional
+     *
+     * @author Nikolaos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-04
+     *
+     * @param CodeGuy $I
+     */
+    public function testDoctypeSet10Transitional(CodeGuy $I)
+    {
+        $doctype  = PhTag::XHTML10_TRANSITIONAL;
+        PhTag::setDocType($doctype);
+
+        $I->assertEquals(
+            $this->docTypeToString($doctype),
+            PhTag::getDocType(),
+            sprintf($this->message, 'Doctype (1.0 Transitional)')
+        );
+    }
+
+    /**
+     * Tests the setting the doctype 1.0 Frameset
+     *
+     * @author Nikolaos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-04
+     *
+     * @param CodeGuy $I
+     */
+    public function testDoctypeSet10Frameset(CodeGuy $I)
+    {
+        $doctype  = PhTag::XHTML10_FRAMESET;
+        PhTag::setDocType($doctype);
+
+        $I->assertEquals(
+            $this->docTypeToString($doctype),
+            PhTag::getDocType(),
+            sprintf($this->message, 'Doctype (1.0 Frameset)')
+        );
+    }
+
+    /**
+     * Tests the setting the doctype 1.1
+     *
+     * @author Nikolaos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-04
+     *
+     * @param CodeGuy $I
+     */
+    public function testDoctypeSet11(CodeGuy $I)
+    {
+        $doctype  = PhTag::XHTML11;
+        PhTag::setDocType($doctype);
+
+        $I->assertEquals(
+            $this->docTypeToString($doctype),
+            PhTag::getDocType(),
+            sprintf($this->message, 'Doctype (1.1)')
+        );
+    }
+
+    /**
+     * Tests the setting the doctype 2.0
+     *
+     * @author Nikolaos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-04
+     *
+     * @param CodeGuy $I
+     */
+    public function testDoctypeSet20(CodeGuy $I)
+    {
+        $doctype  = PhTag::XHTML20;
+        PhTag::setDocType($doctype);
+
+        $I->assertEquals(
+            $this->docTypeToString($doctype),
+            PhTag::getDocType(),
+            sprintf($this->message, 'Doctype (2.0)')
+        );
+    }
+
+    /**
+     * Tests the setting the doctype to a wrong setting
+     *
+     * @author Nikolaos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-04
+     *
+     * @param CodeGuy $I
+     */
+    public function testDoctypeSetWrongParameter(CodeGuy $I)
+    {
+        $doctype  = 99;
+        PhTag::setDocType($doctype);
+
+        $I->assertEmpty(
+            $this->docTypeToString($doctype),
+            PhTag::getDocType(),
+            sprintf($this->message, 'Doctype wrong parameter')
+        );
+    }
+
+	private function docTypeToString($doctype)
+	{
+        $tab = "\t";
+
+		switch ($doctype) {
+			case 1:
+                return '<!DOCTYPE html ' .
+                       'PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">' . PHP_EOL;
+			case 2:
+                return '<!DOCTYPE html ' .
+                       'PUBLIC "-//W3C//DTD HTML 4.01//EN"' .
+                       PHP_EOL .
+                       $tab .
+                       '"http://www.w3.org/TR/html4/strict.dtd">' . PHP_EOL;
+			case 3:
+                return '<!DOCTYPE html ' .
+                       'PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"' .
+                       PHP_EOL .
+                       $tab .
+                       '"http://www.w3.org/TR/html4/loose.dtd">' . PHP_EOL;
+			case 4:
+                return '<!DOCTYPE html ' .
+                       'PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN"' .
+                       PHP_EOL .
+                       $tab .
+                       '"http://www.w3.org/TR/html4/frameset.dtd">' .
+                       PHP_EOL;
+			case 5:
+                return '<!DOCTYPE html>' . PHP_EOL;
+			case 6:
+                return '<!DOCTYPE html ' .
+                       'PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"' .
+                       PHP_EOL .
+                       $tab .
+                       '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' .
+                       PHP_EOL;
+			case 7:
+                return '<!DOCTYPE html ' .
+                       'PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"' .
+                       PHP_EOL .
+                       $tab .
+                       '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' .
+                       PHP_EOL;
+			case 8:
+                return '<!DOCTYPE html ' .
+                       'PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN"' .
+                       PHP_EOL .
+                       $tab .
+                       '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">' .
+                       PHP_EOL;
+			case 9:
+                return '<!DOCTYPE html ' .
+                       'PUBLIC "-//W3C//DTD XHTML 1.1//EN"' .
+                       PHP_EOL .
+                       $tab .
+                       '"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">' .
+                       PHP_EOL;
+			case 10:
+                return '<!DOCTYPE html ' .
+                       'PUBLIC "-//W3C//DTD XHTML 2.0//EN"' .
+                       PHP_EOL .
+                       $tab .
+                       '"http://www.w3.org/MarkUp/DTD/xhtml2.dtd">' .
+                       PHP_EOL;
+            default:
+                return '';
+		}
     }
 }
