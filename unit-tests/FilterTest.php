5401dfe0a9a601e7280aa0a89896c725923d0247
dreamsxin <dreamsxin@126.com>
2014-09-29 09:23:24 +0800

    Add FilterTest.php

---
+++ unit-tests/FilterTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+	+------------------------------------------------------------------------+
+	| Phalcon Framework                                                      |
+	+------------------------------------------------------------------------+
+	| Copyright (c) 2011-2014 Phalcon Team (http://www.phalconphp.com)       |
+	+------------------------------------------------------------------------+
+	| This source file is subject to the New BSD License that is bundled     |
+	| with this package in the file docs/LICENSE.txt.                        |
+	|                                                                        |
+	| If you did not receive a copy of the license and are unable to         |
+	| obtain it through the world-wide-web, please send an email             |
+	| to license@phalconphp.com so we can send you a copy immediately.       |
+	+------------------------------------------------------------------------+
+	| Authors: Andres Gutierrez <andres@phalconphp.com>                      |
+	|          Eduar Carvajal <eduar@phalconphp.com>                         |
+	+------------------------------------------------------------------------+
+*/
+
+class FilterTest extends PHPUnit_Framework_TestCase
+{
+	public function testXSS()
+	{
+		$str = '<strong style="color:blue">Click</strong><div>name</div>';
+		$filter = new Phalcon\Filter;
+		$ret = $filter->sanitize('<strong style="color:blue" onclick="alert(\'clicked\')">Click</strong><div style="color:expression(1+1)">name</div>', 'xssclean');
+
+		$this->assertEquals($ret, $str);
+	}
+}
