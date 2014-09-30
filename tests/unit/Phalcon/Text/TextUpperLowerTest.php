b8fc9e71afd4d7eeeb154ffc661e1950b82cc2ae
Nikolaos Dimopoulos <nikos@niden.net>
2014-09-30 11:38:35 -0400

    Removed duplicate Security test from php-tests Converted Text::upper and Text::camelize strings to Codeception/BDD

---
+++ tests/unit/Phalcon/Text/TextUpperLowerTest.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * TextUpperLowerTest.php
+ * \Phalcon\Text\TextUpperLowerTest
+ *
+ * Tests the Phalcon\Text component
+ *
+ * PhalconPHP Framework
+ *
+ * @copyright (c) 2011-2014 Phalcon Team
+ * @link      http://www.phalconphp.com
+ * @author    Andres Gutierrez <andres@phalconphp.com>
+ * @author    Nikolaos Dimopoulos <nikos@phalconphp.com>
+ *
+ * The contents of this file are subject to the New BSD License that is
+ * bundled with this package in the file docs/LICENSE.txt
+ *
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world-wide-web, please send an email to license@phalconphp.com
+ * so that we can send you a copy immediately.
+ */
+
+namespace Phalcon\Tests\unit\Phalcon\Text;
+
+use Phalcon\Text as PhText;
+
+class TextUpperLowerTest extends \Codeception\TestCase\Test
+{
+    use \Codeception\Specify;
+
+    /**
+     * Tests the upper function
+     *
+     * @author Nikos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-29
+     */
+    public function testUpper()
+    {
+        $this->specify(
+            "upper returns incorrect results",
+            function () {
+
+                expect(PhText::upper('hello'))->equals('HELLO');
+                expect(PhText::upper('HELLO'))->equals('HELLO');
+                expect(PhText::upper('1234'))->equals('1234');
+            }
+        );
+    }
+
+    /**
+     * Tests the upper function with international characters
+     *
+     * @author Nikos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-29
+     */
+    public function testUpperInternational()
+    {
+        $this->specify(
+            "upper with international characters returns incorrect results",
+            function () {
+
+                expect(PhText::upper('γεια'))->equals('ΓΕΙΑ');
+                expect(PhText::upper('gruß'))->equals('GRUSS');
+            }
+        );
+    }
+
+    /**
+     * Tests the lower function
+     *
+     * @author Nikos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-29
+     */
+    public function testLower()
+    {
+        $this->specify(
+            "lower returns incorrect results",
+            function () {
+
+                expect(PhText::upper('hello'))->equals('hello');
+                expect(PhText::upper('HELLO'))->equals('hello');
+                expect(PhText::upper('1234'))->equals('1234');
+            }
+        );
+    }
+
+    /**
+     * Tests the lower function with international characters
+     *
+     * @author Nikos Dimopoulos <nikos@phalconphp.com>
+     * @since  2014-09-29
+     */
+    public function testLowerInternational()
+    {
+        $this->specify(
+            "upper with international characters returns incorrect results",
+            function () {
+
+                expect(PhText::upper('ΓΕΙΑ'))->equals('γεια');
+                expect(PhText::upper('GRUSS'))->equals('gruß');
+            }
+        );
+    }
+
+}
