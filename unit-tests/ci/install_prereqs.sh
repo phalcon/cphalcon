99772ab53b904efc5ef2d1193362aae864cfae1a
Vladimir Khramov <hramov@dv-tender.ru>
2014-11-09 16:00:42 +1000

    test on travis

--- unit-tests/ci/install_prereqs.sh
+++ unit-tests/ci/install_prereqs.sh
@@ -17,8 +17,7 @@ CFLAGS="-O2 -g3 -fno-strict-aliasing" pecl install igbinary < /dev/null &
 
 phpenv config-add "$DIR/memcache.ini"
 phpenv config-rm xdebug.ini
+wait
 
 composer self-update
 composer --version
-
-wait
