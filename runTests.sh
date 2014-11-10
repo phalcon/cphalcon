2a42d8e23b71a08b00134b54705c58632449bb17
Vladimir Khramov <hramov@dv-tender.ru>
2014-11-11 01:14:59 +1000

    build fail on any test fail

--- runTests.sh
+++ runTests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ZEND_DONT_UNLOAD_MODULES=1 $(phpenv which php) ./unit-tests/ci/phpunit.php --debug -c unit-tests/phpunit.xml --testsuite=stable
+status_phpunit=$?
 
 if [ "$(php -r 'echo substr(PHP_VERSION, 0, 3);')" = "5.3" ];
 then
@@ -9,4 +10,13 @@ then
 else
    $(phpenv which php) codecept.phar build
    ZEND_DONT_UNLOAD_MODULES=1 $(phpenv which php) codecept.phar run
+fi
+status_codeception=$?
+
+if [ ${status_phpunit} != 0 ] ; then
+    exit ${status_phpunit}
+fi
+
+if [ ${status_codeception} != 0 ] ; then
+    exit ${status_codeception}
 fi
