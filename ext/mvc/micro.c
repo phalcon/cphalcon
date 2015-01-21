a0f31485209ae307d5f6d7c35f8d1c899d46d102
Emil Huseynaliev <emilh1791@gmail.com>
2015-01-21 00:52:36 -0500

    Fixes fatal error in micro apps
    In Micro::handle(), issent() was being called on the return value regardless of type (if the value is an object).
    The following error was encountered when the return value's type is not an instance of Phalcon\Http\Response:
    
    Fatal error: Call to undefined function Xyz::issent()

--- ext/mvc/micro.c
+++ ext/mvc/micro.c
@@ -1064,13 +1064,15 @@ PHP_METHOD(Phalcon_Mvc_Micro, handle){
 	if (Z_TYPE_P(returned_value) == IS_OBJECT) {
 		int returned_response = instanceof_function_ex(Z_OBJCE_P(returned_value), phalcon_http_responseinterface_ce, 1 TSRMLS_CC);
 
-		PHALCON_CALL_METHOD(&returned_response_sent, returned_value, "issent");
-		
-		if (returned_response && PHALCON_IS_FALSE(returned_response_sent)) {
-			/** 
-			 * Automatically send the responses
-			 */
-			PHALCON_CALL_METHOD(NULL, returned_value, "send");
+		if (returned_response) {
+			PHALCON_CALL_METHOD(&returned_response_sent, returned_value, "issent");
+			
+			if (PHALCON_IS_FALSE(returned_response_sent)) {
+				/** 
+				 * Automatically send the responses
+				 */
+				PHALCON_CALL_METHOD(NULL, returned_value, "send");
+			}
 		}
 	}
 	
