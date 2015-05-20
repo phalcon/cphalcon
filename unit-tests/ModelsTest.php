b150daf49398d2374f4a39c812589bfbcef55694
Nemanja Ognjanovic <nemanja@ognjanovic.me>
2015-05-20 18:59:35 +0200

    indentation fix :)

--- unit-tests/ModelsTest.php
+++ unit-tests/ModelsTest.php
@@ -136,10 +136,10 @@ class ModelsTest extends PHPUnit_Framework_TestCase
 		$this->issue886($di);
 	}
 
-    public function testIssue10371()
-    {
-        $this->assertTrue(in_array('addBehavior', get_class_methods('Phalcon\Mvc\Model')));
-    }
+	public function testIssue10371()
+	{
+		$this->assertTrue(in_array('addBehavior', get_class_methods('Phalcon\Mvc\Model')));
+	}
 
 	protected function issue1534($di)
 	{
