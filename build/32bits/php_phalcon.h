27284714fd24a20a2ca69f45706e0137ca2e6eb3
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2015-05-31 13:06:51 -0500

    Regenerating build [ci skip]

--- build/32bits/php_phalcon.h
+++ build/32bits/php_phalcon.h
@@ -196,7 +196,7 @@ typedef zend_function zephir_fcall_cache_entry;
 
 
 #define PHP_PHALCON_NAME        "phalcon"
-#define PHP_PHALCON_VERSION     "2.0.2"
+#define PHP_PHALCON_VERSION     "2.0.3"
 #define PHP_PHALCON_EXTNAME     "phalcon"
 #define PHP_PHALCON_AUTHOR      "Phalcon Team and contributors"
 #define PHP_PHALCON_ZEPVERSION  "0.6.3a"
