95330c483d698e63ffc9f673c98c339cb01396d7
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2016-01-05 14:04:42 -0500

    Fixed PHP7 incompatibility

--- optimizers/PhalconFilterAlphanumOptimizer.php
+++ optimizers/PhalconFilterAlphanumOptimizer.php
@@ -63,8 +63,10 @@ class PhalconFilterAlphanumOptimizer extends OptimizerAbstract
 		$context->headersManager->add('kernel/filter');
 
 		$resolvedParams = $call->getReadOnlyResolvedParams($expression['parameters'], $context, $expression);
-		$context->codePrinter->output('zephir_filter_alphanum(' . $symbolVariable->getName() . ', ' . $resolvedParams[0] . ');');
+
+		$symbol = $context->backend->getVariableCode($symbolVariable);
+		$context->codePrinter->output('zephir_filter_alphanum(' . $symbol . ', ' . $resolvedParams[0] . ');');
+
 		return new CompiledExpression('variable', $symbolVariable->getRealName(), $expression);
 	}
-
 }
