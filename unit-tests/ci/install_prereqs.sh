40de471c12c555e1d57cf2482260c523de8a21c5
Serghei Iakovlev <sadhooklay@gmail.com>
2016-02-26 01:23:08 +0200

    Fixed Travis build

--- unit-tests/ci/install_prereqs.sh
+++ unit-tests/ci/install_prereqs.sh
@@ -1,25 +1,61 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
 DIR=$(readlink -enq $(dirname $0))
+PHP_VER=$(php -r 'echo substr(PHP_VERSION, 0, 3);');
+CFLAGS="-g0 -O0 -std=gnu90";
 
 pecl channel-update pecl.php.net
 
-if [ "$(php -r 'echo substr(PHP_VERSION, 0, 3);')" = "5.5" ]; then
-	( pecl install apcu < /dev/null || ( pecl config-set preferred_state beta; pecl install apcu < /dev/null ) && phpenv config-add "$DIR/apcu.ini" ) &
-else
-	if [ "$(php -r 'echo substr(PHP_VERSION, 0, 3);')" = "5.6" ]; then
-		( pecl install apcu < /dev/null || ( pecl config-set preferred_state beta; pecl install apcu < /dev/null ) && phpenv config-add "$DIR/apcu.ini" ) &
-	else
-		( CFLAGS="-O2 -g3 -fno-strict-aliasing" pecl upgrade apc < /dev/null; phpenv config-add "$DIR/apc.ini" ) &
-	fi
-fi
+enable_extension() {
+    if [ -z "$1" ]; then
+        return 1;
+    fi
 
-CFLAGS="-O2 -g3 -fno-strict-aliasing" pecl install igbinary < /dev/null &
-CFLAGS="-O1 -g3 -fno-strict-aliasing" pecl install imagick < /dev/null &
-CFLAGS="-O1 -g3 -fno-strict-aliasing" pecl install yaml < /dev/null &
-# CFLAGS="-O2 -g3 -fno-strict-aliasing" pecl upgrade mongo < /dev/null &
+    if [ -z $(php -m | grep $1) ]; then
+        phpenv config-add "$DIR/$1.ini"
+    fi
 
-phpenv config-add "$DIR/memcache.ini"
-phpenv config-add "$DIR/mongo.ini"
+    return 0;
+}
+
+install_extension() {
+    if [ "$2" ]; then
+        pecl config-set preferred_state beta;
+    fi
+
+    declare INSTALLED=$(pecl list $1 | grep 'not installed')
+
+    if [ -z "$1" ]; then
+        return 1;
+    fi
+
+    if [ -z "${INSTALLED}" ]; then
+        printf "\n" | pecl upgrade $1 &> /dev/null
+    else
+        printf "\n" | pecl install $1 &> /dev/null
+    fi
+
+   enable_extension $1
+
+   return 0;
+}
+
+install_extension igbinary
+install_extension imagick
+install_extension yaml
+install_extension mongo
+enable_extension memcache
+enable_extension memcached
+
+case ${PHP_VER} in
+    "5.4")
+        install_extension apc
+        ;;
+    "*")
+        install_extension apcu beta
+        ;;
+esac
+
 phpenv config-rm xdebug.ini
+
 wait
