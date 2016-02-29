a47043ace0f2e7496751fbfb8192c6e3364cb9d5
Jaskaran Singh <jaskaransingh156@gmail.com>
2016-02-29 17:30:04 +0530

    Revert "Adding addServers method to enable pool of servers for memcache"
    This reverts commit 05e027327e1bc424dfb949e178a40d996060fb53.

--- build/64bits/phalcon.zep.h
+++ build/64bits/phalcon.zep.h
@@ -7706,7 +7706,6 @@ ZEPHIR_INIT_CLASS(Phalcon_Cache_Backend_Memcache);
 
 static PHP_METHOD(Phalcon_Cache_Backend_Memcache, __construct);
 static PHP_METHOD(Phalcon_Cache_Backend_Memcache, _connect);
-static PHP_METHOD(Phalcon_Cache_Backend_Memcache, addServers);
 static PHP_METHOD(Phalcon_Cache_Backend_Memcache, get);
 static PHP_METHOD(Phalcon_Cache_Backend_Memcache, save);
 static PHP_METHOD(Phalcon_Cache_Backend_Memcache, delete);
@@ -7755,10 +7754,6 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_cache_backend_memcache_decrement, 0, 0, 0
 	ZEND_ARG_INFO(0, keyName)
 	ZEND_ARG_INFO(0, value)
 ZEND_END_ARG_INFO()
-ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_cache_backend_memcache_addServers, 0, 0, 0)
-	ZEND_ARG_INFO(0, host)
-	ZEND_ARG_INFO(0, port)
-ZEND_END_ARG_INFO()
 
 ZEPHIR_INIT_FUNCS(phalcon_cache_backend_memcache_method_entry) {
 	PHP_ME(Phalcon_Cache_Backend_Memcache, __construct, arginfo_phalcon_cache_backend_memcache___construct, ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
@@ -7766,7 +7761,6 @@ ZEPHIR_INIT_FUNCS(phalcon_cache_backend_memcache_method_entry) {
 	PHP_ME(Phalcon_Cache_Backend_Memcache, get, arginfo_phalcon_cache_backend_memcache_get, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Cache_Backend_Memcache, save, arginfo_phalcon_cache_backend_memcache_save, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Cache_Backend_Memcache, delete, arginfo_phalcon_cache_backend_memcache_delete, ZEND_ACC_PUBLIC)
-        PHP_ME(Phalcon_Cache_Backend_Memcache, addServers, arginfo_phalcon_cache_backend_memcache_addServers, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Cache_Backend_Memcache, queryKeys, arginfo_phalcon_cache_backend_memcache_querykeys, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Cache_Backend_Memcache, exists, arginfo_phalcon_cache_backend_memcache_exists, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Cache_Backend_Memcache, increment, arginfo_phalcon_cache_backend_memcache_increment, ZEND_ACC_PUBLIC)
