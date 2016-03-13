a2a29d53a2ae962ca218bab8bb68f9f8b29c0c1e
Serghei Iakovlev <sadhooklay@gmail.com>
2016-03-13 17:47:32 +0200

    Refactored Collection test

--- tests/_data/collections/Songs.php
+++ tests/_data/collections/Songs.php
@@ -4,6 +4,18 @@ namespace Phalcon\Test\Collections;
 
 use Phalcon\Mvc\Collection;
 
+/**
+ * Songs Collection
+ *
+ * @method static Songs[] find($parameters = null)
+ * @method static Songs findFirst($parameters = null)
+ *
+ * @property \MongoId _id
+ * @property string artist
+ * @property string name
+ *
+ * @package Phalcon\Test\Collections
+ */
 class Songs extends Collection
 {
 }
