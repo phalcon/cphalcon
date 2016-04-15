9e354771965b1a5779cbda1a81fb3170efecb39a
Serghei Iakovlev <sadhooklay@gmail.com>
2016-04-15 19:58:30 +0300

    Fixed Transaction Manager proxy class
    @nsossonko

--- tests/_proxies/Mvc/Model/Transaction/Manager.php
+++ tests/_proxies/Mvc/Model/Transaction/Manager.php
@@ -2,15 +2,17 @@
 
 namespace Phalcon\Test\Proxy\Mvc\Model\Transaction;
 
+use Phalcon\DiInterface;
+use Phalcon\Mvc\Model\TransactionInterface;
 use Phalcon\Mvc\Model\Transaction\Manager as PhManager;
 
 /**
- * \Phalcon\Test\Proxy\Mvc\Manager
- * Url proxy class for \Phalcon\Mvc\Manager
+ * \Phalcon\Test\Proxy\Mvc\Model\Transaction\Manager
+ * Transaction Manager proxy class for \Phalcon\Mvc\Model\Transaction\Manager
  *
  * @copyright (c) 2011-2016 Phalcon Team
  * @link      http://www.phalconphp.com
- * @author    Nochum Sossonko <nsossonko@hotmail.com>
+ * @author    Serghei Iakovlev <serghei@phalconphp.com>
  * @package   Phalcon\Test\Proxy\Mvc\Model\Transaction
  *
  * The contents of this file are subject to the New BSD License that is
@@ -22,13 +24,83 @@ use Phalcon\Mvc\Model\Transaction\Manager as PhManager;
  */
 class Manager extends PhManager
 {
-    public function getTransactions()
+    public function __construct(DiInterface $dependencyInjector = null)
     {
-      return $this->_transactions;
+        parent::__construct($dependencyInjector);
     }
 
-    public function count()
+    public function setDI(DiInterface $dependencyInjector)
     {
-      return $this->_number;
+        parent::setDI($dependencyInjector);
+    }
+
+    public function getDI()
+    {
+        return parent::getDI();
+    }
+
+    public function setDbService($service)
+    {
+        return parent::setDbService($service);
+    }
+
+    public function getDbService()
+    {
+        return parent::getDbService();
+    }
+
+    public function setRollbackPendent($rollbackPendent)
+    {
+        return parent::setRollbackPendent($rollbackPendent);
+    }
+
+    public function getRollbackPendent()
+    {
+        return parent::getRollbackPendent();
+    }
+
+    public function has()
+    {
+        return parent::has();
+    }
+
+    public function get($autoBegin = true)
+    {
+        return parent::get($autoBegin);
+    }
+
+    public function getOrCreateTransaction($autoBegin = true)
+    {
+        return parent::getOrCreateTransaction($autoBegin);
+    }
+
+    public function rollbackPendent()
+    {
+        parent::rollbackPendent();
+    }
+
+    public function commit()
+    {
+        parent::commit();
+    }
+
+    public function rollback($collect = true)
+    {
+        parent::rollback($collect);
+    }
+
+    public function notifyRollback(TransactionInterface $transaction)
+    {
+        parent::notifyRollback($transaction);
+    }
+
+    public function notifyCommit(TransactionInterface $transaction)
+    {
+        parent::notifyCommit($transaction);
+    }
+
+    public function collectTransactions()
+    {
+        parent::collectTransactions();
     }
 }
