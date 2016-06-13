9afaf8dd8724bff2249559c00bb6481000eb4547
Serghei Iakovlev <serghei@phalconphp.com>
2016-06-13 22:29:41 +0300

    Trying to install apcu on PHP 5.6

--- tests/_ci/install_prereqs.sh
+++ tests/_ci/install_prereqs.sh
@@ -56,7 +56,7 @@ if [ ${TRAVIS_PHP_VERSION} == "5.4" ]; then
 elif [ ${TRAVIS_PHP_VERSION} == "7.0" ]; then
     ( mkdir -p /tmp/apcu && cd /tmp/apcu && git clone https://github.com/krakjoe/apcu /tmp/apcu && phpize && ./configure && make -j 4 && sudo make install && phpenv config-add "$DIR/apcu.ini" ) &
 else
-    ( pecl install apcu &> /dev/null && phpenv config-add "$DIR/apcu.ini" ) &
+    ( pecl install apcu-4.0.11 &> /dev/null && phpenv config-add "$DIR/apcu.ini" ) &
 fi
 
 wait
