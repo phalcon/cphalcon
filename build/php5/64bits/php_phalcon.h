6dae11452aa9e8f43d42cba2a2d2ab90bcafb469
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2016-06-27 09:30:44 -0500

    Regenerating builds [ci skip]

--- build/php5/64bits/php_phalcon.h
+++ build/php5/64bits/php_phalcon.h
@@ -185,7 +185,7 @@ typedef zend_function zephir_fcall_cache_entry;
 #define PHP_PHALCON_VERSION     "2.1.0r"
 #define PHP_PHALCON_EXTNAME     "phalcon"
 #define PHP_PHALCON_AUTHOR      "Phalcon Team and contributors"
-#define PHP_PHALCON_ZEPVERSION  "0.9.2a-dev"
+#define PHP_PHALCON_ZEPVERSION  "0.9.3a-dev-5c0fb106a6"
 #define PHP_PHALCON_DESCRIPTION "Web framework delivered as a C-extension for PHP"
 
 typedef struct _zephir_struct_db { 
