a7b1c817576c65447c984420769dca50a9996f80
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2016-08-23 21:44:30 -0500

    Generating php7 build [ci skip]

--- build/php7/64bits/php_phalcon.h
+++ build/php7/64bits/php_phalcon.h
@@ -154,10 +154,10 @@ typedef zend_function zephir_fcall_cache_entry;
 
 
 #define PHP_PHALCON_NAME        "phalcon"
-#define PHP_PHALCON_VERSION     "3.0.0"
+#define PHP_PHALCON_VERSION     "3.0.1"
 #define PHP_PHALCON_EXTNAME     "phalcon"
 #define PHP_PHALCON_AUTHOR      "Phalcon Team and contributors"
-#define PHP_PHALCON_ZEPVERSION  "0.9.3a-dev-e716dbe641"
+#define PHP_PHALCON_ZEPVERSION  "0.9.4a-dev-7e304ba18c"
 #define PHP_PHALCON_DESCRIPTION "Web framework delivered as a C-extension for PHP"
 
 typedef struct _zephir_struct_db { 
