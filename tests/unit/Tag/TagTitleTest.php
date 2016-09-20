1780843f8b475f73487177f9fa599b54da6313e4
Boris Delev <butamuh4o@gmail.com>
2016-09-20 07:24:27 +0300

    Fix new line in test for setTitle

--- tests/unit/Tag/TagTitleTest.php
+++ tests/unit/Tag/TagTitleTest.php
@@ -64,7 +64,7 @@ class TagTitleTest extends UnitTest
 
                 expect(Tag::getTitle())->equals("<title>{$value}</title>" . PHP_EOL);
                 
-                expect(Tag::getTitle(false))->equals("{$value}" . PHP_EOL);
+                expect(Tag::getTitle(false))->equals("{$value}");
             }
         );
     }
