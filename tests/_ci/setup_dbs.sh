002f642953f3f9c6fe1a5d6399531f1545f96887
Serghei Iakovlev <serghei@phalconphp.com>
2017-04-09 15:39:12 +0300

    Fixed tests/_ci/setup_dbs.sh

--- tests/_ci/setup_dbs.sh
+++ tests/_ci/setup_dbs.sh
@@ -14,9 +14,9 @@
 CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-$(dirname $(dirname $CURRENT_DIR))}"
 
-source ${TRAVIS_BUILD_DIR}/tests/_ci/install_common.sh
+mysql_db="CREATE DATABASE IF NOT EXISTS phalcon_test charset=utf8mb4 collate=utf8mb4_unicode_ci;"
 
-(mysql -uroot -e 'create database phalcon_test charset=utf8mb4 collate=utf8mb4_unicode_ci;' && mysql -uroot phalcon_test < "${TRAVIS_BUILD_DIR}/tests/_data/schemas/mysql/mysql.dump.sql") &
+(mysql -uroot -e "${mysql_db}" && mysql -uroot phalcon_test < "${TRAVIS_BUILD_DIR}/tests/_data/schemas/mysql/mysql.dump.sql") &
 (psql -c 'create database phalcon_test;' -U postgres && psql -U postgres phalcon_test -q -f "${TRAVIS_BUILD_DIR}/tests/_data/schemas/postgresql/phalcon_test.sql") &
 sqlite3 /tmp/phalcon_test.sqlite < "${TRAVIS_BUILD_DIR}/tests/_data/schemas/sqlite/phalcon_test.sql"
 
