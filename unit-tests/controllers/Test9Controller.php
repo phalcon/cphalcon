99ac4a679a2567bc34c04a59c0149416cafb2011
Dongasai <1514582970@qq.com>
2017-04-25 01:18:10 +0800

    2017年4月25日01:18:07

---
+++ unit-tests/controllers/Test9Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+class Test9Controller extends Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+        global $noCallAction_value;
+        $noCallAction_value = 14;
+    }
+
+    /**
+     * noCallAction
+     */
+    public function noCallAction()
+    {
+        global $noCallAction_value;
+        $noCallAction_value = 24;
+    }
+
+}
