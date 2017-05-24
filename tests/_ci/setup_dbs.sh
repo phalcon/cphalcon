b01fa86e7cd3e4e9d799aa74aae8f34f4f66f83a
Serghei Iakovlev <serghei@phalconphp.com>
2017-05-24 02:24:47 +0200

    Update test & build process
    Create databases before install
    
    Cleaned setup_dbs.sh
    
    Use latest stable Codeception
    
    Fixed setup_dbs.sh
    
    Clean .travis.yml
    
    Play with opcache fast_shutdown
    
    (cherry picked from commit ec29fe8)

--- tests/_ci/setup_dbs.sh
+++ tests/_ci/setup_dbs.sh
@@ -14,10 +14,18 @@
 CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-$(dirname $(dirname $CURRENT_DIR))}"
 
-mysql_db="CREATE DATABASE IF NOT EXISTS phalcon_test charset=utf8mb4 collate=utf8mb4_unicode_ci;"
+echo -e "Create MySQL database..."
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS phalcon_test charset=utf8mb4 collate=utf8mb4_unicode_ci;"
+cat "${TRAVIS_BUILD_DIR}/tests/_data/schemas/mysql/phalcon_test.sql" | mysql -u root phalcon_test
+echo -e "Done\n"
 
-(mysql -uroot -e "${mysql_db}" && mysql -uroot phalcon_test < "${TRAVIS_BUILD_DIR}/tests/_data/schemas/mysql/mysql.dump.sql") &
-(psql -c 'create database phalcon_test;' -U postgres && psql -U postgres phalcon_test -q -f "${TRAVIS_BUILD_DIR}/tests/_data/schemas/postgresql/phalcon_test.sql") &
+echo -e "Create PostgreSQL database..."
+psql -c 'create database phalcon_test;' -U postgres
+psql -U postgres phalcon_test -q -f "${TRAVIS_BUILD_DIR}/tests/_data/schemas/postgresql/phalcon_test.sql"
+echo -e "Done\n"
+
+echo -e "Create SQLite database..."
 sqlite3 /tmp/phalcon_test.sqlite < "${TRAVIS_BUILD_DIR}/tests/_data/schemas/sqlite/phalcon_test.sql"
+echo -e "Done\n"
 
 wait
