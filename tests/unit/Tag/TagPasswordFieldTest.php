29669a110c3cf2ffd084f628a9cb1c3c97ed7ba7
Serghei Iakovlev <serghei@phalconphp.com>
2017-06-18 14:09:47 +0300

    Updated tests' docblock

--- tests/unit/Tag/TagPasswordFieldTest.php
+++ tests/unit/Tag/TagPasswordFieldTest.php
@@ -15,7 +15,7 @@ use Phalcon\Test\Module\UnitTest;
  * @package   Phalcon\Test\Unit\Tag
  *
  * The contents of this file are subject to the New BSD License that is
- * bundled with this package in the file docs/LICENSE.txt
+ * bundled with this package in the file LICENSE.txt
  *
  * If you did not receive a copy of the license and are unable to obtain it
  * through the world-wide-web, please send an email to license@phalconphp.com
