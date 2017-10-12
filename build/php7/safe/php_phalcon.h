2490839a7e8164d59c3fc750d52e11b5dbb4830d
Serghei Iakovlev <serghei@phalconphp.com>
2017-10-12 20:08:45 +0300

    Regenerated build (PHP7)

--- build/php7/safe/php_phalcon.h
+++ build/php7/safe/php_phalcon.h
@@ -132,10 +132,10 @@ typedef zend_function zephir_fcall_cache_entry;
 
 
 #define PHP_PHALCON_NAME        "phalcon"
-#define PHP_PHALCON_VERSION     "3.2.2"
+#define PHP_PHALCON_VERSION     "3.2.3"
 #define PHP_PHALCON_EXTNAME     "phalcon"
 #define PHP_PHALCON_AUTHOR      "Phalcon Team and contributors"
-#define PHP_PHALCON_ZEPVERSION  "0.9.9-868cb1f92b"
+#define PHP_PHALCON_ZEPVERSION  "0.10.3-7f475126c7"
 #define PHP_PHALCON_DESCRIPTION "Web framework delivered as a C-extension for PHP"
 
 typedef struct _zephir_struct_db { 
