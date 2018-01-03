cf136fa03599f31fbf0b4d513dbd8bf2bc2c4b01
Serghei Iakovlev <serghei@phalconphp.com>
2018-01-03 16:22:21 +0200

    Cleaned Volt Compiler tests

--- tests/unit/Mvc/View/Engine/Volt/Compiler/Statements/SwitchCaseTest.php
+++ tests/unit/Mvc/View/Engine/Volt/Compiler/Statements/SwitchCaseTest.php
@@ -4,21 +4,15 @@ namespace Phalcon\Test\Unit\Mvc\View\Engine\Volt\Compiller\Statements;
 
 use Phalcon\Test\Module\UnitTest;
 use Phalcon\Mvc\View\Engine\Volt\Compiler;
+use Phalcon\Test\Unit\Mvc\View\Engine\Volt\CompilerTrait;
 
 class SwitchCaseTest extends UnitTest
 {
-    protected function _before()
-    {
-        $cache = [
-            'views/switch-case/simple-usage.volt.php',
-        ];
+    use CompilerTrait;
 
-        foreach ($cache as $file) {
-            if (file_exists(PATH_DATA . $file)) {
-                unlink(PATH_DATA . $file);
-            }
-        }
-    }
+    protected $cache = [
+        'views/filters/default.volt.php',
+    ];
 
     /** @test */
     public function shouldRecognizeEmptySwitch()
@@ -28,23 +22,19 @@ class SwitchCaseTest extends UnitTest
             function () {
                 $volt = new Compiler();
 
-                $volt->compileFile(
-                    PATH_DATA . 'views/switch-case/simple-usage.volt',
-                    PATH_DATA . 'views/switch-case/simple-usage.volt.php'
-                );
+                $view = env('PATH_DATA') . 'views/switch-case/simple-usage.volt';
+                $volt->compileFile($view, $view . '.php');
 
-                $compilation = file_get_contents(PATH_DATA . 'views/switch-case/simple-usage.volt.php');
-                $expected =<<<TPL
-<?php switch (\$username): ?>
-<?php case 'Jim': ?>
-Hello username
-<?php case 'Nik': ?>
-<?= \$username ?>!
-<?php break; ?>
-<?php default: ?>
-Who are you?
-<?php endswitch ?>\n
-TPL;
+                $compilation = file_get_contents($view . '.php');
+                $expected = "<?php switch (\$username): ?>\n" .
+                            "<?php case 'Jim': ?>\n" .
+                            "Hello username\n" .
+                            "<?php case 'Nik': ?>\n" .
+                            "<?= \$username ?>!\n" .
+                            "<?php break; ?>\n" .
+                            "<?php default: ?>\n" .
+                            "Who are you?\n" .
+                            "<?php endswitch ?>\n";
 
                 expect($compilation)->same($expected);
             }
