e7a092ae7ff33a8a6b47fbf5c8b4f1f49af36a5a
Serghei Iakovlev <serghei@phalconphp.com>
2018-03-25 13:39:56 +0300

    Added request events to provide possibility using custom authorization resolvers

---
+++ tests/_data/listener/NegotiateAuthorizationListener.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Phalcon\Test\Listener;
+
+use Phalcon\Events\Event;
+use Phalcon\Http\Request;
+
+/**
+ * Phalcon\Test\Listener\NegotiateAuthorizationListener
+ *
+ * @copyright (c) 2011-2018 Phalcon Team
+ * @link      https://www.phalconphp.com
+ * @author    Andres Gutierrez <andres@phalconphp.com>
+ * @author    Serghei Iakovlev <serghei@phalconphp.com>
+ * @package   Phalcon\Test\Listener
+ *
+ * The contents of this file are subject to the New BSD License that is
+ * bundled with this package in the file LICENSE.txt
+ *
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world-wide-web, please send an email to license@phalconphp.com
+ * so that we can send you a copy immediately.
+ */
+class NegotiateAuthorizationListener
+{
+    public function afterAuthorizationResolve(Event $event, Request $request, array $data)
+    {
+        if (empty($data['server']['CUSTOM_KERBEROS_AUTH'])) {
+            return false;
+        }
+
+        list($type,) = explode(' ', $data['server']['CUSTOM_KERBEROS_AUTH'], 2);
+
+        if (!$type || stripos($type, 'negotiate') !== 0) {
+            return false;
+        }
+
+        return [
+           'Authorization'=> $data['server']['CUSTOM_KERBEROS_AUTH'],
+        ];
+    }
+}
