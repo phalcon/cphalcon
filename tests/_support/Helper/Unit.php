d7db8df486f84dff7ba93a381495fcb64944263d
Serghei Iakovlev <serghei@phalconphp.com>
2018-03-26 22:25:07 +0300

    Test Dump::output

--- tests/_support/Helper/Unit.php
+++ tests/_support/Helper/Unit.php
@@ -57,6 +57,33 @@ class Unit extends Module
     }
 
     /**
+     * Calls private or protected method.
+     *
+     * @param string|object $obj
+     * @param mixed         $method,... Method with a variable number of arguments
+     *
+     * @return mixed
+     * @throws \ReflectionException
+     */
+    public function callProtectedMethod($obj, $method)
+    {
+        $reflectionClass = new ReflectionClass($obj);
+
+        $reflectionMethod = $reflectionClass->getMethod($method);
+        $reflectionMethod->setAccessible(true);
+
+        if (!is_object($obj)) {
+            $obj = $reflectionClass->newInstanceWithoutConstructor();
+        }
+
+        // $obj, $method
+        $args = array_slice(func_get_args(), 2);
+        array_unshift($args, $obj);
+
+        return call_user_func_array([$reflectionMethod, 'invoke'], $args);
+    }
+
+    /**
      * Returns a unique file name
      *
      * @author Nikos Dimopoulos <nikos@phalconphp.com>
