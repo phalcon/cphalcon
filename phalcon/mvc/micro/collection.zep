phalcon/cphalcon

commit 7fbc09253ef1d67414fa028cd5188b9f10da5bf5

Strengthened parameter/return types.

phalcon/mvc/micro/collection.zep
@@ -51,13 +51,8 @@ class Collection implements CollectionInterface
 
 	/**
 	 * Internal function to add a handler to the group
-	 *
-	 * @param string|array method
-	 * @param string routePattern
-	 * @param mixed handler
-	 * @param string name
 	 */
-	protected function _addMap(var method, var routePattern, var handler, var name)
+	protected function _addMap(string! method, string! routePattern, callable handler, string name)
 	{
 		let this->_handlers[] = [method, routePattern, handler, name];
 	}
@@ -81,20 +76,14 @@ class Collection implements CollectionInterface
 
 	/**
 	 * Returns the registered handlers
-	 *
-	 * @return array
 	 */
-	public function getHandlers()
+	public function getHandlers() -> array
 	{
 		return this->_handlers;
 	}
 
 	/**
 	 * Sets the main handler
-	 *
-	 * @param mixed handler
-	 * @param boolean lazy
-	 * @return \Phalcon\Mvc\Micro\Collection
 	 */
 	public function setHandler(var handler, boolean lazy = false) -> <Collection>
 	{
@@ -121,23 +110,16 @@ class Collection implements CollectionInterface
 
 	/**
 	 * Returns the main handler
-	 *
-	 * @return mixed
 	 */
-	public function getHandler()
+	public function getHandler() -> var
 	{
 		return this->_handler;
 	}
 
 	/**
 	 * Maps a route to a handler
-	 *
-	 * @param  string routePattern
-	 * @param  callable handler
-	 * @param  string name
-	 * @return \Phalcon\Mvc\Micro\Collection
 	 */
-	public function map(string! routePattern, var handler, var name = null) -> <Collection>
+	public function map(string! routePattern, callable handler, string name = null) -> <Collection>
 	{
 		this->_addMap(null, routePattern, handler, name);
 		return this;
@@ -161,13 +143,8 @@ class Collection implements CollectionInterface
 
 	/**
 	 * Maps a route to a handler that only matches if the HTTP method is GET
-	 *
-	 * @param  string routePattern
-	 * @param  callable handler
-	 * @param  string name
-	 * @return \Phalcon\Mvc\Micro\Collection
 	 */
-	public function get(string! routePattern, handler, var name = null) -> <Collection>
+	public function get(string! routePattern, callable handler, string name = null) -> <Collection>
 	{
 		this->_addMap("GET", routePattern, handler, name);
 		return this;
@@ -175,13 +152,8 @@ class Collection implements CollectionInterface
 
 	/**
 	 * Maps a route to a handler that only matches if the HTTP method is POST
-	 *
-	 * @param  string routePattern
-	 * @param  callable handler
-	 * @param  string name
-	 * @return \Phalcon\Mvc\Micro\Collection
 	 */
-	public function post(string! routePattern, handler, var name = null) -> <Collection>
+	public function post(string! routePattern, callable handler, string name = null) -> <Collection>
 	{
 		this->_addMap("POST", routePattern, handler, name);
 		return this;
@@ -189,13 +161,8 @@ class Collection implements CollectionInterface
 
 	/**
 	 * Maps a route to a handler that only matches if the HTTP method is PUT
-	 *
-	 * @param  string routePattern
-	 * @param  callable handler
-	 * @param  string name
-	 * @return \Phalcon\Mvc\Micro\Collection
 	 */
-	public function put(string! routePattern, var handler, var name = null) -> <Collection>
+	public function put(string! routePattern, callable handler, string name = null) -> <Collection>
 	{
 		this->_addMap("PUT", routePattern, handler, name);
 		return this;
@@ -203,13 +170,8 @@ class Collection implements CollectionInterface
 
 	/**
 	 * Maps a route to a handler that only matches if the HTTP method is PATCH
-	 *
-	 * @param  string routePattern
-	 * @param  callable handler
-	 * @param  string name
-	 * @return \Phalcon\Mvc\Micro\Collection
 	 */
-	public function patch(string! routePattern, var handler, var name = null) -> <Collection>
+	public function patch(string! routePattern, callable handler, string name = null) -> <Collection>
 	{
 		this->_addMap("PATCH", routePattern, handler, name);
 		return this;
@@ -217,13 +179,8 @@ class Collection implements CollectionInterface
 
 	/**
 	 * Maps a route to a handler that only matches if the HTTP method is HEAD
-	 *
-	 * @param  string routePattern
-	 * @param  callable handler
-	 * @param  string name
-	 * @return \Phalcon\Mvc\Micro\Collection
 	 */
-	public function head(string! routePattern, var handler, var name = null) -> <Collection>
+	public function head(string! routePattern, callable handler, string name = null) -> <Collection>
 	{
 		this->_addMap("HEAD", routePattern, handler, name);
 		return this;
@@ -231,13 +188,8 @@ class Collection implements CollectionInterface
 
 	/**
 	 * Maps a route to a handler that only matches if the HTTP method is DELETE
-	 *
-	 * @param  string   routePattern
-	 * @param  callable handler
-	 * @param  string name
-	 * @return \Phalcon\Mvc\Micro\Collection
 	 */
-	public function delete(string! routePattern, var handler, var name = null) -> <Collection>
+	public function delete(string! routePattern, callable handler, string name = null) -> <Collection>
 	{
 		this->_addMap("DELETE", routePattern, handler, name);
 		return this;
@@ -245,12 +197,8 @@ class Collection implements CollectionInterface
 
 	/**
 	 * Maps a route to a handler that only matches if the HTTP method is OPTIONS
-	 *
-	 * @param string routePattern
-	 * @param callable handler
-	 * @return \Phalcon\Mvc\Micro\Collection
 	 */
-	public function options(string! routePattern, handler, var name = null) -> <Collection>
+	public function options(string! routePattern, callable handler, string name = null) -> <Collection>
 	{
 		this->_addMap("OPTIONS", routePattern, handler, name);
 		return this;

phalcon/annotations/collection.zep
@@ -52,20 +52,16 @@ class Collection implements \Iterator, \Countable
 	 *
 	 * @param array reflectionData
 	 */
-	public function __construct(var reflectionData = null)
+	public function __construct(array reflectionData = [])
 	{
 		var annotations, annotationData;
 
-		if typeof reflectionData != "null" && typeof reflectionData != "array" {
-			throw new Exception("Reflection data must be an array");
+		let annotations = [];
+
+		for annotationData in reflectionData {
+			let annotations[] = new Annotation(annotationData);
 		}
 
-		let annotations = [];
-		if typeof reflectionData == "array" {
-			for annotationData in reflectionData {
-				let annotations[] = new Annotation(annotationData);
-			}
-		}
 		let this->_annotations = annotations;
 	}
 
@@ -87,8 +83,6 @@ class Collection implements \Iterator, \Countable
 
 	/**
 	 * Returns the current annotation in the iterator
-	 *
-	 * @return \Phalcon\Annotations\Annotation
 	 */
 	public function current() -> <Annotation> | boolean
 	{

phalcon/assets/collection.zep
@@ -156,13 +156,9 @@ class Collection implements \Countable, \Iterator
 	/**
 	 * Adds a javascript resource to the collection
 	 *
-	 * @param string path
-	 * @param boolean local
-	 * @param boolean filter
 	 * @param array attributes
-	 * @return \Phalcon\Assets\Collection
 	 */
-	public function addJs(string! path, var local = null, boolean filter = true, attributes = null) -> <Collection>
+	public function addJs(string! path, boolean local = null, boolean filter = true, attributes = null) -> <Collection>
 	{
 		var collectionLocal, collectionAttributes;
 
@@ -227,10 +223,8 @@ class Collection implements \Countable, \Iterator
 
 	/**
 	 * Returns the current position/key in the iterator
-	 *
-	 * @return int
 	 */
-	public function key()
+	public function key() -> int
 	{
 		return this->_position;
 	}

phalcon/mvc/collection.zep
@@ -291,11 +291,8 @@ abstract class Collection implements EntityInterface, CollectionInterface, Injec
 	 *<code>
 	 *	echo $robot->readAttribute("name");
 	 *</code>
-	 *
-	 * @param string attribute
-	 * @return mixed
 	 */
-	public function readAttribute(string! attribute)
+	public function readAttribute(string! attribute) -> var | null
 	{
 		if !isset this->{attribute} {
 			return null;
@@ -310,9 +307,6 @@ abstract class Collection implements EntityInterface, CollectionInterface, Injec
 	 *<code>
 	 *	$robot->writeAttribute("name", "Rosey");
 	 *</code>
-	 *
-	 * @param string attribute
-	 * @param mixed value
 	 */
 	public function writeAttribute(string attribute, var value)
 	{
@@ -342,9 +336,7 @@ abstract class Collection implements EntityInterface, CollectionInterface, Injec
 	 * Returns a collection resultset
 	 *
 	 * @param array params
-	 * @param \Phalcon\Mvc\Collection collection
 	 * @param \MongoDb connection
-	 * @param boolean unique
 	 * @return array
 	 */
 	protected static function _getResultset(var params, <CollectionInterface> collection, connection, boolean unique)
@@ -463,9 +455,7 @@ abstract class Collection implements EntityInterface, CollectionInterface, Injec
 	 * Perform a count over a resultset
 	 *
 	 * @param array params
-	 * @param \Phalcon\Mvc\Collection collection
 	 * @param \MongoDb connection
-	 * @return int
 	 */
 	protected static function _getGroupResultset(params, <Collection> collection, connection) -> int
 	{
@@ -526,13 +516,8 @@ abstract class Collection implements EntityInterface, CollectionInterface, Injec
 
 	/**
 	 * Executes internal hooks before save a document
-	 *
-	 * @param \Phalcon\DiInterface dependencyInjector
-	 * @param boolean disableEvents
-	 * @param boolean exists
-	 * @return boolean
 	 */
-	protected final function _preSave(dependencyInjector, boolean disableEvents, boolean exists) -> boolean
+	protected final function _preSave(<DiInterface> dependencyInjector, boolean disableEvents, boolean exists) -> boolean
 	{
 		var eventName;
 
@@ -828,7 +813,6 @@ abstract class Collection implements EntityInterface, CollectionInterface, Injec
 	 * Checks if the document exists in the collection
 	 *
 	 * @param \MongoCollection collection
-	 * @return boolean
 	 */
 	protected function _exists(collection) -> boolean
 	{

commit f814f4bb2bea3eadd49711974671545edcf69208

Fixed regression changes from 7fbc092

phalcon/mvc/micro/collection.zep
@@ -50,9 +50,12 @@ class Collection implements CollectionInterface
 	protected _handlers;
 
 	/**
-	 * Internal function to add a handler to the group
+	 * Internal function to add a handler to the group.
+	 *
+	 * @param string|array method
+	 * @param callable|string handler
 	 */
-	protected function _addMap(string! method, string! routePattern, callable handler, string name)
+	protected function _addMap(var method, string! routePattern, var handler, string name)
 	{
 		let this->_handlers[] = [method, routePattern, handler, name];
 	}
@@ -83,7 +86,9 @@ class Collection implements CollectionInterface
 	}
 
 	/**
-	 * Sets the main handler
+	 * Sets the main handler.
+	 *
+	 * @param callable|string handler
 	 */
 	public function setHandler(var handler, boolean lazy = false) -> <Collection>
 	{
@@ -117,24 +122,27 @@ class Collection implements CollectionInterface
 	}
 
 	/**
-	 * Maps a route to a handler
+	 * Maps a route to a handler.
+	 *
+	 * @param callable|string handler
 	 */
-	public function map(string! routePattern, callable handler, string name = null) -> <Collection>
+	public function map(string! routePattern, var handler, string name = null) -> <Collection>
 	{
 		this->_addMap(null, routePattern, handler, name);
 		return this;
 	}
 
 	/**
-	 * Maps a route to a handler via methods
+	 * Maps a route to a handler via methods.
 	 *
-	 * @param  string routePattern
-	 * @param  callable handler
-	 * @param  string|array method
-	 * @param  string name
-	 * @return \Phalcon\Mvc\Micro\Collection
+	 * <code>
+	 * $collection->mapVia("/test", "indexAction", ["POST", "GET"], "test");
+	 * </code>
+	 *
+	 * @param callable handler
+	 * @param string|array method
 	 */
-	public function mapVia(string! routePattern, var handler, var method, var name = null) -> <Collection>
+	public function mapVia(string! routePattern, var handler, var method, string name = null) -> <Collection>
 	{
 		this->_addMap(method, routePattern, handler, name);
 
@@ -142,63 +150,77 @@ class Collection implements CollectionInterface
 	}
 
 	/**
-	 * Maps a route to a handler that only matches if the HTTP method is GET
+	 * Maps a route to a handler that only matches if the HTTP method is GET.
+	 *
+	 * @param callable|string handler
 	 */
-	public function get(string! routePattern, callable handler, string name = null) -> <Collection>
+	public function get(string! routePattern, var handler, string name = null) -> <Collection>
 	{
 		this->_addMap("GET", routePattern, handler, name);
 		return this;
 	}
 
 	/**
-	 * Maps a route to a handler that only matches if the HTTP method is POST
+	 * Maps a route to a handler that only matches if the HTTP method is POST.
+	 *
+	 * @param callable|string handler
 	 */
-	public function post(string! routePattern, callable handler, string name = null) -> <Collection>
+	public function post(string! routePattern, var handler, string name = null) -> <Collection>
 	{
 		this->_addMap("POST", routePattern, handler, name);
 		return this;
 	}
 
 	/**
-	 * Maps a route to a handler that only matches if the HTTP method is PUT
+	 * Maps a route to a handler that only matches if the HTTP method is PUT.
+	 *
+	 * @param callable|string handler
 	 */
-	public function put(string! routePattern, callable handler, string name = null) -> <Collection>
+	public function put(string! routePattern, var handler, string name = null) -> <Collection>
 	{
 		this->_addMap("PUT", routePattern, handler, name);
 		return this;
 	}
 
 	/**
-	 * Maps a route to a handler that only matches if the HTTP method is PATCH
+	 * Maps a route to a handler that only matches if the HTTP method is PATCH.
+	 *
+	 * @param callable|string handler
 	 */
-	public function patch(string! routePattern, callable handler, string name = null) -> <Collection>
+	public function patch(string! routePattern, var handler, string name = null) -> <Collection>
 	{
 		this->_addMap("PATCH", routePattern, handler, name);
 		return this;
 	}
 
 	/**
-	 * Maps a route to a handler that only matches if the HTTP method is HEAD
+	 * Maps a route to a handler that only matches if the HTTP method is HEAD.
+	 *
+	 * @param callable|string handler
 	 */
-	public function head(string! routePattern, callable handler, string name = null) -> <Collection>
+	public function head(string! routePattern, var handler, string name = null) -> <Collection>
 	{
 		this->_addMap("HEAD", routePattern, handler, name);
 		return this;
 	}
 
 	/**
-	 * Maps a route to a handler that only matches if the HTTP method is DELETE
+	 * Maps a route to a handler that only matches if the HTTP method is DELETE.
+	 *
+	 * @param callable|string handler
 	 */
-	public function delete(string! routePattern, callable handler, string name = null) -> <Collection>
+	public function delete(string! routePattern, var handler, string name = null) -> <Collection>
 	{
 		this->_addMap("DELETE", routePattern, handler, name);
 		return this;
 	}
 
 	/**
-	 * Maps a route to a handler that only matches if the HTTP method is OPTIONS
+	 * Maps a route to a handler that only matches if the HTTP method is OPTIONS.
+	 *
+	 * @param callable|string handler
 	 */
-	public function options(string! routePattern, callable handler, string name = null) -> <Collection>
+	public function options(string! routePattern, var handler, string name = null) -> <Collection>
 	{
 		this->_addMap("OPTIONS", routePattern, handler, name);
 		return this;