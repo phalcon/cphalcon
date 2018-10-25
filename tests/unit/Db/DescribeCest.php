56ed56b1bf7e123608a3576622787cd5b3939215
Nikolaos Dimopoulos <nikos@niden.net>
2018-10-25 15:20:10 -0400

    [#13543] - Added more assertions

--- tests/unit/Db/DescribeCest.php
+++ tests/unit/Db/DescribeCest.php
@@ -28,12 +28,23 @@ class DescribeCest
         }
     }
 
-    public function checkColumnNames(\UnitTester $I)
+    public function checkTableExists(\UnitTester $I)
     {
         $table = 'dialect_table';
         $I->assertTrue($this->connection->tableExists($table));
+        $I->assertFalse($this->connection->tableExists('unknown-table'));
+        $I->assertTrue($this->connection->tableExists($table, TEST_DB_MYSQL_NAME));
+        $I->assertFalse($this->connection->tableExists('unknown-table', 'unknown-db'));
+    }
+
+    public function checkColumnNames(\UnitTester $I)
+    {
+        $table = 'dialect_table';
         $expected = $this->getExpectedColumns();
         $I->assertEquals($expected, $this->connection->describeColumns($table));
+
+        $expected = $this->getExpectedColumns();
+        $I->assertEquals($expected, $this->connection->describeColumns($table, TEST_DB_MYSQL_NAME));
     }
 
     private function getExpectedColumns(): array
@@ -654,193 +665,8 @@ class DescribeCest
         ];
     }
 
-//    public function getExpectedColumnsMysql()
-//    {
-//        return [
-//            0 => Phalcon\Db\Column::__set_state([
-//                '_columnName' => 'cedula',
-//                '_schemaName' => NULL,
-//                '_type' => 5,
-//                '_isNumeric' => false,
-//                '_size' => 15,
-//                '_scale' => 0,
-//                '_default' => NULL,
-//                '_unsigned' => false,
-//                '_notNull' => true,
-//                '_autoIncrement' => false,
-//                '_primary' => true,
-//                '_first' => true,
-//                '_after' => NULL,
-//                '_bindType' => 2,
-//            ]),
-//            1 => Phalcon\Db\Column::__set_state([
-//                '_columnName' => 'tipo_documento_id',
-//                '_schemaName' => NULL,
-//                '_type' => 0,
-//                '_isNumeric' => true,
-//                '_size' => 3,
-//                '_scale' => 0,
-//                '_default' => NULL,
-//                '_unsigned' => true,
-//                '_notNull' => true,
-//                '_autoIncrement' => false,
-//                '_first' => false,
-//                '_after' => 'cedula',
-//                '_bindType' => 1,
-//            ]),
-//            2 => Phalcon\Db\Column::__set_state([
-//                '_columnName' => 'nombres',
-//                '_schemaName' => NULL,
-//                '_type' => 2,
-//                '_isNumeric' => false,
-//                '_size' => 100,
-//                '_scale' => 0,
-//                '_default' => '',
-//                '_unsigned' => false,
-//                '_notNull' => true,
-//                '_autoIncrement' => false,
-//                '_first' => false,
-//                '_after' => 'tipo_documento_id',
-//                '_bindType' => 2,
-//            ]),
-//            3 => Phalcon\Db\Column::__set_state([
-//                '_columnName' => 'telefono',
-//                '_schemaName' => NULL,
-//                '_type' => 2,
-//                '_isNumeric' => false,
-//                '_size' => 20,
-//                '_scale' => 0,
-//                '_default' => NULL,
-//                '_unsigned' => false,
-//                '_notNull' => false,
-//                '_autoIncrement' => false,
-//                '_first' => false,
-//                '_after' => 'nombres',
-//                '_bindType' => 2,
-//            ]),
-//            4 => Phalcon\Db\Column::__set_state([
-//                '_columnName' => 'direccion',
-//                '_schemaName' => NULL,
-//                '_type' => 2,
-//                '_isNumeric' => false,
-//                '_size' => 100,
-//                '_scale' => 0,
-//                '_default' => NULL,
-//                '_unsigned' => false,
-//                '_notNull' => false,
-//                '_autoIncrement' => false,
-//                '_first' => false,
-//                '_after' => 'telefono',
-//                '_bindType' => 2,
-//            ]),
-//            5 => Phalcon\Db\Column::__set_state([
-//                '_columnName' => 'email',
-//                '_schemaName' => NULL,
-//                '_type' => 2,
-//                '_isNumeric' => false,
-//                '_size' => 50,
-//                '_scale' => 0,
-//                '_default' => NULL,
-//                '_unsigned' => false,
-//                '_notNull' => false,
-//                '_autoIncrement' => false,
-//                '_first' => false,
-//                '_after' => 'direccion',
-//                '_bindType' => 2,
-//            ]),
-//            6 => Phalcon\Db\Column::__set_state([
-//                '_columnName' => 'fecha_nacimiento',
-//                '_schemaName' => NULL,
-//                '_type' => 1,
-//                '_isNumeric' => false,
-//                '_size' => 0,
-//                '_scale' => 0,
-//                '_default' => '1970-01-01',
-//                '_unsigned' => false,
-//                '_notNull' => false,
-//                '_autoIncrement' => false,
-//                '_first' => false,
-//                '_after' => 'email',
-//                '_bindType' => 2,
-//            ]),
-//            7 => Phalcon\Db\Column::__set_state([
-//                '_columnName' => 'ciudad_id',
-//                '_schemaName' => NULL,
-//                '_type' => 0,
-//                '_isNumeric' => true,
-//                '_size' => 10,
-//                '_scale' => 0,
-//                '_default' => '0',
-//                '_unsigned' => true,
-//                '_notNull' => false,
-//                '_autoIncrement' => false,
-//                '_first' => false,
-//                '_after' => 'fecha_nacimiento',
-//                '_bindType' => 1,
-//            ]),
-//            8 => Phalcon\Db\Column::__set_state([
-//                '_columnName' => 'creado_at',
-//                '_schemaName' => NULL,
-//                '_type' => 1,
-//                '_isNumeric' => false,
-//                '_size' => 0,
-//                '_scale' => 0,
-//                '_default' => NULL,
-//                '_unsigned' => false,
-//                '_notNull' => false,
-//                '_autoIncrement' => false,
-//                '_first' => false,
-//                '_after' => 'ciudad_id',
-//                '_bindType' => 2,
-//            ]),
-//            9 => Phalcon\Db\Column::__set_state([
-//                '_columnName' => 'cupo',
-//                '_schemaName' => NULL,
-//                '_type' => 3,
-//                '_isNumeric' => true,
-//                '_size' => 16,
-//                '_scale' => 2,
-//                '_default' => NULL,
-//                '_unsigned' => false,
-//                '_notNull' => true,
-//                '_autoIncrement' => false,
-//                '_first' => false,
-//                '_after' => 'creado_at',
-//                '_bindType' => 32,
-//            ]),
-//            10 => Phalcon\Db\Column::__set_state([
-//                '_columnName' => 'estado',
-//                '_schemaName' => NULL,
-//                '_type' => 18,
-//                '_isNumeric' => false,
-//                '_size' => "'A','I','X'",
-//                '_scale' => 0,
-//                '_default' => NULL,
-//                '_unsigned' => false,
-//                '_notNull' => true,
-//                '_autoIncrement' => false,
-//                '_first' => false,
-//                '_after' => 'cupo',
-//                '_bindType' => 2,
-//            ])
-//        ];
-//    }
-//
 //    public function testDbMysql()
 //    {
-//        //Table exist
-//        $this->assertEquals($connection->tableExists('personas'), 1);
-//        $this->assertEquals($connection->tableExists('noexist'), 0);
-//        $this->assertEquals($connection->tableExists('personas', 'phalcon_test'), 1);
-//        $this->assertEquals($connection->tableExists('personas', 'test'), 0);
-//
-//        $expectedDescribe = $this->getExpectedColumnsMysql();
-//        $describe = $connection->describeColumns('personas');
-//
-//        $this->assertEquals($describe, $expectedDescribe);
-//
-//        $describe = $connection->describeColumns('personas', 'phalcon_test');
-//        $this->assertEquals($describe, $expectedDescribe);
 //
 //        //Table Options
 //        $expectedOptions = [
