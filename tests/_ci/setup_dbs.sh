3c511ede3e050ba38a1521dd743a02583bd58560
Nikolaos Dimopoulos <nikos@niden.net>
2018-10-29 18:08:35 -0400

    [#13543] - Corrected schema names

--- tests/_ci/setup_dbs.sh
+++ tests/_ci/setup_dbs.sh
@@ -17,20 +17,20 @@ TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-$(dirname $(dirname $CURRENT_DIR))}"
 
 echo -e "Create MySQL database..."
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS phalcon_test charset=utf8mb4 collate=utf8mb4_unicode_ci;"
-cat "${TRAVIS_BUILD_DIR}/tests/_data/schemas/mysql/phalcon_test.sql" | mysql -u root phalcon_test
+cat "${TRAVIS_BUILD_DIR}/tests/_data/schemas/phalcon-schema-mysql.sql" | mysql -u root phalcon_test
 echo -e "Done\n"
 
 echo -e "Create PostgreSQL database..."
 psql -c 'create database phalcon_test;' -U postgres
-psql -U postgres phalcon_test -q -f "${TRAVIS_BUILD_DIR}/tests/_data/schemas/postgresql/phalcon_test.sql"
+psql -U postgres phalcon_test -q -f "${TRAVIS_BUILD_DIR}/tests/_data/schemas/phalcon-schema-postgresql.sql"
 echo -e "Done\n"
 
 echo -e "Create SQLite database..."
-sqlite3 /tmp/phalcon_test.sqlite < "${TRAVIS_BUILD_DIR}/tests/_data/schemas/sqlite/phalcon_test.sql"
+sqlite3 /tmp/phalcon_test.sqlite < "${TRAVIS_BUILD_DIR}/tests/_data/schemas/phalcon-schema-sqlite.sql"
 echo -e "Done\n"
 
 echo -e "Create translations SQLite database..."
-sqlite3 /tmp/translations.sqlite < "${TRAVIS_BUILD_DIR}/tests/_data/schemas/sqlite/translations.sql"
+sqlite3 /tmp/translations.sqlite < "${TRAVIS_BUILD_DIR}/tests/_data/schemas/phalcon-schema-sqlite-translations.sql"
 echo -e "Done\n"
 
 wait
