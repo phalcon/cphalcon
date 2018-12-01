a1b3192303e3a9c9f0d4415e91413e079f9b2ebb
Nikolaos Dimopoulos <nikos@niden.net>
2018-12-01 11:47:00 -0500

    [#13578] - Converted more Router tests

---
+++ tests/unit/Mvc/Router/AnnotationsCest.php
@@ -0,0 +1,207 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalconphp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Test\Unit\Mvc\Router;
+
+use Phalcon\Annotations\Adapter\Memory;
+use Phalcon\Di;
+use Phalcon\Http\Request;
+use Phalcon\Mvc\Router\Annotations;
+use Phalcon\Mvc\Router\Route;
+use Phalcon\Test\Fixtures\controllers\NamespacedAnnotationController;
+use Phalcon\Test\Fixtures\Traits\DiTrait;
+use UnitTester;
+use function is_object;
+use const PATH_DATA;
+
+class AnnotationsCest
+{
+    use DiTrait;
+
+    public function _before(UnitTester $I)
+    {
+        $this->newDi();
+        $this->setDiRequest();
+        $this->setDiAnnotations();
+    }
+
+    public function testRouterFullResources(UnitTester $I)
+    {
+        require_once PATH_DATA . 'fixtures/controllers/NamespacedAnnotationController.php';
+
+        $routes = $this->getRoutes();
+        foreach ($routes as $route) {
+            $uri        = $route['uri'];
+            $method     = $route['method'];
+            $controller = $route['controller'];
+            $action     = $route['action'];
+            $params     = $route['params'];
+
+            $container = $this->getDi();
+            $router    = new Annotations(false);
+
+            $router->setDI($container);
+
+            $router->addResource("Phalcon\Test\Fixtures\controllers\Robots", "/");
+            $router->addResource("Phalcon\Test\Fixtures\controllers\Products", "/products");
+            $router->addResource("Phalcon\Test\Fixtures\controllers\About", "/about");
+
+            $router->handle("/products");
+
+            $expected = 6;
+            $actual   = $router->getRoutes();
+            $I->assertCount($expected, $actual);
+
+            $router = new Annotations(false);
+
+            $router->setDI($container);
+            $router->addResource("Phalcon\Test\Fixtures\controllers\Robots", "/");
+            $router->addResource("Phalcon\Test\Fixtures\controllers\Products", "/products");
+            $router->addResource("Phalcon\Test\Fixtures\controllers\About", "/about");
+            $router->handle("/about");
+
+            $expected = 5;
+            $actual   = $router->getRoutes();
+            $I->assertCount($expected, $actual);
+
+            $router = new Annotations(false);
+            $router->setDI($container);
+            $router->setDefaultNamespace("MyNamespace\\Controllers");
+            $router->addResource("NamespacedAnnotation", "/namespaced");
+            $router->handle("/namespaced");
+
+            $expected = 1;
+            $actual   = $router->getRoutes();
+            $I->assertCount($expected, $actual);
+
+            $router = new Annotations(false);
+            $router->setDI($container);
+            $router->addResource("MyNamespace\\Controllers\\NamespacedAnnotation", "/namespaced");
+            $router->handle("/namespaced/");
+
+
+            $router = new Annotations(false);
+
+            $router->setDI($container);
+            $router->addResource("Phalcon\Test\Fixtures\controllers\Robots");
+            $router->addResource("Phalcon\Test\Fixtures\controllers\Products");
+            $router->addResource("Phalcon\Test\Fixtures\controllers\About");
+            $router->addResource("Phalcon\Test\Fixtures\controllers\Main");
+            $router->handle("/");
+
+            $expected = 9;
+            $actual   = $router->getRoutes();
+            $I->assertCount($expected, $actual);
+
+            $route = $router->getRouteByName("save-robot");
+            $I->assertTrue(is_object($route));
+
+            $class = Route::class;
+            $I->assertInstanceOf($class, $route);
+
+            $route = $router->getRouteByName("save-product");
+            $I->assertTrue(is_object($route));
+
+            $class = Route::class;
+            $I->assertInstanceOf($class, $route);
+
+            $_SERVER["REQUEST_METHOD"] = $method;
+            $router->handle($uri);
+
+            $expected = $controller;
+            $actual   = $router->getControllerName();
+            $I->assertEquals($expected, $actual);
+            $expected = $action;
+            $actual   = $router->getActionName();
+            $I->assertEquals($expected, $actual);
+            $expected = $params;
+            $actual   = $router->getParams();
+            $I->assertEquals($expected, $actual);
+            $I->assertTrue($router->isExactControllerName());
+        }
+    }
+
+    private function getRoutes(): array
+    {
+        return [
+            [
+                "uri"        => "/products/save",
+                "method"     => "PUT",
+                "controller" => "products",
+                "action"     => "save",
+                "params"     => [],
+            ],
+            [
+                "uri"        => "/products/save",
+                "method"     => "POST",
+                "controller" => "products",
+                "action"     => "save",
+                "params"     => [],
+            ],
+            [
+                "uri"        => "/products/edit/100",
+                "method"     => "GET",
+                "controller" => "products",
+                "action"     => "edit",
+                "params"     => ["id" => "100"],
+            ],
+            [
+                "uri"        => "/products",
+                "method"     => "GET",
+                "controller" => "products",
+                "action"     => "index",
+                "params"     => [],
+            ],
+            [
+                "uri"        => "/robots/edit/100",
+                "method"     => "GET",
+                "controller" => "robots",
+                "action"     => "edit",
+                "params"     => ["id" => "100"],
+            ],
+            [
+                "uri"        => "/robots",
+                "method"     => "GET",
+                "controller" => "robots",
+                "action"     => "index",
+                "params"     => [],
+            ],
+            [
+                "uri"        => "/robots/save",
+                "method"     => "PUT",
+                "controller" => "robots",
+                "action"     => "save",
+                "params"     => [],
+            ],
+            [
+                "uri"        => "/about/team",
+                "method"     => "GET",
+                "controller" => "about",
+                "action"     => "team",
+                "params"     => [],
+            ],
+            [
+                "uri"        => "/about/team",
+                "method"     => "POST",
+                "controller" => "about",
+                "action"     => "teampost",
+                "params"     => [],
+            ],
+            [
+                "uri"        => "/",
+                "method"     => "GET",
+                "controller" => "main",
+                "action"     => "index",
+                "params"     => [],
+            ],
+        ];
+    }
+}
