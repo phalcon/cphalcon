e1c56a8e52d0815f02a346c48744680f281b215a
Nikolaos Dimopoulos <nikos@niden.net>
2018-12-04 20:47:30 -0500

    [#13578] - Refactored tests to use dataProviders

--- tests/_data/fixtures/Helpers/TranslateQueryHelper.php
+++ tests/_data/fixtures/Helpers/TranslateQueryHelper.php
@@ -11,6 +11,7 @@
 
 namespace Phalcon\Test\Fixtures\Helpers;
 
+use Codeception\Example;
 use Phalcon\Translate\Adapter\NativeArray;
 use Phalcon\Test\Fixtures\Traits\TranslateTrait;
 use UnitTester;
@@ -22,247 +23,125 @@ class TranslateQueryHelper
     protected $function = '_';
 
     /**
-     * Tests Phalcon\Translate\Adapter\NativeArray :: query() - English
+     * Tests Phalcon\Translate\Adapter\NativeArray :: query()
+     *
+     * @param UnitTester $I
+     * @param Example    $data
+     *
+     * @dataProvider getQueryProvider
      *
      * @author Phalcon Team <team@phalconphp.com>
      * @since  2018-11-13
      */
-    public function testQueryEnglish(UnitTester $I)
+    public function translateAdapterNativearrayQuery(UnitTester $I, Example $data)
     {
-        $I->wantToTest($this->function . ' english');
-        $language   = $this->getArrayConfig()['en'];
+        $I->wantToTest(
+            sprintf(
+                'Translate\Adapter\NativeArray - %s - %s',
+                $this->function,
+                $data['language']
+            )
+        );
+        $language   = $this->getArrayConfig()[$data['code']];
         $translator = new NativeArray(['content' => $language]);
-
-        $expected = 'Hello';
-        $actual   = $translator->{$this->function}('hi');
-        $I->assertEquals($expected, $actual);
-
-        $expected = 'Good Bye';
-        $actual   = $translator->{$this->function}('bye');
-        $I->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Tests Phalcon\Translate\Adapter\NativeArray :: query() - Spanish
-     *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2014-09-12
-     */
-    public function testQuerySpanish(UnitTester $I)
-    {
-        $I->wantToTest($this->function . ' spanish');
-        $language   = $this->getArrayConfig()['es'];
-        $translator = new NativeArray(['content' => $language]);
-
-        $expected = 'Hola';
-        $actual   = $translator->{$this->function}('hi');
-        $I->assertEquals($expected, $actual);
-
-        $expected = 'Adiós';
-        $actual   = $translator->{$this->function}('bye');
-        $I->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Tests Phalcon\Translate\Adapter\NativeArray :: query() - French
-     *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2014-09-12
-     */
-    public function testQueryFrench(UnitTester $I)
-    {
-        $I->wantToTest($this->function . ' french');
-        $language   = $this->getArrayConfig()['fr'];
-        $translator = new NativeArray(['content' => $language]);
-
-        $expected = 'Bonjour';
-        $actual   = $translator->{$this->function}('hi');
-        $I->assertEquals($expected, $actual);
-
-        $expected = 'Au revoir';
-        $actual   = $translator->{$this->function}('bye');
-        $I->assertEquals($expected, $actual);
+        foreach ($data['tests'] as $key => $expected) {
+            $actual   = $translator->{$this->function}($key);
+            $I->assertEquals($expected, $actual);
+        }
     }
 
     /**
      * Tests Phalcon\Translate\Adapter\NativeArray :: query() -
-     * variable substitution in string with no variables - English
+     * variable substitution in string with no variables
+     *
+     * @param UnitTester $I
+     * @param Example    $data
+     *
+     * @dataProvider getQueryProvider
      *
      * @author Phalcon Team <team@phalconphp.com>
      * @since  2014-09-12
      */
-    public function testVariableSubstitutionInStringWithNoVariablesEnglish(UnitTester $I)
+    public function translateAdapterNativearrayVariableSubstitutionNoVariables(UnitTester $I, Example $data)
     {
-        $I->wantToTest($this->function . ' variable substitution in string with no variables english');
-        $language   = $this->getArrayConfig()['en'];
+        $I->wantToTest(
+            sprintf(
+                'Translate\Adapter\NativeArray - variable substitution no variables - %s',
+                $data['language']
+            )
+        );
+        $language   = $this->getArrayConfig()[$data['code']];
         $translator = new NativeArray(['content' => $language]);
-
-        $expected = 'Hello';
-        $actual   = $translator->{$this->function}('hi', ['name' => 'my friend']);
-        $I->assertEquals($expected, $actual);
+        foreach ($data['tests'] as $key => $expected) {
+            $actual   = $translator->{$this->function}($key, ['name' => 'my friend']);
+            $I->assertEquals($expected, $actual);
+        }
     }
 
     /**
      * Tests Phalcon\Translate\Adapter\NativeArray :: query() -
-     * variable substitution in string (one variable) - English
+     * variable substitution in string (one variable)
+     *
+     * @param UnitTester $I
+     * @param Example    $data
+     *
+     * @dataProvider getQueryOneVariable
      *
      * @author Phalcon Team <team@phalconphp.com>
      * @since  2014-09-12
      */
-    public function testVariableSubstitutionOneEnglish(UnitTester $I)
+    public function translateAdapterNativearrayVariableSubstitutionOneVariable(UnitTester $I, Example $data)
     {
-        $I->wantToTest($this->function . ' variable substitution one english');
-        $language   = $this->getArrayConfig()['en'];
+        $I->wantToTest(
+            sprintf(
+                'Translate\Adapter\NativeArray - variable substitution one variable - %s',
+                $data['language']
+            )
+        );
+        $language   = $this->getArrayConfig()[$data['code']];
         $translator = new NativeArray(['content' => $language]);
-
-        $expected = 'Hello my friend';
-        $actual   = $translator->{$this->function}('hello-key', ['name' => 'my friend']);
-        $I->assertEquals($expected, $actual);
+        foreach ($data['tests'] as $key => $expected) {
+            $actual   = $translator->{$this->function}($key, ['name' => 'my friend']);
+            $I->assertEquals($expected, $actual);
+        }
     }
 
     /**
      * Tests Phalcon\Translate\Adapter\NativeArray :: query() -
-     * variable substitution in string (two variable) - English
+     * variable substitution in string (two variables)
+     *
+     * @param UnitTester $I
+     * @param Example    $data
+     *
+     * @dataProvider getQueryTwoVariables
      *
      * @author Phalcon Team <team@phalconphp.com>
      * @since  2014-09-12
      */
-    public function testVariableSubstitutionTwoEnglish(UnitTester $I)
+    public function translateAdapterNativearrayVariableSubstitutionTwoVariable(UnitTester $I, Example $data)
     {
-        $I->wantToTest($this->function . ' variable substitution two english');
-        $language   = $this->getArrayConfig()['en'];
+        $I->wantToTest(
+            sprintf(
+                'Translate\Adapter\NativeArray - variable substitution two variables - %s',
+                $data['language']
+            )
+        );
+        $language   = $this->getArrayConfig()[$data['code']];
         $translator = new NativeArray(['content' => $language]);
-
-        $vars = [
+        $vars       = [
             'song'   => 'Dust in the wind',
             'artist' => 'Kansas',
         ];
-
-        $expected = 'This song is Dust in the wind (Kansas)';
-        $actual   = $translator->{$this->function}('song-key', $vars);
-        $I->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Tests Phalcon\Translate\Adapter\NativeArray :: query() -
-     * variable substitution in string with no variables - Spanish
-     *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2014-09-12
-     */
-    public function testVariableSubstitutionInStringWithNoVariablesSpanish(UnitTester $I)
-    {
-        $I->wantToTest($this->function . ' variable substitution in string with no variables spanish');
-        $language   = $this->getArrayConfig()['es'];
-        $translator = new NativeArray(['content' => $language]);
-
-        $expected = 'Hola';
-        $actual   = $translator->{$this->function}('hi', ['name' => 'my friend']);
-        $I->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Tests Phalcon\Translate\Adapter\NativeArray :: query() -
-     * variable substitution in string (one variable) - Spanish
-     *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2014-09-12
-     */
-    public function testVariableSubstitutionOneSpanish(UnitTester $I)
-    {
-        $I->wantToTest($this->function . ' variable substitution one spanish');
-        $language   = $this->getArrayConfig()['es'];
-        $translator = new NativeArray(['content' => $language]);
-
-        $expected = 'Hola my friend';
-        $actual   = $translator->{$this->function}('hello-key', ['name' => 'my friend']);
-        $I->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Tests Phalcon\Translate\Adapter\NativeArray :: query() -
-     * variable substitution in string (two variable) - Spanish
-     *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2014-09-12
-     */
-    public function testVariableSubstitutionTwoSpanish(UnitTester $I)
-    {
-        $I->wantToTest($this->function . ' variable substitution two english');
-        $language   = $this->getArrayConfig()['es'];
-        $translator = new NativeArray(['content' => $language]);
-
-        $vars = [
-            'song'   => 'Dust in the wind',
-            'artist' => 'Kansas',
-        ];
-
-        $expected = 'La canción es Dust in the wind (Kansas)';
-        $actual   = $translator->{$this->function}('song-key', $vars);
-        $I->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Tests Phalcon\Translate\Adapter\NativeArray :: query() -
-     * variable substitution in string with no variables - French
-     *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2014-09-12
-     */
-    public function testVariableSubstitutionInStringWithNoVariablesFrench(UnitTester $I)
-    {
-        $I->wantToTest($this->function . ' variable substitution in string with no variables french');
-        $language   = $this->getArrayConfig()['fr'];
-        $translator = new NativeArray(['content' => $language]);
-
-        $expected = 'Bonjour';
-        $actual   = $translator->{$this->function}('hi', ['name' => 'my friend']);
-        $I->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Tests Phalcon\Translate\Adapter\NativeArray :: query() -
-     * variable substitution in string (one variable) - French
-     *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2014-09-12
-     */
-    public function testVariableSubstitutionOneFrench(UnitTester $I)
-    {
-        $I->wantToTest($this->function . ' variable substitution one french');
-        $language   = $this->getArrayConfig()['fr'];
-        $translator = new NativeArray(['content' => $language]);
-
-        $expected = 'Bonjour my friend';
-        $actual   = $translator->{$this->function}('hello-key', ['name' => 'my friend']);
-        $I->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Tests Phalcon\Translate\Adapter\NativeArray :: query() -
-     * variable substitution in string (two variable) - French
-     *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2014-09-12
-     */
-    public function testVariableSubstitutionTwoFrench(UnitTester $I)
-    {
-        $I->wantToTest($this->function . ' variable substitution two french');
-        $language   = $this->getArrayConfig()['fr'];
-        $translator = new NativeArray(['content' => $language]);
-
-        $vars = [
-            'song'   => 'Dust in the wind',
-            'artist' => 'Kansas',
-        ];
-
-        $expected = 'La chanson est Dust in the wind (Kansas)';
-        $actual   = $translator->{$this->function}('song-key', $vars);
-        $I->assertEquals($expected, $actual);
+        foreach ($data['tests'] as $key => $expected) {
+            $actual   = $translator->{$this->function}($key, $vars);
+            $I->assertEquals($expected, $actual);
+        }
     }
 
     /**
      * Tests Phalcon\Translate\Adapter\NativeArray :: query() - array access and UTF8 strings
+     *
+     * @param UnitTester $I
      *
      * @author Phalcon Team <team@phalconphp.com>
      * @since  2014-09-12
@@ -281,5 +160,104 @@ class TranslateQueryHelper
         $expected = 'Привет, John D. Doe!';
         $actual   = $translator->{$this->function}('Hello %fname% %mname% %lname%!', $vars);
         $I->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Data provider for the query tests
+     *
+     * @return array
+     */
+    private function getQueryProvider(): array
+    {
+        return [
+            [
+                'language' => 'English',
+                'code'     => 'en',
+                'tests'    => [
+                    'hi'  => 'Hello',
+                    'bye' => 'Good Bye',
+                ],
+            ],
+            [
+                'language' => 'Spanish',
+                'code'     => 'es',
+                'tests'    => [
+                    'hi'  => 'Hola',
+                    'bye' => 'Adiós',
+                ],
+            ],
+            [
+                'language' => 'French',
+                'code'     => 'fr',
+                'tests'    => [
+                    'hi'  => 'Bonjour',
+                    'bye' => 'Au revoir',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Data provider for the query one variable substitution
+     *
+     * @return array
+     */
+    private function getQueryOneVariable(): array
+    {
+        return [
+            [
+                'language' => 'English',
+                'code'     => 'en',
+                'tests'    => [
+                    'hello-key' => 'Hello my friend',
+                ],
+            ],
+            [
+                'language' => 'Spanish',
+                'code'     => 'es',
+                'tests'    => [
+                    'hello-key' => 'Hola my friend',
+                ],
+            ],
+            [
+                'language' => 'French',
+                'code'     => 'fr',
+                'tests'    => [
+                    'hello-key' => 'Bonjour my friend',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Data provider for the query one variable substitution
+     *
+     * @return array
+     */
+    private function getQueryTwoVariables(): array
+    {
+        return [
+            [
+                'language' => 'English',
+                'code'     => 'en',
+                'tests'    => [
+                    'song-key' => 'This song is Dust in the wind (Kansas)',
+                ],
+            ],
+            [
+                'language' => 'Spanish',
+                'code'     => 'es',
+                'tests'    => [
+                    'song-key' => 'La canción es Dust in the wind (Kansas)',
+                ],
+            ],
+            [
+                'language' => 'French',
+                'code'     => 'fr',
+                'tests'    => [
+                    'song-key' => 'La chanson est Dust in the wind (Kansas)',
+                ],
+            ],
+        ];
     }
 }
