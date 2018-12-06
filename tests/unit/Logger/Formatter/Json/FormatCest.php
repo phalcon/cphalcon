86dea08ae97b1ab92493de30c3feba82b5b69e91
Nikolaos Dimopoulos <nikos@niden.net>
2018-12-05 19:40:23 -0500

    [#13438] - Added more tests for the Json formatter

--- tests/unit/Logger/Formatter/Json/FormatCest.php
+++ tests/unit/Logger/Formatter/Json/FormatCest.php
@@ -12,6 +12,9 @@ declare(strict_types=1);
 
 namespace Phalcon\Test\Unit\Logger\Formatter\Json;
 
+use Phalcon\Logger;
+use Phalcon\Logger\Formatter\Json;
+use Phalcon\Logger\Item;
 use UnitTester;
 
 /**
@@ -32,6 +35,40 @@ class FormatCest
     public function loggerFormatterJsonFormat(UnitTester $I)
     {
         $I->wantToTest("Logger\Formatter\Json - format()");
-        $I->skipTest("Need implementation");
+        $line = new Json();
+
+        $time = time();
+        $item = new Item('log message', 'debug', Logger::DEBUG, $time);
+
+        $expected = sprintf(
+            '{"type":"debug", "message":"log message", "timestamp":"%s"}',
+            date('D, d M y H:i:s O', $time)
+        );
+        $actual   = $line->format($item);
+        $I->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Tests Phalcon\Logger\Formatter\Json :: format() -custom
+     *
+     * @param UnitTester $I
+     *
+     * @author Phalcon Team <team@phalconphp.com>
+     * @since  2018-11-13
+     */
+    public function loggerFormatterJsonFormatCustom(UnitTester $I)
+    {
+        $I->wantToTest("Logger\Formatter\Json - format() - custom");
+        $line = new Json('YmdHis');
+
+        $time = time();
+        $item = new Item('log message', 'debug', Logger::DEBUG, $time);
+
+        $expected = sprintf(
+            '{"type":"debug", "message":"log message", "timestamp":"%s"}',
+            date('YmdHis', $time)
+        );
+        $actual   = $line->format($item);
+        $I->assertEquals($expected, $actual);
     }
 }
