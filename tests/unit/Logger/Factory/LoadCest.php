2a983bcc02cae24268bcf357f77698b6235c9cf0
Nikolaos Dimopoulos <nikos@niden.net>
2018-12-06 18:39:49 -0500

    [#13438] - Work on the Factory; Wrote more tests for it

--- tests/unit/Logger/Factory/LoadCest.php
+++ tests/unit/Logger/Factory/LoadCest.php
@@ -11,6 +11,7 @@
 
 namespace Phalcon\Test\Unit\Logger\Factory;
 
+use Phalcon\Logger;
 use Phalcon\Logger\Adapter\Stream;
 use Phalcon\Logger\Factory;
 use Phalcon\Test\Fixtures\Traits\FactoryTrait;
@@ -20,58 +21,76 @@ class LoadCest
 {
     use FactoryTrait;
 
+    /**
+     * @param UnitTester $I
+     */
     public function _before(UnitTester $I)
     {
         $this->init();
     }
 
-//    /**
-//     * Tests Phalcon\Logger\Factory :: load() - Config
-//     *
-//     * @param UnitTester $I
-//     *
-//     * @author Wojciech Ślawski <jurigag@gmail.com>
-//     * @since  2017-03-02
-//     */
-//    public function loggerFactoryLoadConfig(UnitTester $I)
-//    {
-//        $I->wantToTest('Logger\Factory - load() - Config');
-//        $options = $this->config->logger;
-//        $this->runTests($I, $options);
-//    }
-//
-//    /**
-//     * Runs the tests based on different configurations
-//     *
-//     * @param UnitTester   $I
-//     * @param Config|array $options
-//     */
-//    private function runTests(UnitTester $I, $options)
-//    {
-//        /** @var File $logger */
-//        $logger = Factory::load($options);
-//
-//        $class  = File::class;
-//        $actual = $logger;
-//        $I->assertInstanceOf($class, $actual);
-//
-//        $expected = $options['name'];
-//        $actual   = $logger->getPath();
-//        $I->assertEquals($expected, $actual);
-//    }
-//
-//    /**
-//     * Tests Phalcon\Logger\Factory :: load() - array
-//     *
-//     * @param UnitTester $I
-//     *
-//     * @author Wojciech Ślawski <jurigag@gmail.com>
-//     * @since  2017-03-02
-//     */
-//    public function loggerFactoryLoadArray(UnitTester $I)
-//    {
-//        $I->wantToTest('Logger\Factory - load() - array');
-//        $options = $this->arrayConfig['logger'];
-//        $this->runTests($I, $options);
-//    }
+    /**
+     * Tests Phalcon\Logger\Factory :: load() - Config
+     *
+     * @param UnitTester $I
+     *
+     * @author Wojciech Ślawski <jurigag@gmail.com>
+     * @since  2017-03-02
+     */
+    public function loggerFactoryLoadConfig(UnitTester $I)
+    {
+        $I->wantToTest('Logger\Factory - load() - Config');
+        $options = $this->config->logger;
+        $this->runTests($I, $options);
+    }
+
+    /**
+     * Tests Phalcon\Logger\Factory :: load() - array
+     *
+     * @param UnitTester $I
+     *
+     * @author Wojciech Ślawski <jurigag@gmail.com>
+     * @since  2017-03-02
+     */
+    public function loggerFactoryLoadArray(UnitTester $I)
+    {
+        $I->wantToTest('Logger\Factory - load() - array');
+        $options = $this->arrayConfig['logger'];
+        $this->runTests($I, $options);
+    }
+
+    /**
+     * Runs the tests based on different configurations
+     *
+     * @param UnitTester   $I
+     * @param Config|array $options
+     */
+    private function runTests(UnitTester $I, $options)
+    {
+        /** @var Logger $logger */
+        $logger = Factory::load($options);
+
+        $class  = Logger::class;
+        $actual = $logger;
+        $I->assertInstanceOf($class, $actual);
+
+        $expected = $options['name'];
+        $actual   = $logger->getName();
+        $I->assertEquals($expected, $actual);
+
+        $adapters = $logger->getAdapters();
+        $expected = count($options['adapters']);
+        $I->assertCount($expected, $adapters);
+
+        $counter = 0;
+        foreach ($adapters as $adapter) {
+            $class = sprintf('Phalcon\Logger\Adapter\%s', $options['adapters'][$counter]['adapter']);
+            $I->assertInstanceOf($class, $adapter);
+
+            $expected = $options['adapters'][$counter]['name'];
+            $actual   = $adapter->getName();
+            $I->assertEquals($expected, $actual);
+            $counter++;
+        }
+    }
 }
