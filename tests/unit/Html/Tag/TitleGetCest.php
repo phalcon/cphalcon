bc70a4c66d8a55b90a6dc73e242a20d25a9cb884
Nikolaos Dimopoulos <nikos@niden.net>
2018-12-09 21:54:17 -0500

    [#12379] - Renamed the methods; Rearranged the test stubs

---
+++ tests/unit/Html/Tag/TitleGetCest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalconphp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Test\Unit\Html\Tag;
+
+use UnitTester;
+
+/**
+ * Class TitleGetCest
+ *
+ * @package Phalcon\Test\Unit\Html\Tag
+ */
+class TitleGetCest
+{
+    /**
+     * Tests Phalcon\Html\Tag :: titleGet()
+     *
+     * @param UnitTester $I
+     *
+     * @author Phalcon Team <team@phalconphp.com>
+     * @since  2018-11-13
+     */
+    public function htmlTagTitleGet(UnitTester $I)
+    {
+        $I->wantToTest('Html\Tag - titleGet()');
+        $I->skipTest('Need implementation');
+    }
+}
