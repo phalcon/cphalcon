6c69c4ff7cf6a020fd51078a942d7ee42ba358d3
Ruud Boon <ruud@t-boonproductions.nl>
2018-12-12 07:59:52 +0100

    Fixed base64 test

--- tests/unit/Security/Random/Base64Cest.php
+++ tests/unit/Security/Random/Base64Cest.php
@@ -35,18 +35,39 @@ class Base64Cest
         $examples = [null, 2, 12, 16, 24, 48, 100];
 
         foreach ($examples as $len) {
-            $random = new Random();
+            $random = new \Phalcon\Security\Random();
 
             $isValid = function ($base64) {
                 return (preg_match("#[^a-z0-9+_=/-]+#i", $base64) === 0);
             };
 
             $base64 = $random->base64($len);
+            $I->assertTrue(is_string($base64));
 
             $expected = ($len === null) ? 16 : $len;
             $actual   = strlen($base64);
             $I->assertTrue($this->checkSize($base64, $expected));
-            $I->assertTrue($isValid($actual));
+            $I->assertTrue($isValid($base64));
         }
+    }
+
+    /**
+     * Size formula: 4 * ($n / 3) and this need to be rounded up to a multiple
+     * of 4.
+     *
+     * @param string $string
+     * @param int    $n
+     *
+     * @return bool
+     */
+    protected function checkSize($string, $n)
+    {
+        if (round(4 * ($n / 3)) % 4 === 0) {
+            $len = round(4 * ($n / 3));
+        } else {
+            $len = round((4 * ($n / 3) + 4 / 2) / 4) * 4;
+        }
+
+        return strlen($string) == $len;
     }
 }
