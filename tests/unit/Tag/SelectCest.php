8f3a54e9ae2f7a286841703ed26958e38f00559f
Cameron Hall <me@chall.id.au>
2019-01-27 22:33:22 +1100

    #13352: Select elements can now render without any options

--- tests/unit/Tag/SelectCest.php
+++ tests/unit/Tag/SelectCest.php
@@ -12,24 +12,56 @@ declare(strict_types=1);
 
 namespace Phalcon\Test\Unit\Tag;
 
+use Phalcon\Test\Fixtures\Helpers\TagSetup;
 use UnitTester;
 
 /**
  * Class SelectCest
  */
-class SelectCest
+class SelectCest extends TagSetup
 {
     /**
      * Tests Phalcon\Tag :: select()
      *
      * @param UnitTester $I
      *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2018-11-13
+     * @author Cameron Hall <me@chall.id.au>
+     * @since  2019-01-27
      */
     public function tagSelect(UnitTester $I)
     {
         $I->wantToTest('Tag - select()');
-        $I->skipTest('Need implementation');
+        
+        $this->testFieldParameter(
+            $I,
+            'select',
+            [
+                'potato',
+                [
+                    'Phalcon',
+                    'PHP'
+                ]
+            ],
+            "<select id=\"potato\" name=\"potato\">\n\t<option value=\"0\">Phalcon</option>\n\t<option value=\"1\">PHP</option>\n</select"
+        );
+    }
+
+    /**
+     * Tests Phalcon\Tag :: select() with no options
+     *
+     * @param UnitTester $I
+     *
+     * @author Cameron Hall <me@chall.id.au>
+     * @since  2019-01-27
+     *
+     * @issue https://github.com/phalcon/cphalcon/issues/13352
+     */
+    public function tagSelectWithNoOptions(UnitTester $I)
+    {
+        $I->wantToTest('Tag - select() with no options');
+
+        $this->testFieldParameter($I, 'select', [
+            'potato',
+        ], "<select id=\"potato\" name=\"potato\">\n</select");
     }
 }
