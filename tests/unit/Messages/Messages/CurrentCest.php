ef511031544abc29dac23eb251165faefa8d7518
Németh Balázs <nemeth.balazs@wponline.hu>
2019-02-10 02:31:42 +0100

    Added geSetMetaData test Modified some unit tests to use metaData

--- tests/unit/Messages/Messages/CurrentCest.php
+++ tests/unit/Messages/Messages/CurrentCest.php
@@ -34,8 +34,8 @@ class CurrentCest
         $I->wantToTest('Messages\Messages - current()');
         $messages = new Messages(
             [
-                new Message('This is a message #1', 'MyField1', 'MyType1', 111),
-                new Message('This is a message #2', 'MyField2', 'MyType2', 222),
+                new Message('This is a message #1', 'MyField1', 'MyType1', 111, ['My1' => 'Metadata1']),
+                new Message('This is a message #2', 'MyField2', 'MyType2', 222, ['My2' => 'Metadata2']),
             ]
         );
 
@@ -45,10 +45,11 @@ class CurrentCest
 
         $expected = Message::__set_state(
             [
-                '_message' => 'This is a message #1',
-                '_field'   => 'MyField1',
-                '_type'    => 'MyType1',
-                '_code'    => 111,
+                '_message'  => 'This is a message #1',
+                '_field'    => 'MyField1',
+                '_type'     => 'MyType1',
+                '_code'     => 111,
+                '_metaData' => ['My1' => 'Metadata1']
             ]
         );
         $I->assertEquals($expected, $actual);
