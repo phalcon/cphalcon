a023d169381ac8157a9d822bc63e1457fa1ec67e
Alexandru (Sasha) Busuioc <busuioc.alexandru@gmail.com>
2019-02-12 21:42:53 +0000

    Add `Dispatcher::toCamelCase()` method

--- tests/unit/Dispatcher/GetHandlerClassCest.php
+++ tests/unit/Dispatcher/GetHandlerClassCest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace Phalcon\Test\Unit\Dispatcher;
 
 use UnitTester;
+use Phalcon\Mvc\Dispatcher;
 
 /**
  * Class GetHandlerClassCest
@@ -30,6 +31,66 @@ class GetHandlerClassCest
     public function dispatcherGetHandlerClass(UnitTester $I)
     {
         $I->wantToTest('Dispatcher - getHandlerClass()');
-        $I->skipTest('Need implementation');
+
+        $dispatcher = new Dispatcher();
+
+        // test the handler name
+        $dispatcher->setNamespaceName('');
+        $dispatcher->setHandlerSuffix('');
+
+        $dispatcher->setControllerName('hello');
+        $actualHandler = $dispatcher->getHandlerClass();
+        $I->assertEquals('Hello', $actualHandler);
+
+        $dispatcher->setControllerName('hello-phalcon');
+        $actualHandler = $dispatcher->getHandlerClass();
+        $I->assertEquals('HelloPhalcon', $actualHandler);
+
+        $dispatcher->setControllerName('hello_phalcon');
+        $actualHandler = $dispatcher->getHandlerClass();
+        $I->assertEquals('HelloPhalcon', $actualHandler);
+
+        $dispatcher->setControllerName('HelloPhalcon');
+        $actualHandler = $dispatcher->getHandlerClass();
+        $I->assertEquals('HelloPhalcon', $actualHandler);
+
+        $dispatcher->setControllerName('Hello\\Phalcon');
+        $actualHandler = $dispatcher->getHandlerClass();
+        $I->assertEquals('Hello\\Phalcon', $actualHandler);
+
+        // test the suffix
+        $dispatcher->setHandlerSuffix('Ctrl');
+
+        $dispatcher->setControllerName('hello');
+        $actualHandler = $dispatcher->getHandlerClass();
+        $I->assertEquals('HelloCtrl', $actualHandler);
+
+        $dispatcher->setControllerName('hello-phalcon');
+        $actualHandler = $dispatcher->getHandlerClass();
+        $I->assertEquals('HelloPhalconCtrl', $actualHandler);
+
+        $dispatcher->setControllerName('hello_phalcon');
+        $actualHandler = $dispatcher->getHandlerClass();
+        $I->assertEquals('HelloPhalconCtrl', $actualHandler);
+
+        $dispatcher->setControllerName('Hello\\Phalcon');
+        $actualHandler = $dispatcher->getHandlerClass();
+        $I->assertEquals('Hello\\PhalconCtrl', $actualHandler);
+
+        $dispatcher->setControllerName('hello_ns\\Phalcon');
+        $actualHandler = $dispatcher->getHandlerClass();
+        $I->assertEquals('hello_ns\\PhalconCtrl', $actualHandler);
+
+        // test the namespace
+        $dispatcher->setNamespaceName('App');
+        $dispatcher->setControllerName('hello');
+        $actualHandler = $dispatcher->getHandlerClass();
+        $I->assertEquals('App\\HelloCtrl', $actualHandler);
+
+        $dispatcher->setNamespaceName('Ola\\');
+        $dispatcher->setControllerName('phalcon');
+        $actualHandler = $dispatcher->getHandlerClass();
+        $I->assertEquals('Ola\\PhalconCtrl', $actualHandler);
+
     }
 }
