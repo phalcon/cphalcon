3d25b9c4d077cbde30254024d67cd20f8bf40521
Nikolaos Dimopoulos <nikos@niden.net>
2019-02-19 11:11:05 -0500

    [#11789] - Added getSize tests

--- tests/unit/Http/Message/Stream/GetSizeCest.php
+++ tests/unit/Http/Message/Stream/GetSizeCest.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 
 namespace Phalcon\Test\Unit\Http\Message\Stream;
 
+use Phalcon\Http\Message\Stream;
 use UnitTester;
 
 /**
@@ -30,6 +31,27 @@ class GetSizeCest
     public function httpMessageStreamGetSize(UnitTester $I)
     {
         $I->wantToTest('Http\Message\Stream - getSize()');
-        $I->skipTest('Need implementation');
+        $fileName = dataFolder('/assets/stream/bill-of-rights.txt');
+        $expected = filesize($fileName);
+        $stream   = new Stream($fileName, 'rb');
+        $actual   = $stream->getSize();
+        $I->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Tests Phalcon\Http\Message\Stream :: getSize() - invalid stream
+     *
+     * @param UnitTester $I
+     *
+     * @author Phalcon Team <team@phalconphp.com>
+     * @since  2019-02-10
+     */
+    public function httpMessageStreamGetSizeInvalid(UnitTester $I)
+    {
+        $I->wantToTest('Http\Message\Stream - getSize() - invalid');
+        $stream   = new Stream('php://memory', 'rb');
+        $expected = 0;
+        $actual   = $stream->getSize();
+        $I->assertEquals($expected, $actual);
     }
 }
