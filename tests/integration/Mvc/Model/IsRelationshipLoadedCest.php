2175ded052f4bcca3989db6020629ca539ddfcba
Németh Balázs <nemeth.balazs@wponline.hu>
2019-04-26 15:37:46 +0200

    Internal model relation cache refactored Added tests to confirm relation storage cache Added tests for issue #13964

--- tests/integration/Mvc/Model/IsRelationshipLoadedCest.php
+++ tests/integration/Mvc/Model/IsRelationshipLoadedCest.php
@@ -13,23 +13,154 @@ declare(strict_types=1);
 namespace Phalcon\Test\Integration\Mvc\Model;
 
 use IntegrationTester;
+use Phalcon\Test\Fixtures\Traits\DiTrait;
+use Phalcon\Test\Models\AlbumORama;
 
 /**
  * Class IsRelationshipLoadedCest
  */
 class IsRelationshipLoadedCest
 {
+    use DiTrait;
+
+    public function _before(IntegrationTester $I)
+    {
+        $this->setNewFactoryDefault();
+        $this->setDiMysql();
+    }
+
     /**
-     * Tests Phalcon\Mvc\Model :: isRelationshipLoaded()
+     * Testing relationship loading using Model::__get()
      *
      * @param IntegrationTester $I
      *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2018-11-13
+     * @author Balázs Németh <https://github.com/zsilbi>
+     * @since  2019-04-26
      */
-    public function mvcModelIsRelationshipLoaded(IntegrationTester $I)
+    public function mvcModelUnderscoreGet(IntegrationTester $I)
     {
-        $I->wantToTest('Mvc\Model - isRelationshipLoaded()');
-        $I->skipTest('Need implementation');
+
+        $I->wantToTest('Mvc\Model - isRelationshipLoaded() when using __get()');
+
+        /**
+         * @var AlbumORama\Albums $album
+         */
+        $album = AlbumORama\Albums::findFirst();
+
+        $I->assertFalse($album->isRelationshipLoaded('artist'));
+        $I->assertFalse($album->isRelationshipLoaded('songs'));
+
+        /**
+         * @var AlbumORama\Artists $artist
+         */
+        $artist = $album->artist;
+
+        $I->assertTrue($album->isRelationshipLoaded('artist'));
+
+        /**
+         * @var \Phalcon\Mvc\Model\Resultset\Simple $songs
+         */
+        $songs = $album->songs;
+
+        $I->assertTrue($album->isRelationshipLoaded('songs'));
+    }
+
+    /**
+     * Testing relationship loading using Model::__set()
+     *
+     * @param IntegrationTester $I
+     *
+     * @author Balázs Németh <https://github.com/zsilbi>
+     * @since  2019-04-26
+     */
+    public function mvcModelUnderscoreSet(IntegrationTester $I)
+    {
+        $I->wantToTest('Mvc\Model - isRelationshipLoaded() when using __set()');
+
+        /**
+         * @var AlbumORama\Albums $album
+         */
+        $album = AlbumORama\Albums::findFirst();
+
+        $I->assertFalse($album->isRelationshipLoaded('artist'));
+        $I->assertFalse($album->isRelationshipLoaded('songs'));
+
+        $album->artist = new AlbumORama\Artists();
+
+        $I->assertTrue($album->isRelationshipLoaded('artist'));
+
+        $album->songs = [new AlbumORama\Songs(), new AlbumORama\Songs()];
+
+        $I->assertTrue($album->isRelationshipLoaded('songs'));
+    }
+
+    /**
+     * Testing relationship loading using Model::__call() getters
+     *
+     * @param IntegrationTester $I
+     *
+     * @author Balázs Németh <https://github.com/zsilbi>
+     * @since  2019-04-26
+     */
+    public function mvcModelUnderscoreCallGet(IntegrationTester $I)
+    {
+        $I->wantToTest('Mvc\Model - isRelationshipLoaded() when using __call() getters');
+
+        /**
+         * @var AlbumORama\Albums $album
+         */
+        $album = AlbumORama\Albums::findFirst();
+
+        $I->assertFalse($album->isRelationshipLoaded('artist'));
+        $I->assertFalse($album->isRelationshipLoaded('songs'));
+
+        /**
+         * @var AlbumORama\Artists $artist
+         */
+        $artist = $album->getArtist();
+
+        $I->assertTrue($album->isRelationshipLoaded('artist'));
+
+        /**
+         * @var \Phalcon\Mvc\Model\Resultset\Simple $songs
+         */
+        $songs = $album->getSongs();
+
+        $I->assertTrue($album->isRelationshipLoaded('songs'));
+    }
+
+    /**
+     * Testing relationship loading using Model::getRelated() method
+     *
+     * @param IntegrationTester $I
+     *
+     * @author Balázs Németh <https://github.com/zsilbi>
+     * @since  2019-04-26
+     */
+    public function mvcModelGetRelated(IntegrationTester $I)
+    {
+        $I->wantToTest('Mvc\Model - isRelationshipLoaded() when using getRelated()');
+
+        /**
+         * @var AlbumORama\Albums $album
+         */
+        $album = AlbumORama\Albums::findFirst();
+
+        $I->assertFalse($album->isRelationshipLoaded('artist'));
+        $I->assertFalse($album->isRelationshipLoaded('songs'));
+
+        /**
+         * @var AlbumORama\Artists $artist
+         */
+        $artist = $album->getRelated('artist');
+
+        $I->assertTrue($album->isRelationshipLoaded('artist'));
+
+        /**
+         * @var \Phalcon\Mvc\Model\Resultset\Simple $songs
+         */
+        $songs = $album->getRelated('songs');
+
+        $I->assertTrue($album->isRelationshipLoaded('songs'));
     }
 }
