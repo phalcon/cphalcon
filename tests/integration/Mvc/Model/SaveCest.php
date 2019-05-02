1cd7b8b724a219064b80508674f928f9e474e35c
Németh Balázs <nemeth.balazs@wponline.hu>
2019-05-02 19:03:52 +0200

    Added tests for Mvc\Model::isRelationShipLoaded() Added tests for Mvc\Model::save() Added tests for Mvc\Model::__set() Added tests for Mvc\Model::writeAttribute()

--- tests/integration/Mvc/Model/SaveCest.php
+++ tests/integration/Mvc/Model/SaveCest.php
@@ -13,23 +13,209 @@ declare(strict_types=1);
 namespace Phalcon\Test\Integration\Mvc\Model;
 
 use IntegrationTester;
+use Phalcon\Test\Fixtures\Traits\DiTrait;
+use Phalcon\Test\Models;
+use Phalcon\Mvc\Model;
 
 /**
  * Class SaveCest
  */
 class SaveCest
 {
+    use DiTrait;
+
+    public function _before(IntegrationTester $I)
+    {
+        $this->setNewFactoryDefault();
+        $this->setDiMysql();
+    }
+
     /**
      * Tests Phalcon\Mvc\Model :: save()
      *
      * @param IntegrationTester $I
      *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2018-11-13
+     * @author Balázs Németh <https://github.com/zsilbi>
+     * @since  2019-04-30
      */
     public function mvcModelSave(IntegrationTester $I)
     {
         $I->wantToTest('Mvc\Model - save()');
-        $I->skipTest('Need implementation');
+
+        /**
+         * New model
+         */
+        $user = new Models\Users();
+        $user->id = 54321;
+        $user->name = null;
+
+        $I->assertFalse($user->save());
+
+        $user->name = 'New User';
+
+        $I->assertTrue($user->save());
+
+        /**
+         * Saved model
+         */
+        $user = Models\Users::findFirst(54321);
+
+        $I->assertEquals(
+            [
+                'id'   => 54321,
+                'name' => 'New User',
+            ],
+            $user->toArray()
+        );
+
+        $user->name = 'Existing User';
+
+        $I->assertTrue($user->save());
+
+        /**
+         * Modified saved model
+         */
+        $user = Models\Users::findFirst(54321);
+
+        $I->assertEquals(
+            [
+                'id'   => 54321,
+                'name' => 'Existing User',
+            ],
+            $user->toArray()
+        );
+
+        $user->name = null;
+
+        $I->assertFalse($user->save());
+
+        /**
+         * Verify model count
+         */
+        $I->assertEquals(1, Models\Users::count(['id = 54321']));
+
+        /**
+         * Deleting is necessary because other tests may rely on specific row count
+         */
+        $I->assertTrue($user->delete());
+    }
+
+    /**
+     * Tests Phalcon\Mvc\Model :: save() with related records
+     *
+     * @param IntegrationTester $I
+     *
+     * @author Balázs Németh <https://github.com/zsilbi>
+     * @since  2019-04-30
+     */
+    public function mvcModelSaveWithRelatedRecords(IntegrationTester $I)
+    {
+        $I->wantToTest('Mvc\Model - save() with related records');
+
+        $robotPart = new Models\RobotsParts();
+
+        $robotPart->robot = new Models\Robots();
+        $robotPart->robot->assign([
+            'name'     => 'Test Robots',
+            'type'     => 'mechanical',
+            'year'     => 2019,
+            'datetime' => (new \DateTime())->format('Y-m-d'),
+            'text'     => 'Test text',
+        ]);
+
+        $part = new Models\Parts();
+        $part->name = 'Test Parts';
+
+        $robotPart->part = $part;
+
+        $I->assertTrue($robotPart->save());
+
+        $I->assertTrue($robotPart->robots_id > 0);
+        $I->assertTrue($robotPart->parts_id > 0);
+
+        $I->assertTrue($robotPart->robot->id > 0);
+        $I->assertTrue($part->id > 0);
+
+        $connection = $this->getService('db');
+
+        $I->assertFalse((bool) $connection->isUnderTransaction());
+
+        $I->assertEquals($robotPart->getDirtyState(), Model::DIRTY_STATE_PERSISTENT);
+
+        $I->assertEquals($robotPart->robot->getDirtyState(), Model::DIRTY_STATE_PERSISTENT);
+        $I->assertEquals($part->getDirtyState(), Model::DIRTY_STATE_PERSISTENT);
+
+        /**
+         * Deleting is necessary because other tests may rely on specific row count
+         */
+        $I->assertTrue($robotPart->delete());
+    }
+
+    /**
+     * Tests Phalcon\Mvc\Model :: save() after fetching related records
+     *
+     * @param IntegrationTester $I
+     *
+     * @see https://github.com/phalcon/cphalcon/issues/13964
+     *
+     * @author Balázs Németh <https://github.com/zsilbi>
+     * @since  2019-04-26
+     */
+    public function mvcModelSaveAfterFetchingRelated(IntegrationTester $I)
+    {
+        $I->wantToTest('Mvc\Model - save() after fetching related');
+
+        /**
+         * @var Models\AlbumORama\Albums $album
+         */
+        $album = Models\AlbumORama\Albums::findFirst();
+
+        /**
+         * @var Models\AlbumORama\Artists $artist
+         */
+        $artist = $album->artist;
+
+        $I->assertTrue($album->save());
+
+        /**
+         * @var \Phalcon\Mvc\Model\Resultset\Simple $songs
+         */
+        $songs = $album->songs;
+
+        $I->assertTrue($album->save());
+    }
+
+    /**
+     * Tests Phalcon\Mvc\Model :: save() after using related records getters
+     *
+     * @param IntegrationTester $I
+     *
+     * @see https://github.com/phalcon/cphalcon/issues/13964
+     *
+     * @author Balázs Németh <https://github.com/zsilbi>
+     * @since  2019-04-26
+     */
+    public function mvcModelSaveAfterUsingRelatedGetters(IntegrationTester $I)
+    {
+        $I->wantToTest('Mvc\Model - save() after using related records getters');
+
+        /**
+         * @var Models\AlbumORama\Albums $album
+         */
+        $album = Models\AlbumORama\Albums::findFirst();
+
+        /**
+         * @var Models\AlbumORama\Artists $artist
+         */
+        $artist = $album->getArtist();
+
+        $I->assertTrue($album->save());
+
+        /**
+         * @var \Phalcon\Mvc\Model\Resultset\Simple $songs
+         */
+        $songs = $album->getSongs();
+
+        $I->assertTrue($album->save());
     }
 }
