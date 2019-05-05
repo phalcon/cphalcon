bd52be03fe826d233c6f818c7b1dae82d1d73aac
Nikolaos Dimopoulos <nikos@niden.net>
2019-05-05 17:22:20 -0400

    [#13439] - Fixed metadata tests

--- tests/integration/Mvc/Model/MetaData/Libmemcached/ConstructCest.php
+++ tests/integration/Mvc/Model/MetaData/Libmemcached/ConstructCest.php
@@ -13,8 +13,10 @@ declare(strict_types=1);
 namespace Phalcon\Test\Integration\Mvc\Model\MetaData\Libmemcached;
 
 use IntegrationTester;
+use Phalcon\Cache\AdapterFactory;
 use Phalcon\Mvc\Model\MetaData\Libmemcached;
 use Phalcon\Mvc\Model\MetaDataInterface;
+use Phalcon\Storage\SerializerFactory;
 use Phalcon\Test\Fixtures\Traits\DiTrait;
 use Phalcon\Test\Models\Robots;
 use function dataDir;
@@ -37,7 +39,10 @@ class ConstructCest
         $this->container->setShared(
             'modelsMetadata',
             function () {
+                $serializer = new SerializerFactory();
+                $factory    = new AdapterFactory($serializer);
                 return new Libmemcached(
+                    $factory,
                     [
                         'servers' => [
                             [
