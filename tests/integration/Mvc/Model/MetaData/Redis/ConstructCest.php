cba73ce0bc4aac56dbb22bdac6194ad313c18e22
Nikolaos Dimopoulos <nikos@niden.net>
2019-05-05 17:47:47 -0400

    [#13439] - Fixing tests again

--- tests/integration/Mvc/Model/MetaData/Redis/ConstructCest.php
+++ tests/integration/Mvc/Model/MetaData/Redis/ConstructCest.php
@@ -13,9 +13,9 @@ declare(strict_types=1);
 namespace Phalcon\Test\Integration\Mvc\Model\MetaData\Redis;
 
 use IntegrationTester;
-use Phalcon\Cache\AdapterFactory;
 use Phalcon\Mvc\Model\MetaData\Redis;
 use Phalcon\Mvc\Model\MetaDataInterface;
+use Phalcon\Storage\AdapterFactory;
 use Phalcon\Storage\SerializerFactory;
 use Phalcon\Test\Fixtures\Traits\DiTrait;
 use Phalcon\Test\Models\Robots;
