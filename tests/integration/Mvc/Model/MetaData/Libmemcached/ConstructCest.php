77632b980e2c1a0613abdea3cecfdee9103775eb
Nikolaos Dimopoulos <nikos@niden.net>
2019-05-05 20:30:25 -0400

    Revert "[#13439] - Fixed metadata tests"
    This reverts commit 4b4a5bff88d0ebf2a7b7e15db040d28349876b1c.

--- tests/integration/Mvc/Model/MetaData/Libmemcached/ConstructCest.php
+++ tests/integration/Mvc/Model/MetaData/Libmemcached/ConstructCest.php
@@ -13,10 +13,8 @@ declare(strict_types=1);
 namespace Phalcon\Test\Integration\Mvc\Model\MetaData\Libmemcached;
 
 use IntegrationTester;
-use Phalcon\Cache\AdapterFactory;
 use Phalcon\Mvc\Model\MetaData\Libmemcached;
 use Phalcon\Mvc\Model\MetaDataInterface;
-use Phalcon\Storage\SerializerFactory;
 use Phalcon\Test\Fixtures\Traits\DiTrait;
 use Phalcon\Test\Models\Robots;
 use function dataDir;
@@ -39,10 +37,7 @@ class ConstructCest
         $this->container->setShared(
             'modelsMetadata',
             function () {
-                $serializer = new SerializerFactory();
-                $factory    = new AdapterFactory($serializer);
                 return new Libmemcached(
-                    $factory,
                     [
                         'servers' => [
                             [
