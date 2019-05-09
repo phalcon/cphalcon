c9cb5bdfe4cb1410f7e5f1a5f97a73d41c555088
Nikolaos Dimopoulos <nikos@niden.net>
2019-05-08 23:39:05 -0400

    [#13949] - Added tests

--- tests/integration/Mvc/Model/Transaction/RollbackCest.php
+++ tests/integration/Mvc/Model/Transaction/RollbackCest.php
@@ -13,12 +13,37 @@ declare(strict_types=1);
 namespace Phalcon\Test\Integration\Mvc\Model\Transaction;
 
 use IntegrationTester;
+use Phalcon\Mvc\Model\Transaction\Failed;
+use Phalcon\Test\Fixtures\Traits\DiTrait;
+use Phalcon\Test\Models\Personas;
 
 /**
  * Class RollbackCest
  */
 class RollbackCest
 {
+    use DiTrait;
+
+    private $records = [];
+
+    public function _before(IntegrationTester $I)
+    {
+        $this->setNewFactoryDefault();
+        $this->setDiMysql();
+        $this->records = [];
+    }
+
+    public function _after(IntegrationTester $I)
+    {
+        $db = $this->container->get('db');
+
+        foreach ($this->records as $record) {
+            $db->execute('DELETE FROM personas WHERE cedula = "' . $record . '"');
+        }
+
+        $this->records = [];
+    }
+
     /**
      * Tests Phalcon\Mvc\Model\Transaction :: rollback()
      *
@@ -30,6 +55,79 @@ class RollbackCest
     public function mvcModelTransactionRollback(IntegrationTester $I)
     {
         $I->wantToTest('Mvc\Model\Transaction - rollback()');
-        $I->skipTest('Need implementation');
+
+        $tm = $this->container->getShared('transactionManager');
+
+        $count = Personas::count();
+        $transaction = $tm->get();
+
+        for ($i = 0; $i < 10; $i++) {
+            $persona = new Personas();
+            $persona->setTransaction($transaction);
+            $persona->cedula            = 'T-Cx' . $i;
+            $persona->tipo_documento_id = 1;
+            $persona->nombres           = 'LOST LOST';
+            $persona->telefono          = '2';
+            $persona->cupo              = 0;
+            $persona->estado            = 'A';
+
+            $result = $persona->save();
+
+            $this->records[] = $persona->cedula;
+            $I->assertTrue($result);
+        }
+
+        $transaction->rollback();
+        $I->assertEquals($count, Personas::count());
+
+    }
+
+    /**
+     * Tests Phalcon\Mvc\Model\Transaction :: rollback() - exception
+     *
+     * @param IntegrationTester $I
+     *
+     * @author Phalcon Team <team@phalconphp.com>
+     * @since  2018-11-13
+     */
+    public function mvcModelTransactionRollbackException(IntegrationTester $I)
+    {
+        $I->wantToTest('Mvc\Model\Transaction - rollback() - exception');
+
+        $tm = $this->container->getShared('transactionManager');
+
+        $count = Personas::count();
+        $transaction = $tm
+            ->get()
+            ->throwRollbackException(true)
+        ;
+
+        for ($i = 0; $i < 10; $i++) {
+            $persona = new Personas();
+            $persona->setTransaction($transaction);
+            $persona->cedula            = 'T-Cx' . $i;
+            $persona->tipo_documento_id = 1;
+            $persona->nombres           = 'LOST LOST';
+            $persona->telefono          = '2';
+            $persona->cupo              = 0;
+            $persona->estado            = 'A';
+
+            $result = $persona->save();
+
+            $this->records[] = $persona->cedula;
+            $I->assertTrue($result);
+        }
+
+        try {
+            $transaction->rollback();
+            $I->assertTrue(
+                false,
+                "The transaction's rollback didn't throw an expected exception. Emergency stop"
+            );
+        } catch (Failed $e) {
+            $I->assertEquals($e->getMessage(), "Transaction aborted");
+        }
+
+        $I->assertEquals($count, Personas::count());
     }
 }
