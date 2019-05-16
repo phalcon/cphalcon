a9f30ffd0b3822ebb8bd894e27cff7eef498a690
Nikolaos Dimopoulos <nikos@niden.net>
2019-05-16 11:16:35 -0400

    T13869 crypt aes 256 gcm (#14079)
    * [#13869] - Added auth data and removed insecure algos
    
    * [#13869] - Adjustments to the interface
    
    * [#13869] - Adjustments to tests and new tests
    
    * [#13869] - Updated the changelog

--- tests/unit/Crypt/EncryptCest.php
+++ tests/unit/Crypt/EncryptCest.php
@@ -12,7 +12,10 @@ declare(strict_types=1);
 
 namespace Phalcon\Test\Unit\Crypt;
 
+use function hash_hmac_algos;
 use Phalcon\Crypt;
+use Phalcon\Crypt\Exception;
+use function substr;
 use UnitTester;
 
 class EncryptCest
@@ -34,7 +37,6 @@ class EncryptCest
         ];
 
         $ciphers = [
-            'AES-128-ECB',
             'AES-128-CBC',
             'AES-128-CFB',
             'AES-128-OFB',
@@ -77,6 +79,58 @@ class EncryptCest
 
                 $I->assertEquals($test, $actual);
             }
+        }
+    }
+
+    /**
+     * Tests Phalcon\Crypt :: encrypt() - unsupported algo
+     *
+     * @author Phalcon Team <team@phalconphp.com>
+     * @since  2018-11-13
+     */
+    public function cryptEncryptException(UnitTester $I)
+    {
+        $I->wantToTest('Crypt - encrypt() - exception');
+
+        $I->expectThrowable(
+            new Exception(
+                'The cipher algorithm "AES-128-ECB" is not supported on this system.'
+            ),
+            function () {
+                $crypt = new Crypt();
+                $crypt->setCipher('AES-128-ECB');
+            }
+        );
+    }
+
+    /**
+     * Tests Phalcon\Crypt :: encrypt() - gcm
+     *
+     * @author Phalcon Team <team@phalconphp.com>
+     * @since  2019-05-15
+     */
+    public function cryptEncryptGcm(UnitTester $I)
+    {
+        $I->wantToTest('Crypt - encrypt()');
+
+        $ciphers = [
+            'aes-128-gcm',
+            'aes-128-ccm',
+        ];
+
+        $crypt = new Crypt();
+
+        foreach ($ciphers as $cipher) {
+            $crypt
+                ->setCipher($cipher)
+                ->setAuthTag('1234')
+                ->setAuthData('abcd')
+                ->setKey('123456')
+            ;
+
+            $encryption = $crypt->encrypt('phalcon');
+            $actual     = $crypt->decrypt($encryption);
+            $I->assertEquals('phalcon', $actual);
         }
     }
 }
