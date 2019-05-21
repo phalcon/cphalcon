be50307f23c77956df41e444e02d4f721e3250d7
Nikolaos Dimopoulos <nikos@niden.net>
2019-05-21 14:09:52 -0400

    More tests

--- tests/integration/Mvc/Model/MetaData/Libmemcached/WriteCest.php
+++ tests/integration/Mvc/Model/MetaData/Libmemcached/WriteCest.php
@@ -28,18 +28,18 @@ class WriteCest
 {
     use DiTrait;
 
-    /**
-     * Tests Phalcon\Mvc\Model\MetaData\Libmemcached :: write()
-     *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2018-11-13
-     */
-    public function mvcModelMetadataLibmemcachedWrite(IntegrationTester $I)
-    {
-        $I->wantToTest('Mvc\Model\MetaData\Libmemcached - write()');
-        $I->skipTest('Need implementation');
-    }
-
+//    /**
+//     * Tests Phalcon\Mvc\Model\MetaData\Libmemcached :: write()
+//     *
+//     * @author Phalcon Team <team@phalconphp.com>
+//     * @since  2018-11-13
+//     */
+//    public function mvcModelMetadataLibmemcachedWrite(IntegrationTester $I)
+//    {
+//        $I->wantToTest('Mvc\Model\MetaData\Libmemcached - write()');
+//        $I->skipTest('Need implementation');
+//    }
+//
     /**
      * Tests Phalcon\Mvc\Model\MetaData\Libmemcached :: write() - exception
      *
@@ -49,19 +49,21 @@ class WriteCest
     public function mvcModelMetadataLibmemcachedWriteException(IntegrationTester $I)
     {
         $I->wantToTest('Mvc\Model\MetaData\Libmemcached - write() - exception');
-        $I->skipTest('Mvc\Model\MetaData\Libmemcached - write() - exception');
+        $I->skipTest('Check this to clear the cache if the key is there');
+
         $this->setNewFactoryDefault();
         $this->setDiMysql();
-        $this->container->set(
-            'modelsMetadata',
-            function () {
-                $options                       = getOptionsLibmemcached();
-                $options['servers'][0]['port'] = 4000;
-                $serializer                    = new SerializerFactory();
-                $factory                       = new AdapterFactory($serializer);
-                return new Libmemcached($factory, $options);
-            }
-        );
+
+        $options  = getOptionsLibmemcached();
+
+        $options['servers'][0]['port'] = 4000;
+
+        $serializer = new SerializerFactory();
+        $factory    = new AdapterFactory($serializer);
+        $cache      = new Libmemcached($factory, $options);
+
+        $this->container->setShared('modelsMetadata', $cache);
+
         $I->expectThrowable(
             new Exception('Failed to store metaData to the cache adapter'),
             function () use ($I) {
@@ -73,6 +75,7 @@ class WriteCest
                 Robots::findFirst(1);
             }
         );
+
         $I->expectThrowable(
             new \Exception('Failed to store metaData to the cache adapter', 1024),
             function () use ($I) {
