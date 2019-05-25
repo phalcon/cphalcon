bf34d077139e3980e18ae9058f4a1e63b43d9f86
Sid Roberts <sid@sidroberts.co.uk>
2019-05-25 09:44:33 +0900

    Added Db\Dialect\Mysql drop tests.

--- tests/integration/Db/Dialect/Mysql/DropTableCest.php
+++ tests/integration/Db/Dialect/Mysql/DropTableCest.php
@@ -12,22 +12,60 @@ declare(strict_types=1);
 
 namespace Phalcon\Test\Integration\Db\Dialect\Mysql;
 
+use Codeception\Example;
 use IntegrationTester;
+use Phalcon\Test\Fixtures\Traits\DialectTrait;
 
-/**
- * Class DropTableCest
- */
 class DropTableCest
 {
+    use DialectTrait;
+
     /**
      * Tests Phalcon\Db\Dialect\Mysql :: dropTable()
      *
-     * @author Phalcon Team <team@phalconphp.com>
-     * @since  2018-11-13
+     * @author Sid Roberts <https://github.com/SidRoberts>
+     * @since  2019-05-25
+     *
+     * @dataProvider getDropTableFixtures
      */
-    public function dbDialectMysqlDropTable(IntegrationTester $I)
+    public function dbDialectMysqlDropTable(IntegrationTester $I, Example $example)
     {
         $I->wantToTest('Db\Dialect\Mysql - dropTable()');
-        $I->skipTest('Need implementation');
+
+        $schema   = $example[0];
+        $ifExists = $example[1];
+        $expected = $example[2];
+
+        $dialect = $this->getDialectMysql();
+
+        $actual = $dialect->dropTable('table', $schema, $ifExists);
+
+        $I->assertEquals($expected, $actual);
+    }
+
+    protected function getDropTableFixtures(): array
+    {
+        return [
+            [
+                '',
+                true,
+                'DROP TABLE IF EXISTS `table`',
+            ],
+            [
+                'schema',
+                true,
+                'DROP TABLE IF EXISTS `schema`.`table`',
+            ],
+            [
+                '',
+                false,
+                'DROP TABLE `table`',
+            ],
+            [
+                'schema',
+                false,
+                'DROP TABLE `schema`.`table`',
+            ],
+        ];
     }
 }
