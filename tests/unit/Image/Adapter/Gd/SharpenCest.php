034ecea207be590ac0ebd43f4afd395f5f9e29f8
Emilio Degiovanni <emiliodeg@gmail.com>
2019-05-27 10:21:05 -0300

    Added sharpen tests

--- tests/unit/Image/Adapter/Gd/SharpenCest.php
+++ tests/unit/Image/Adapter/Gd/SharpenCest.php
@@ -12,10 +12,14 @@ declare(strict_types=1);
 
 namespace Phalcon\Test\Unit\Image\Adapter\Gd;
 
+use Phalcon\Image\Adapter\Gd;
+use Phalcon\Test\Fixtures\Traits\GdTrait;
 use UnitTester;
 
 class SharpenCest
 {
+    use GdTrait;
+
     /**
      * Tests Phalcon\Image\Adapter\Gd :: sharpen()
      *
@@ -26,6 +30,38 @@ class SharpenCest
     {
         $I->wantToTest('Image\Adapter\Gd - sharpen()');
 
-        $I->skipTest('Need implementation');
+        $outputDir = 'tests/image/gd';
+        $params = [
+            [10, '9b94b582851e0df3d06960abae30ce7a'],
+            [50, '2f39d4ccd43c058ca243f784f61b8487'],
+            [100, '71fe7f3643aee3af73f30b7b53795e82'],
+        ];
+        $i = 0;
+
+        foreach ($params as list($amount, $md5)) {
+            $image = new Gd(
+                dataDir('assets/images/phalconphp.jpg')
+            );
+
+            $outputImage = $i++ . 'sharpen.jpg';
+            $output = outputDir($outputDir . '/' . $outputImage);
+
+            $image->sharpen($amount)
+                ->save($output);
+
+            $I->amInPath(
+                outputDir($outputDir)
+            );
+
+            $I->seeFileFound($outputImage);
+
+            $I->assertSame(
+                $md5,
+                md5_file($output),
+                'Checking MD5'
+            );
+
+            $I->safeDeleteFile($outputImage);
+        }
     }
 }
