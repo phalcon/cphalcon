5636ccc47b54fa56531a46f9db967f98b6e288fc
zcq <64899484@qq.com>
2019-06-02 22:26:22 +0800

    test: add ServerRequest __construct  Header[host] error

--- tests/unit/Http/Message/ServerRequest/ConstructCest.php
+++ tests/unit/Http/Message/ServerRequest/ConstructCest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace Phalcon\Test\Unit\Http\Message\ServerRequest;
 
 use Phalcon\Http\Message\ServerRequest;
+use Phalcon\Http\Message\Uri;
 use Psr\Http\Message\ServerRequestInterface;
 use UnitTester;
 
@@ -30,5 +31,20 @@ class ConstructCest
         $request = new ServerRequest();
         $class   = ServerRequestInterface::class;
         $I->assertInstanceOf($class, $request);
+    }
+
+    /**
+     * Tests Phalcon\Http\Message\ServerRequest :: __construct()
+     *
+     * @author cq-z <64899484@qq.com>
+     * @since  2019-06-02
+     */
+    public function httpMessageServerRequestConstructIssues14151(UnitTester $I)
+    {
+        $I->wantToTest('Http\Message\ServerRequest - __construct()');
+        $request = new ServerRequest("GET",new Uri(),[],'php://input',['host'=>['127.0.0.1']]);
+        $expected = ['127.0.0.1'];
+        $actual   = $request->getHeader('host');
+        $I->assertEquals($expected, $actual);
     }
 }
