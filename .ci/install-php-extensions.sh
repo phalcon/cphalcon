f7497ef0f4d26a542a521b5489e498679db86b36
Serghei Iakovlev <sadhooklay@gmail.com>
2019-08-03 02:27:10 +0300

    Correct install II [skip appveyor]

--- .ci/install-php-extensions.sh
+++ .ci/install-php-extensions.sh
@@ -57,28 +57,26 @@ printf "\\n" | pecl install --force yaml 1> /dev/null
 (>&1 echo 'Install mongodb extension ...')
 printf "\\n" | pecl install --force mongodb 1> /dev/null
 
-sodium_ext=$($(phpenv which php-config) --extension-dir)/sodium.so
-if [[ "$(php --ri sodium 1> /dev/null)" = "" ]] && [[ ! -f "${sodium_ext}" ]]
+if [[ "$(php --ri sodium 1> /dev/null)" = "" ]]
 then
   # for some reason Ubuntu 18.04 on Travis CI doesn't install libsodium-dev
   # via "addons -> apt -> packages"
-  if [ "${CI}" = "true" ] && [ "$(dpkg -s libsodium-dev 1>/dev/null)" != "" ]
+  if [ "${CI}" = "true" ]
   then
-    (>&1 echo "Install libsodium-dev...")
-    sudo apt-get install \
-      --no-install-recommends \
-      --quiet \
-      --assume-yes \
-      libsodium-dev 1> /dev/null
+    # shellcheck disable=SC2091
+    if ! $(ldconfig -p | grep -q libsodium)
+    then
+      (>&1 echo "Install libsodium-dev...")
+      sudo apt-get install \
+        --no-install-recommends \
+        --quiet \
+        --assume-yes \
+        libsodium-dev 1> /dev/null
+    fi
   fi
 
   (>&1 echo 'Install libsodium extension ...')
   printf "\\n" | pecl install --force libsodium 1> /dev/null
-fi
-
-if [[ "$(php --ri sodium 1> /dev/null)" = "" ]] && [[ -f "${sodium_ext}" ]]
-then
-	echo 'extension="sodium.so"' > "$PHP_CONF_D/sodium.ini"
 fi
 
 redis_ext=$($(phpenv which php-config) --extension-dir)/redis.so
