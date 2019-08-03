6a7cfc21ffbc45bda42da24b31846660e41375a5
Serghei Iakovlev <sadhooklay@gmail.com>
2019-08-03 13:53:10 +0300

    Less verbosity on install MySQL on Travis

--- .ci/install-mysql-5.7.sh
+++ .ci/install-mysql-5.7.sh
@@ -16,8 +16,8 @@ then
 fi
 
 echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo debconf-set-selections
-wget https://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
-sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
-sudo apt-get update -q
-sudo apt-get install -q -y --allow-unauthenticated -o Dpkg::Options::=--force-confnew mysql-server
-sudo mysql_upgrade
+wget -q https://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
+sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb 1> /dev/null
+sudo apt-get update -q 1> /dev/null
+sudo apt-get install -q -y --allow-unauthenticated -o Dpkg::Options::=--force-confnew mysql-server 1> /dev/null
+sudo mysql_upgrade 1> /dev/null
