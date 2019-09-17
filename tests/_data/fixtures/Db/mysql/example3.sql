653a2d9183998cf0768bb1792d58b583f53e9c08
Ruud Boon <ruud@t-boonproductions.nl>
2019-09-17 12:54:03 +0200

    Example was missing space

--- tests/_data/fixtures/Db/mysql/example3.sql
+++ tests/_data/fixtures/Db/mysql/example3.sql
@@ -3,5 +3,5 @@ CREATE TABLE `table` (
 	`column3` DECIMAL(10,2) NOT NULL,
 	`column1` VARCHAR(10),
 	PRIMARY KEY (`column3`),
-	CONSTRAINT `fk3` FOREIGN KEY (`column1`) REFERENCES `ref_table`(`column2`) ON DELETE CASCADE
+	CONSTRAINT `fk3` FOREIGN KEY (`column1`) REFERENCES `ref_table` (`column2`) ON DELETE CASCADE
 )
