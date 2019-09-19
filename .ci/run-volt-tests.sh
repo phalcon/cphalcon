7100aaba05a03eb588df90b61e6a274498a74c51
Serghei Iakovlev <sadhooklay@gmail.com>
2019-09-20 01:47:46 +0300

    Improved volt tests runner

--- .ci/run-volt-tests.sh
+++ .ci/run-volt-tests.sh
@@ -25,7 +25,7 @@ php ./ext/run-tests.php \
   -d "report_zend_debug=0" \
   -d "zend.assertions=1" \
   -d "extension=$(php-config --extension-dir)/psr.so" \
-  -d "extension=${TRAVIS_BUILD_DIR}/ext/modules/phalcon.so" \
+  -d "extension=$(pwd)/ext/modules/phalcon.so" \
   --offline \
   --show-diff \
   --set-timeout 120 \
