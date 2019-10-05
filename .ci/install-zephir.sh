15c9c3005ecc3ce30b2be8825547f81a7a78647a
Serghei Iakovlev <sadhooklay@gmail.com>
2019-10-06 01:10:30 +0300

    Fix Zephir installer [skip appveyor]

--- .ci/install-zephir.sh
+++ .ci/install-zephir.sh
@@ -32,7 +32,8 @@ then
   chmod +x "$HOME/bin/zephir"
 else
   git clone -b "$ZEPHIR_VERSION" --depth 1 -q https://github.com/phalcon/zephir
-  cd zephir || exit
-  composer install "$DEFAULT_COMPOSER_FLAGS"
+  cd zephir || exit 1
+  echo "composer install ${DEFAULT_COMPOSER_FLAGS[*]}"
+  eval "composer install ${DEFAULT_COMPOSER_FLAGS[*]}"
   ln -s "$(pwd)/zephir" "$HOME/bin/zephir"
 fi
