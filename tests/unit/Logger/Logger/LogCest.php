8030576b80d8730f6ea1a5ac325ade2bd0a97d94
Nikolaos Dimopoulos <nikos@niden.net>
2019-10-21 19:36:20 -0400

    [#14480] - Added log level tests

--- tests/unit/Logger/Logger/LogCest.php
+++ tests/unit/Logger/Logger/LogCest.php
@@ -15,6 +15,8 @@ namespace Phalcon\Test\Unit\Logger\Logger;
 use Phalcon\Logger\Adapter\Stream;
 use Phalcon\Logger;
 use UnitTester;
+use function date;
+use function sprintf;
 
 class LogCest
 {
@@ -74,6 +76,85 @@ class LogCest
             );
 
             $I->seeInThisFile($expected);
+        }
+
+        $I->safeDeleteFile($fileName);
+    }
+
+    /**
+     * Tests Phalcon\Logger :: log() - logLevel
+     */
+    public function loggerLogLogLevel(UnitTester $I)
+    {
+        $I->wantToTest('Logger - log() - logLevel');
+
+        $logPath  = logsDir();
+        $fileName = $I->getNewFileName('log', 'log');
+        $adapter  = new Stream($logPath . $fileName);
+
+        $logger = new Logger(
+            'my-logger',
+            [
+                'one' => $adapter,
+            ]
+        );
+
+        $logger->setLogLevel(Logger::ALERT);
+
+        $levelsYes = [
+            Logger::ALERT     => 'alert',
+            Logger::CRITICAL  => 'critical',
+            Logger::EMERGENCY => 'emergency',
+            'alert'           => 'alert',
+            'critical'        => 'critical',
+            'emergency'       => 'emergency',
+        ];
+
+        $levelsNo = [
+            Logger::DEBUG     => 'debug',
+            Logger::ERROR     => 'error',
+            Logger::INFO      => 'info',
+            Logger::NOTICE    => 'notice',
+            Logger::WARNING   => 'warning',
+            Logger::CUSTOM    => 'custom',
+            'debug'           => 'debug',
+            'error'           => 'error',
+            'info'            => 'info',
+            'notice'          => 'notice',
+            'warning'         => 'warning',
+            'custom'          => 'custom',
+            'unknown'         => 'custom',
+        ];
+
+        foreach ($levelsYes as $level => $levelName) {
+            $logger->log($level, 'Message ' . $levelName);
+        }
+
+        foreach ($levelsNo as $level => $levelName) {
+            $logger->log($level, 'Message ' . $levelName);
+        }
+
+        $I->amInPath($logPath);
+        $I->openFile($fileName);
+
+        foreach ($levelsYes as $levelName) {
+            $expected = sprintf(
+                '[%s][%s] Message %s',
+                date('D, d M y H:i:s O'),
+                $levelName,
+                $levelName
+            );
+            $I->seeInThisFile($expected);
+        }
+
+        foreach ($levelsNo as $levelName) {
+            $expected = sprintf(
+                '[%s][%s] Message %s',
+                date('D, d M y H:i:s O'),
+                $levelName,
+                $levelName
+            );
+            $I->dontSeeInThisFile($expected);
         }
 
         $I->safeDeleteFile($fileName);
