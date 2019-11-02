8780ecc498a87f560d0d86d9ff3f084800cc902b
Németh Balázs <nemeth.balazs@wponline.hu>
2019-11-02 19:30:29 +0100

    Tests for has-on-through relations

---
+++ tests/integration/Mvc/Model/Manager/AddHasOneThroughCest.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Test\Integration\Mvc\Model\Manager;
+
+use IntegrationTester;
+
+/**
+ * Class AddHasOneThroughCest
+ */
+class AddHasOneThroughCest
+{
+    /**
+     * Tests Phalcon\Mvc\Model\Manager :: addHasOneThrough()
+     *
+     * @author Balázs Németh <https://github.com/zsilbi>
+     * @since  2019-11-02
+     */
+    public function mvcModelManagerAddHasOneThrough(IntegrationTester $I)
+    {
+        $I->wantToTest('Mvc\Model\Manager - addHasOneThrough()');
+        $I->skipTest('Need implementation');
+    }
+}
