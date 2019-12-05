f9599632dfa6e03db82a0a645d42e949fcd16d24
Jérémy PASTOURET <pastouret.jeremy@gmail.com>
2019-12-05 16:54:12 +0100

    Add two tests to check addInherit
    I hope the way I wrote these tests is Ok.
    The first one is just to check if addInherit doesn't return any error.
    The second one is deeper. It veriifies if inherit works well by checking different access.

--- tests/unit/Acl/Adapter/Memory/AddInheritCest.php
+++ tests/unit/Acl/Adapter/Memory/AddInheritCest.php
@@ -26,6 +26,82 @@ class AddInheritCest
     {
         $I->wantToTest('Acl\Adapter\Memory - addInherit()');
 
-        $I->skipTest('Need implementation');
+        $acl = new Memory();
+
+        $acl->addRole(new Role('administrator'));
+        $acl->addRole(new Role('apprentice'));
+       
+        $addedInherit = $acl->addInherit('administrator', 'apprentice');
+
+        $I->assertTrue($addedInherit);
+    }
+
+    /**
+     * Tests Phalcon\Acl\Adapter\Memory :: addInherit()
+     *
+     * @author <jenovateurs>
+     * @since  2019-12-05
+     */
+    public function aclAdapterMemoryAddInheritIsAllow(UnitTester $I)
+    {
+        $I->wantToTest('Acl\Adapter\Memory - addInherit() - check access');
+
+        $acl = new Memory();
+
+        //New role
+        $acl->addRole(new Role('administrator'));
+        $acl->addRole(new Role('apprentice'));
+
+        //New Component
+        $acl->addComponent(new Component('folder'), 'list');
+        $acl->addComponent(new Component('folder'), 'add');
+       
+        //Add Inherit
+        $addedInherit = $acl->addInherit('administrator', 'apprentice');
+
+        //Allow access
+        $acl->allow(
+            'apprentice',
+            'folder',
+            'list'
+        );
+
+        $acl->allow(
+            'administrator',
+            'folder',
+            'add'
+        );
+
+        //Check access
+        $apprenticeAccess = $acl->isAllowed(
+            'apprentice',
+            'folder',
+            'list'
+        );
+        $I->assertTrue($apprenticeAccess);
+
+        $administratorAccess = $acl->isAllowed(
+            'administrator',
+            'folder',
+            'add'
+        );
+
+        $I->assertTrue($administratorAccess);
+
+        $apprenticeNoAccess = $acl->isAllowed(
+            'apprentice',
+            'folder',
+            'add'
+        );
+
+        $I->assertFalse($apprenticeNoAccess);
+
+        $administratorAccessInherit = $acl->isAllowed(
+            'administrator',
+            'folder',
+            'list'
+        );
+
+        $I->assertTrue($administratorAccessInherit);
     }
 }
