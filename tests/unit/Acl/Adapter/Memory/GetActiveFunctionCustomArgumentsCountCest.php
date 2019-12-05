fdf39b77053105d62be159e2e6755a20ebd6fdfa
Jérémy PASTOURET <pastouret.jeremy@gmail.com>
2019-12-05 23:31:00 +0100

    Update GetActiveFunctionCustomArgumentsCountCest.php

--- tests/unit/Acl/Adapter/Memory/GetActiveFunctionCustomArgumentsCountCest.php
+++ tests/unit/Acl/Adapter/Memory/GetActiveFunctionCustomArgumentsCountCest.php
@@ -55,7 +55,7 @@ class GetActiveFunctionCustomArgumentsCountCest
                 'accountType' => 'premium',
                 'active'      => true,
             ]
-            );
+        );
 
         $I->assertTrue($isAllowed);
 
