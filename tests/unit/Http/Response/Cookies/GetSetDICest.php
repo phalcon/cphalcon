4ddb47a29a665f3da5fcfd17645079b6e3908094
Jérémy PASTOURET <pastouret.jeremy@gmail.com>
2019-12-07 13:24:30 +0100

    Test : Http\Response\Cookies :: getDI() / setDI()

---
+++ tests/unit/Http/Response/Cookies/GetSetDICest.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Test\Unit\Http\Response\Cookies;
+
+use Phalcon\Di;
+use Phalcon\Http\Response\Cookies as ResponseCookies;
+use UnitTester;
+
+class GetSetDICest
+{
+    /**
+     * Tests Phalcon\Http\Response\Cookies :: getDI() / setDI()
+     *
+     * @author Jeremy PASTOURET <https://github.com/jenovateurs>
+     * @since  2019-12-07
+    */
+    public function httpResponseCookiesGetSetDI(UnitTester $I)
+    {
+        $I->wantToTest('Http\Response\Cookies - getDI()');
+
+        $container = new Di();
+
+        $oResponseCookies = new ResponseCookies();
+
+        $oResponseCookies->setDI($container);
+
+        $I->assertSame(
+            $container,
+            $oResponseCookies->getDI()
+        );
+
+        $class  = Di::class;
+        $actual = $oResponseCookies->getDI();
+        $I->assertInstanceOf($class, $actual);
+    }
+}
