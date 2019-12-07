694e09d4fa471c3ecf6d57e9e3fc3380851dccc6
Jérémy PASTOURET <pastouret.jeremy@gmail.com>
2019-12-07 19:33:43 +0100

    Test Phalcon\Http\Response :: setJsonContent()
    Check if the content, the header is ok. Also test Json option.

--- tests/unit/Http/Response/SetJsonContentCest.php
+++ tests/unit/Http/Response/SetJsonContentCest.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 
 namespace Phalcon\Test\Unit\Http\Response;
 
+use Phalcon\Http\Response;
 use UnitTester;
 
 class SetJsonContentCest
@@ -19,13 +20,40 @@ class SetJsonContentCest
     /**
      * Tests Phalcon\Http\Response :: setJsonContent()
      *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2018-11-13
-     */
+     * @author Jeremy PASTOURET <https://github.com/jenovateurs>
+     * @since  2019-12-07
+    */
     public function httpResponseSetJsonContent(UnitTester $I)
     {
         $I->wantToTest('Http\Response - setJsonContent()');
 
-        $I->skipTest('Need implementation');
+        $aData = [
+            'sentence'  => 'it\'s a "city"',
+            'word'      => '<h1>city</h1>'
+        ];
+
+        $oResponse = new Response();
+        $oResponse->setJsonContent($aData);
+
+        //Check content
+        $I->assertSame(
+            $oResponse->getContent(),
+            json_encode($aData)
+        );
+
+        //Check Header
+        $I->assertSame(
+            $oResponse->getHeaders()->get('Content-Type'),
+            "application/json; charset=UTF-8"
+        );
+
+        //With option
+        $oResponse2 = new Response();
+        $oResponse2->setJsonContent($aData, JSON_HEX_TAG);
+
+        $I->assertSame(
+            $oResponse2->getContent(),
+            json_encode($aData, JSON_HEX_TAG)
+        );
     }
 }
