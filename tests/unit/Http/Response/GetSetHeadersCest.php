1bcfea0900f24e1f43993fc622a982a0cc4dfaba
Jérémy PASTOURET <pastouret.jeremy@gmail.com>
2019-12-08 18:50:58 +0100

    Update GetSetHeadersCest.php

--- tests/unit/Http/Response/GetSetHeadersCest.php
+++ tests/unit/Http/Response/GetSetHeadersCest.php
@@ -45,4 +45,56 @@ class GetSetHeadersCest
             $oResponse->getHeaders()->toArray()
         );
     }
+
+    /**
+     * Tests Phalcon\Http\Response :: setHeaders() - empty
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2018-11-13
+     */
+    public function httpResponseSetHeadersEmpty(UnitTester $I)
+    {
+        $I->wantToTest("Http\Response - setHeaders() - empty");
+
+        $response = new Response();
+        $headers  = new Headers();
+
+        $headers->set('Cache-Control', 'no-cache');
+        $response->setHeaders($headers);
+
+        $I->assertEquals(
+            $headers,
+            $response->getHeaders()
+        );
+    }
+
+    /**
+     * Tests Phalcon\Http\Response :: setHeaders() - merge
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2018-11-13
+     */
+    public function httpResponseSetHeadersMerge(UnitTester $I)
+    {
+        $I->wantToTest("Http\Response - setHeaders() - merge");
+
+        $response = new Response();
+        $headers  = new Headers();
+
+        /**
+         * With setHeader
+         */
+        $headers->set('Cache-Control', 'no-cache');
+        $response->setHeader('Content-Length', '1234');
+        $response->setHeaders($headers);
+
+        $headers = new Headers();
+        $headers->set('Content-Length', '1234');
+        $headers->set('Cache-Control', 'no-cache');
+
+        $I->assertEquals(
+            $headers,
+            $response->getHeaders()
+        );
+    }
 }
