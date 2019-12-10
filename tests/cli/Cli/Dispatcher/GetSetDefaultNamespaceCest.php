958538dbfb75f2aa8604097ea0d215cbe65e54ca
Ruud Boon <ruud@t-boonproductions.nl>
2019-12-10 17:26:23 +0100

    Added test for GetSetDefaultNamespaceCest

---
+++ tests/cli/Cli/Dispatcher/GetSetDefaultNamespaceCest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Test\Cli\Cli\Dispatcher;
+
+use CliTester;
+use Phalcon\Cli\Dispatcher;
+
+class GetSetDefaultNamespaceCest
+{
+    /**
+     * Tests Phalcon\Cli\Dispatcher :: getDefaultNamespace() / setDefaultNamespace()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2018-11-13
+     */
+    public function cliDispatcherGetSetDefaultNamespace(CliTester $I)
+    {
+        $I->wantToTest('Cli\Dispatcher - getDefaultNamespace() / setDefaultNamespace()');
+        $dispatcher = new Dispatcher();
+
+        $I->assertEquals('', $dispatcher->getDefaultNamespace());
+
+        $namespace = "Phalcon\Rules";
+        $dispatcher->setDefaultNamespace($namespace);
+
+        $I->assertEquals($namespace, $dispatcher->getDefaultNamespace());
+
+    }
+}
