4a3eedd1e8698140547261819a4b29dabbd0ec7b
Nikolaos Dimopoulos <nikos@niden.net>
2019-12-13 15:13:31 -0500

    [#14575] - Corrected docblocks

--- tests/_data/fixtures/Http/Message/ServerRequestFactoryFixture.php
+++ tests/_data/fixtures/Http/Message/ServerRequestFactoryFixture.php
@@ -3,7 +3,7 @@
 /**
 * This file is part of the Phalcon Framework.
  *
- * For the full copyright and license information, please view the LICENSE.md
+ * For the full copyright and license information, please view the LICENSE.txt
  * file that was distributed with this source code.
  */
 
