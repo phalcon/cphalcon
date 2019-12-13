12e93498da1a1130f5330a4fb370bae961cf9683
Nikolaos Dimopoulos <nikos@niden.net>
2019-12-13 15:13:31 -0500

    [#14575] - Corrected docblocks

--- tests/_data/fixtures/models/Sources.php
+++ tests/_data/fixtures/models/Sources.php
@@ -5,7 +5,7 @@
  *
  * (c) Phalcon Team <team@phalcon.io>
  *
- * For the full copyright and license information, please view the LICENSE.md
+ * For the full copyright and license information, please view the LICENSE.txt
  * file that was distributed with this source code.
  */
 
--- tests/unit/Storage/Serializer/Json/GetDataCest.php
+++ tests/unit/Storage/Serializer/Json/GetDataCest.php
@@ -3,6 +3,8 @@
 /**
  * This file is part of the Phalcon Framework.
  *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
  * For the full copyright and license information, please view the LICENSE.txt
  * file that was distributed with this source code.
  */
