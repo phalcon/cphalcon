d72501f6e477121a8a27c2aeb8fab0034cf0272a
Nikolaos Dimopoulos <nikos@niden.net>
2019-12-22 23:32:19 -0500

    [#13856] - Added tests for JWT

---
+++ tests/unit/Http/JWT/Validator/ValidateIssuedAtCest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Test\Unit\Http\JWT\Validator;
+
+use Phalcon\Http\JWT\Exceptions\ValidatorException;
+use Phalcon\Http\JWT\Validator;
+use Phalcon\Test\Fixtures\Traits\JWTTrait;
+use UnitTester;
+
+class ValidateIssuedAtCest
+{
+    use JWTTrait;
+
+    /**
+     * Unit Tests Phalcon\Http\JWT\Validator :: validateIssuedAt()
+     *
+     * @since  2019-12-22
+     */
+    public function httpJWTValidatorValidateIssuedAt(UnitTester $I)
+    {
+        $I->wantToTest('Http\JWT\Validator - validateIssuedAt()');
+
+        $token = $this->newToken();
+        $I->expectThrowable(
+            new ValidatorException(
+                "Validation: the token cannot be used yet (future)"
+            ),
+            function () use ($token, $I) {
+                $timestamp = strtotime(("-1 day"));
+                $validator = new Validator($token);
+                $I->assertInstanceOf(Validator::class, $validator);
+                $validator->validateIssuedAt($timestamp);
+            }
+        );
+    }
+}
