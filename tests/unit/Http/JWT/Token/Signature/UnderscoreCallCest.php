171e6d82a1fed02550fc2f1f78b6159636e48022
Nikolaos Dimopoulos <nikos@niden.net>
2019-12-22 23:32:19 -0500

    [#13856] - Added tests for JWT

---
+++ tests/unit/Http/JWT/Token/Signature/UnderscoreCallCest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Test\Unit\Http\JWT\Token\Signature;
+
+use Phalcon\Http\JWT\Token\Signature;
+use UnitTester;
+
+class UnderscoreCallCest
+{
+    /**
+     * Unit Tests Phalcon\Http\JWT\Token\Signature :: __call()
+     *
+     * @since  2019-12-22
+     */
+    public function httpJWTTokenSignatureUnderscoreCall(UnitTester $I)
+    {
+        $I->wantToTest('Http\JWT\Token\Signature - __call()');
+
+        $signature = new Signature('sig-hash', 'encoded-string-here');
+
+        $I->assertEquals('encoded-string-here', $signature->getEncoded());
+        $I->assertEquals('sig-hash', $signature->getHash());
+    }
+}
