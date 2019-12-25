858fdef5d339b377f67ece4e0045706759a7f523
Nikolaos Dimopoulos <nikos@niden.net>
2019-12-24 20:17:10 -0500

    [#13518] - Added check to __isset for potential getter and changed the exception text

--- tests/integration/Mvc/Model/UnderscoreGetCest.php
+++ tests/integration/Mvc/Model/UnderscoreGetCest.php
@@ -14,8 +14,13 @@ declare(strict_types=1);
 namespace Phalcon\Test\Integration\Mvc\Model;
 
 use IntegrationTester;
+use Phalcon\Mvc\Model\Exception;
+use Phalcon\Mvc\Model\Resultset\Simple;
+use Phalcon\Test\Fixtures\Migrations\InvoicesMigration;
 use Phalcon\Test\Fixtures\Traits\DiTrait;
 use Phalcon\Test\Models;
+use Phalcon\Test\Models\Invoices;
+use Phalcon\Test\Models\InvoicesSchema;
 
 class UnderscoreGetCest
 {
@@ -32,165 +37,217 @@ class UnderscoreGetCest
         $this->container['db']->close();
     }
 
+//    /**
+//     * Tests Phalcon\Mvc\Model :: __get()
+//     *
+//     * @author Balázs Németh <https://github.com/zsilbi>
+//     * @since  2019-05-07
+//     */
+//    public function mvcModelUnderscoreGet(IntegrationTester $I)
+//    {
+//        $I->wantToTest('Mvc\Model - __get()');
+//
+//        $user = new Models\Users();
+//
+//        $user->id   = 999;
+//        $user->name = 'Test';
+//
+//        $I->assertEquals(
+//            999,
+//            $user->id
+//        );
+//
+//        $I->assertEquals(
+//            'Test',
+//            $user->name
+//        );
+//    }
+//
+//    /**
+//     * Tests Phalcon\Mvc\Model :: __get() whether it is using getters correctly
+//     *
+//     * @author Balázs Németh <https://github.com/zsilbi>
+//     * @since  2019-05-07
+//     */
+//    public function mvcModelUnderscoreGetIsUsingGetters(IntegrationTester $I)
+//    {
+//        $I->wantToTest("Mvc\Model - __get() whether it is using getters correctly");
+//
+//        $model = new Models\Select();
+//        $model->setId(123);
+//
+//        $I->assertEquals(
+//            123,
+//            $model->id
+//        );
+//
+//        $associativeArray = [
+//            'firstName' => 'First name',
+//            'lastName'  => 'Last name',
+//        ];
+//
+//        $model->setName($associativeArray);
+//
+//        $I->assertEquals(
+//            $associativeArray,
+//            $model->name
+//        );
+//
+//        $model->setText('MyText');
+//
+//        $I->assertEquals(
+//            'MyText',
+//            $model->text
+//        );
+//    }
+//
+//    /**
+//     * Tests Phalcon\Mvc\Model :: __get() related records
+//     *
+//     * @author Balázs Németh <https://github.com/zsilbi>
+//     * @since  2019-05-07
+//     */
+//    public function mvcModelUnderscoreGetRelated(IntegrationTester $I)
+//    {
+//        $I->wantToTest('Mvc\Model - __get() related records');
+//
+//        /**
+//         * Belongs-to relationship
+//         */
+//        $robotPart = Models\RobotsParts::findFirst();
+//
+//        $part = $robotPart->part;
+//
+//        $I->assertInstanceOf(
+//            Models\Parts::class,
+//            $part
+//        );
+//
+//        /**
+//         * Testing has-one relationship
+//         */
+//        $customer = Models\Customers::findFirst();
+//
+//        $user = $customer->user;
+//
+//        $I->assertInstanceOf(
+//            Models\Users::class,
+//            $user
+//        );
+//
+//        /**
+//         * Has-many relationship
+//         */
+//        $robot = Models\Robots::findFirst();
+//
+//        $robotParts = $robot->robotsParts;
+//
+//        $I->assertInstanceOf(
+//            Simple::class,
+//            $robotParts
+//        );
+//    }
+//
+//    /**
+//     * Tests Phalcon\Mvc\Model :: __get() dirty related records
+//     *
+//     * @author Balázs Németh <https://github.com/zsilbi>
+//     * @since  2019-05-07
+//     */
+//    public function mvcModelUnderscoreGetDirtyRelated(IntegrationTester $I)
+//    {
+//        $I->wantToTest('Mvc\Model - __get() dirty related records');
+//
+//        $robot = Models\Robots::findFirst();
+//
+//        /**
+//         * Fill up the related cache with data
+//         */
+//        $robotsParts = $robot->robotsParts;
+//
+//        $I->assertInstanceOf(
+//            Simple::class,
+//            $robotsParts
+//        );
+//
+//        /**
+//         * Add new related records
+//         */
+//        $robot->robotsParts = [
+//            new Models\RobotsParts(),
+//            new Models\RobotsParts(),
+//        ];
+//
+//        /**
+//         * Test if the new records were returned
+//         */
+//        $dirtyRobotsParts = $robot->robotsParts;
+//
+//        $I->assertInternalType(
+//            'array',
+//            $dirtyRobotsParts
+//        );
+//
+//        $I->assertCount(
+//            2,
+//            $dirtyRobotsParts
+//        );
+//
+//        $I->assertInstanceOf(
+//            Models\RobotsParts::class,
+//            $dirtyRobotsParts[0]
+//        );
+//    }
+
     /**
-     * Tests Phalcon\Mvc\Model :: __get()
+     * Tests Phalcon\Mvc\Model :: __get() private property
      *
-     * @author Balázs Németh <https://github.com/zsilbi>
-     * @since  2019-05-07
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2019-12-24
      */
-    public function mvcModelUnderscoreGet(IntegrationTester $I)
+    public function mvcModelUnderscorePrivateProperty(IntegrationTester $I)
     {
-        $I->wantToTest('Mvc\Model - __get()');
+        $I->wantToTest('Mvc\Model - __get() private property');
 
-        $user = new Models\Users();
+        /**
+         * Setup the table
+         */
+        (new InvoicesMigration())($this->container->get('db'));
 
-        $user->id   = 999;
-        $user->name = 'Test';
+        $model = new Invoices();
 
-        $I->assertEquals(
-            999,
-            $user->id
-        );
+        $I->assertFalse(isset($model->superSecret));
+        $I->assertTrue(isset($model->secretValue));
 
-        $I->assertEquals(
-            'Test',
-            $user->name
-        );
+        $model->setSecretValue(123);
+        $I->assertEquals(123, $model->getSecretValue());
+        $model->secretValue = 123;
+        $I->assertEquals(123, $model->secretValue);
     }
 
     /**
-     * Tests Phalcon\Mvc\Model :: __get() whether it is using getters correctly
+     * Tests Phalcon\Mvc\Model :: __get() private property - exception
      *
-     * @author Balázs Németh <https://github.com/zsilbi>
-     * @since  2019-05-07
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2019-12-24
      */
-    public function mvcModelUnderscoreGetIsUsingGetters(IntegrationTester $I)
+    public function mvcModelUnderscorePrivatePropertyException(IntegrationTester $I)
     {
-        $I->wantToTest("Mvc\Model - __get() whether it is using getters correctly");
-
-        $model = new Models\Select();
-        $model->setId(123);
-
-        $I->assertEquals(
-            123,
-            $model->id
-        );
-
-        $associativeArray = [
-            'firstName' => 'First name',
-            'lastName'  => 'Last name',
-        ];
-
-        $model->setName($associativeArray);
-
-        $I->assertEquals(
-            $associativeArray,
-            $model->name
-        );
-
-        $model->setText('MyText');
-
-        $I->assertEquals(
-            'MyText',
-            $model->text
-        );
-    }
-
-    /**
-     * Tests Phalcon\Mvc\Model :: __get() related records
-     *
-     * @author Balázs Németh <https://github.com/zsilbi>
-     * @since  2019-05-07
-     */
-    public function mvcModelUnderscoreGetRelated(IntegrationTester $I)
-    {
-        $I->wantToTest('Mvc\Model - __get() related records');
+        $I->wantToTest('Mvc\Model - __get() private property - exception');
 
         /**
-         * Belongs-to relationship
+         * Setup the table
          */
-        $robotPart = Models\RobotsParts::findFirst();
+        (new InvoicesMigration())($this->container->get('db'));
 
-        $part = $robotPart->part;
-
-        $I->assertInstanceOf(
-            Models\Parts::class,
-            $part
-        );
-
-        /**
-         * Testing has-one relationship
-         */
-        $customer = Models\Customers::findFirst();
-
-        $user = $customer->user;
-
-        $I->assertInstanceOf(
-            Models\Users::class,
-            $user
-        );
-
-        /**
-         * Has-many relationship
-         */
-        $robot = Models\Robots::findFirst();
-
-        $robotParts = $robot->robotsParts;
-
-        $I->assertInstanceOf(
-            \Phalcon\Mvc\Model\Resultset\Simple::class,
-            $robotParts
-        );
-    }
-
-    /**
-     * Tests Phalcon\Mvc\Model :: __get() dirty related records
-     *
-     * @author Balázs Németh <https://github.com/zsilbi>
-     * @since  2019-05-07
-     */
-    public function mvcModelUnderscoreGetDirtyRelated(IntegrationTester $I)
-    {
-        $I->wantToTest('Mvc\Model - __get() dirty related records');
-
-        $robot = Models\Robots::findFirst();
-
-        /**
-         * Fill up the related cache with data
-         */
-        $robotsParts = $robot->robotsParts;
-
-        $I->assertInstanceOf(
-            \Phalcon\Mvc\Model\Resultset\Simple::class,
-            $robotsParts
-        );
-
-        /**
-         * Add new related records
-         */
-        $robot->robotsParts = [
-            new Models\RobotsParts(),
-            new Models\RobotsParts(),
-        ];
-
-        /**
-         * Test if the new records were returned
-         */
-        $dirtyRobotsParts = $robot->robotsParts;
-
-        $I->assertInternalType(
-            'array',
-            $dirtyRobotsParts
-        );
-
-        $I->assertCount(
-            2,
-            $dirtyRobotsParts
-        );
-
-        $I->assertInstanceOf(
-            Models\RobotsParts::class,
-            $dirtyRobotsParts[0]
+        $I->expectThrowable(
+            new Exception(
+                "Cannot access property 'superSecret' (not public)."
+            ),
+            function () {
+                $model = new Invoices();
+                $model->superSecret = 123;
+            }
         );
     }
 }
