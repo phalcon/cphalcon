1a74160eb769aadd0d7b960865ade24ca79d5cc8
Anton Vasiliev <aionbase@gmail.com>
2020-01-01 23:54:04 +0000

    #14669 - Implement tests for ToArrayCest

--- tests/integration/Mvc/Model/Resultset/Simple/ToArrayCest.php
+++ tests/integration/Mvc/Model/Resultset/Simple/ToArrayCest.php
@@ -13,22 +13,51 @@ declare(strict_types=1);
 
 namespace Phalcon\Test\Integration\Mvc\Model\Resultset\Simple;
 
+use Phalcon\Mvc\Model\Query\Builder;
 use IntegrationTester;
+use Phalcon\Test\Fixtures\Traits\DiTrait;
+use Phalcon\Test\Models\Robots;
 
 /**
  * Class ToArrayCest
  */
 class ToArrayCest
 {
+    use DiTrait;
+
     /**
      * Tests Phalcon\Mvc\Model\Resultset\Simple :: toArray()
      *
      * @author Phalcon Team <team@phalcon.io>
-     * @since  2018-11-13
+     * @since  2020-01-01
      */
     public function mvcModelResultsetSimpleToArray(IntegrationTester $I)
     {
         $I->wantToTest('Mvc\Model\Resultset\Simple - toArray()');
-        $I->skipTest('Need implementation');
+
+        $this->newDi();
+        $this->setDiModelsManager();
+        $this->setDiModelsMetadata();
+        $this->setDiMysql();
+
+        $rows = (new Builder())
+            ->from([
+                'Robots' => Robots::class,
+            ])
+            ->orderBy('Robots.id DESC')
+            ->columns([
+                'Robots.id',
+                'Robots.type',
+                'robot_name' => 'Robots.name',
+            ])
+            ->getQuery()
+            ->execute();
+
+        //$renamedArray = $rows->toArray();
+        $untouchedArray = $rows->toArray(false);
+
+        $I->assertInternalType('array', $untouchedArray);
+        //$I->assertInternalType('array', $renamedArray);
+        $I->assertArrayHasKey('robot_name', $untouchedArray[0]);
     }
 }
