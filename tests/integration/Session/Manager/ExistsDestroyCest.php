3d40e8e400bee8a1f8a21f1289a2d64785a78a45
Nikolaos Dimopoulos <nikos@niden.net>
2020-01-06 14:54:49 -0500

    [#14688] - Added tests

--- tests/integration/Session/Manager/ExistsDestroyCest.php
+++ tests/integration/Session/Manager/ExistsDestroyCest.php
@@ -32,6 +32,9 @@ class ExistsDestroyCest
     public function sessionManagerExistsDestroy(IntegrationTester $I)
     {
         $I->wantToTest('Session\Manager - exists()/destroy()');
+        $store = $_SESSION ?? [];
+        $_SESSION = [];
+
         $manager = new Manager();
         $files   = $this->getSessionStream();
         $manager->setAdapter($files);
@@ -46,6 +49,8 @@ class ExistsDestroyCest
 
         $actual = $manager->exists();
         $I->assertFalse($actual);
+
+        $_SESSION = $store;
     }
 
     /**
@@ -60,6 +65,9 @@ class ExistsDestroyCest
     public function sessionManagerDestroySuperGlobal(IntegrationTester $I)
     {
         $I->wantToTest('Session\Manager - destroy() - clean $_SESSION');
+        $store = $_SESSION ?? [];
+        $_SESSION = [];
+
         $manager = new Manager();
         $files   = $this->getSessionStream();
         $manager->setAdapter($files);
@@ -71,13 +79,57 @@ class ExistsDestroyCest
         $I->assertTrue($actual);
 
         $manager->set('test1', __METHOD__);
-        $I->assertArrayHasKey('#test1', $_SESSION);
-        $I->assertContains(__METHOD__, $_SESSION['#test1']);
+        $I->assertArrayHasKey('test1', $_SESSION);
+        $I->assertContains(__METHOD__, $_SESSION['test1']);
 
         $manager->destroy();
-        $I->assertArrayNotHasKey('#test1', $_SESSION);
+        $I->assertArrayNotHasKey('test1', $_SESSION);
 
         $actual = $manager->exists();
         $I->assertFalse($actual);
+
+        $_SESSION = $store;
+    }
+
+    /**
+     * Tests Phalcon\Session\Manager :: destroy() - clean $_SESSION with uniquid
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-01-06
+     */
+    public function sessionManagerDestroySuperGlobalUniquid(IntegrationTester $I)
+    {
+        $I->wantToTest('Session\Manager - destroy() - clean $_SESSION with uniquid');
+
+        $store = $_SESSION ?? [];
+        $_SESSION = [];
+
+        $manager = new Manager();
+        $files   = $this->getSessionStream();
+        $manager->setAdapter($files);
+        $manager->setOptions(
+            [
+                'uniqueId' => 'aaa'
+            ]
+        );
+
+        $actual = $manager->start();
+        $I->assertTrue($actual);
+
+        $actual = $manager->exists();
+        $I->assertTrue($actual);
+
+        $manager->set('test1', __METHOD__);
+
+        $I->assertArrayHasKey('aaa#test1', $_SESSION);
+        $I->assertContains(__METHOD__, $_SESSION['aaa#test1']);
+
+        $manager->destroy();
+        $I->assertArrayNotHasKey('aaa#test1', $_SESSION);
+
+        $actual = $manager->exists();
+        $I->assertFalse($actual);
+
+        $_SESSION = $store;
     }
 }
