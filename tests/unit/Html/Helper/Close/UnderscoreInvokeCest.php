0a7edec1d64ef10e2c33f7d22b25c90be80e4501
Nikolaos Dimopoulos <nikos@niden.net>
2020-01-10 18:27:18 -0500

    [#14696] - Added Html\Button and Html\Close with tests

---
+++ tests/unit/Html/Helper/Close/UnderscoreInvokeCest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Test\Unit\Html\Helper\Close;
+
+use Phalcon\Factory\Exception;
+use Phalcon\Escaper;
+use Phalcon\Html\Helper\Close;
+use Phalcon\Html\TagFactory;
+use UnitTester;
+
+class UnderscoreInvokeCest
+{
+    /**
+     * Tests Phalcon\Html\Helper\Close :: __invoke()
+     *
+     * @param UnitTester $I
+     *
+     * @throws Exception
+     * @since  2020-01-05
+     *
+     */
+    public function htmlHelperCloseUnderscoreInvoke(UnitTester $I)
+    {
+        $I->wantToTest('Html\Helper\Close - __invoke()');
+        $escaper = new Escaper();
+        $helper  = new Close($escaper);
+
+        $expected = "</image>";
+        $actual   = $helper("image");
+        $I->assertEquals($expected, $actual);
+
+        $factory  = new TagFactory($escaper);
+        $locator  = $factory->newInstance('close');
+        $expected = "</image>";
+        $actual   = $locator("image");
+        $I->assertEquals($expected, $actual);
+    }
+}
