cea3ab76759c9d10d25bcf6e8c50cfcab822c871
Nikolaos Dimopoulos <nikos@niden.net>
2020-01-10 18:51:07 -0500

    [#14696] - Added generic element helper and label; More tests

---
+++ phalcon/Html/Helper/Element.zep
@@ -0,0 +1,39 @@
+
+/**
+ * This file is part of the Phalcon.
+ *
+ * (c) Phalcon Team <team@phalcon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Html\Helper;
+
+use Phalcon\Html\Exception;
+
+/**
+ * Class Element
+ */
+class Element extends AbstractHelper
+{
+    /**
+     * Produce a tag.
+     *
+     * @param string $tag
+     * @param string $text
+     * @param array  $attributes
+     * @param bool   $raw
+     *
+     * @return string
+     * @throws Exception
+     */
+    public function __invoke(
+        string tag,
+        string text,
+        array attributes = [],
+        bool raw = false
+    ) -> string {
+        return this->renderFullElement(tag, text, attributes, raw);
+    }
+}
---
+++ tests/unit/Html/Helper/Label/UnderscoreInvokeCest.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Test\Unit\Html\Helper\Label;
+
+use Codeception\Example;
+use Phalcon\Factory\Exception as ExceptionAlias;
+use Phalcon\Escaper;
+use Phalcon\Html\Exception;
+use Phalcon\Html\Helper\Label;
+use Phalcon\Html\TagFactory;
+use UnitTester;
+
+class UnderscoreInvokeCest
+{
+    /**
+     * Tests Phalcon\Html\Helper\Label :: __invoke()
+     *
+     * @param UnitTester $I
+     * @param Example    $example
+     *
+     * @throws Exception
+     * @throws ExceptionAlias
+     * @dataProvider getExamples
+     * @since        2020-01-05
+     *
+     */
+    public function htmlHelperLabelUnderscoreInvoke(UnitTester $I, Example $example)
+    {
+        $I->wantToTest('Html\Helper\Label - __invoke()');
+        $escaper = new Escaper();
+        $helper  = new Label($escaper);
+
+        $expected = $example[0];
+        $actual   = $helper($example[1]);
+        $I->assertEquals($expected, $actual);
+
+        $factory  = new TagFactory($escaper);
+        $locator  = $factory->newInstance('label');
+        $expected = $example[0];
+        $actual   = $locator($example[1]);
+        $I->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @return array
+     */
+    private function getExamples(): array
+    {
+        return [
+            [
+                '<label>',
+                [],
+            ],
+            [
+                '<label for="my-name" id="my-id">',
+                [
+                    'id'  => 'my-id',
+                    'for' => 'my-name',
+                ],
+            ],
+            [
+                '<label for="my-name" id="my-id" class="my-class">',
+                [
+                    'class' => 'my-class',
+                    'for'   => 'my-name',
+                    'id'    => 'my-id',
+                ],
+            ],
+        ];
+    }
+}
