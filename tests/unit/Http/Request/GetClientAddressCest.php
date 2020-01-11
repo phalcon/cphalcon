c084fa038398261b0fd41512f4eec41abf969a1b
Nikolaos Dimopoulos <nikos@niden.net>
2020-01-11 00:12:49 -0500

    [#3135] - Added getBestLanguageIso and reorganized a few tests

--- tests/unit/Http/Request/GetClientAddressCest.php
+++ tests/unit/Http/Request/GetClientAddressCest.php
@@ -13,10 +13,13 @@ declare(strict_types=1);
 
 namespace Phalcon\Test\Unit\Http\Request;
 
+use Phalcon\Test\Fixtures\Traits\DiTrait;
 use UnitTester;
 
 class GetClientAddressCest
 {
+    use DiTrait;
+
     /**
      * Tests Phalcon\Http\Request :: getClientAddress()
      *
@@ -27,6 +30,35 @@ class GetClientAddressCest
     {
         $I->wantToTest('Http\Request - getClientAddress()');
 
-        $I->skipTest('Need implementation');
+        $store = $_SERVER ?? [];
+
+        $this->setNewFactoryDefault();
+        $request = $this->container->get('request');
+
+        $_SERVER = [
+            'REMOTE_ADDR'          => '192.168.0.1',
+            'HTTP_X_FORWARDED_FOR' => '192.168.7.21'
+        ];
+
+        $I->assertEquals(
+            '192.168.0.1',
+            $request->getClientAddress()
+        );
+
+        $I->assertEquals(
+            '192.168.7.21',
+            $request->getClientAddress(true)
+        );
+
+        $_SERVER = [
+            'REMOTE_ADDR' => '86.45.89.47, 214.55.34.56'
+        ];
+
+        $I->assertEquals(
+            '86.45.89.47',
+            $request->getClientAddress()
+        );
+
+        $_SERVER = $store;
     }
 }
