bda01155ad5834a3c8716f1373c433a4a04659ad
Nikolaos Dimopoulos <nikos@niden.net>
2020-01-10 20:30:01 -0500

    [#14696] - Corrected test

--- tests/unit/Html/TagFactory/NewInstanceCest.php
+++ tests/unit/Html/TagFactory/NewInstanceCest.php
@@ -17,16 +17,43 @@ use Codeception\Example;
 use Phalcon\Escaper;
 use Phalcon\Factory\Exception;
 use Phalcon\Html\Helper\Anchor;
-use Phalcon\Html\Helper\AnchorRaw;
+use Phalcon\Html\Helper\Base;
 use Phalcon\Html\Helper\Body;
 use Phalcon\Html\Helper\Button;
 use Phalcon\Html\Helper\Close;
 use Phalcon\Html\Helper\Element;
-use Phalcon\Html\Helper\ElementRaw;
 use Phalcon\Html\Helper\Form;
 use Phalcon\Html\Helper\Img;
+use Phalcon\Html\Helper\Input\Color;
+use Phalcon\Html\Helper\Input\Date;
+use Phalcon\Html\Helper\Input\DateTime;
+use Phalcon\Html\Helper\Input\DateTimeLocal;
+use Phalcon\Html\Helper\Input\Email;
+use Phalcon\Html\Helper\Input\File;
+use Phalcon\Html\Helper\Input\Hidden;
+use Phalcon\Html\Helper\Input\Image;
+use Phalcon\Html\Helper\Input\Input;
+use Phalcon\Html\Helper\Input\Month;
+use Phalcon\Html\Helper\Input\Numeric;
+use Phalcon\Html\Helper\Input\Password;
+use Phalcon\Html\Helper\Input\Range;
+use Phalcon\Html\Helper\Input\Search;
+use Phalcon\Html\Helper\Input\Select;
+use Phalcon\Html\Helper\Input\Submit;
+use Phalcon\Html\Helper\Input\Tel;
+use Phalcon\Html\Helper\Input\Text;
+use Phalcon\Html\Helper\Input\Textarea;
+use Phalcon\Html\Helper\Input\Time;
+use Phalcon\Html\Helper\Input\Url;
+use Phalcon\Html\Helper\Input\Week;
 use Phalcon\Html\Helper\Label;
-use Phalcon\Html\Helper\TextArea;
+use Phalcon\Html\Helper\Link;
+use Phalcon\Html\Helper\Meta;
+use Phalcon\Html\Helper\Ol;
+use Phalcon\Html\Helper\Script;
+use Phalcon\Html\Helper\Style;
+use Phalcon\Html\Helper\Title;
+use Phalcon\Html\Helper\Ul;
 use Phalcon\Html\TagFactory;
 use UnitTester;
 
@@ -76,24 +103,44 @@ class NewInstanceCest
     private function getData(): array
     {
         return [
-            ['a', Anchor::class],
-            ['aRaw', AnchorRaw::class],
-            ['body', Body::class],
-            ['button', Button::class],
-            ['close', Close::class],
-            //            ['doctype'],
-            ['element', Element::class],
-            ['elementRaw', ElementRaw::class],
-            ['form', Form::class],
-            //            ['head'],
-            ['img', Img::class],
-            //            ['input'],
-            ['label', Label::class],
-            //            ['link'],
-            //            ['script'],
-            //            ['style'],
-            ['textarea', TextArea::class],
-            //            ['title'],
+            ["a",                  Anchor::class],
+            ["base",               Base::class],
+            ["body",               Body::class],
+            ["button",             Button::class],
+            ["close",              Close::class],
+            ["element",            Element::class],
+            ["form",               Form::class],
+            ["img",                Img::class],
+            ["inputColor",         Color::class],
+            ["inputDate",          Date::class],
+            ["inputDateTime",      DateTime::class],
+            ["inputDateTimeLocal", DateTimeLocal::class],
+            ["inputEmail",         Email::class],
+            ["inputFile",          File::class],
+            ["inputHidden",        Hidden::class],
+            ["inputImage",         Image::class],
+            ["inputInput",         Input::class],
+            ["inputMonth",         Month::class],
+            ["inputNumeric",       Numeric::class],
+            ["inputPassword",      Password::class],
+            ["inputRange",         Range::class],
+            ["inputSelect",        Select::class],
+            ["inputSearch",        Search::class],
+            ["inputSubmit",        Submit::class],
+            ["inputTel",           Tel::class],
+            ["inputText",          Text::class],
+            ["inputTextarea",      Textarea::class],
+            ["inputTime",          Time::class],
+            ["inputUrl",           Url::class],
+            ["inputWeek",          Week::class],
+            ["label",              Label::class],
+            ["link",               Link::class],
+            ["meta",               Meta::class],
+            ["ol",                 Ol::class],
+            ["script",             Script::class],
+            ["style",              Style::class],
+            ["title",              Title::class],
+            ["ul",                 Ul::class],
         ];
     }
 }
