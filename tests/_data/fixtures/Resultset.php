e03565fd6ed6d4604868bfe0abec2fb3f565d0b5
Nikolaos Dimopoulos <nikos@niden.net>
2020-01-25 19:30:50 -0500

    [#14733] - Fixtures for DM

---
+++ tests/_data/fixtures/Resultset.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+* This file is part of the Phalcon Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Test\Fixtures;
+
+class Resultset
+{
+    public $calculated = '';
+
+    public function __construct(string $calculated)
+    {
+        $this->calculated = $calculated;
+    }
+}
