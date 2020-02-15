1a6c10d407c1091db0957252de016bc0b0b2a93d
Jérémy PASTOURET <pastouret.jeremy@gmail.com>
2020-02-15 23:02:18 +0100

    delete raw sql for new tables

--- tests/_data/assets/db/schemas/mysql_schema.sql
+++ tests/_data/assets/db/schemas/mysql_schema.sql
@@ -12793,30 +12793,3 @@ CREATE TABLE `tiny_int_test` (
   `test` tinyint(1) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- NEW SCHEMA TO TEST MULTIPLE SCHEMA
-CREATE SCHEMA IF NOT EXISTS hardware;
-
-drop table if exists hardware.`robot`;
-CREATE TABLE hardware.`robot` (
-  `robot_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `robot_name` VARCHAR(70) NULL,
-  PRIMARY KEY (`robot_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-drop table if exists hardware.`robot_part`;
-CREATE TABLE hardware.`robot_part` (
-  `robot_part_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `robot_part_name` VARCHAR(70) NULL,
-  PRIMARY KEY (`robot_part_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE SCHEMA IF NOT EXISTS app;
-
-drop table if exists app.`robot_to_robot_part`;
-CREATE TABLE app.`robot_to_robot_part` (
-  `robot_id` int(10) unsigned NOT NULL,
-  `robot_part_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`robot_id`, `robot_part_id` )
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
