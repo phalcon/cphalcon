42be210d7e3eb1be17d42e7c86987a406285112d
Anton Vasiliev <aionbase@gmail.com>
2020-03-02 11:45:13 +0000

    Implement MySQL test for describeColumns() method

---
+++ tests/_data/fixtures/Migrations/ComplexDefaultMigration.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Test\Fixtures\Migrations;
+
+class ComplexDefaultMigration extends AbstractMigration
+{
+    protected $table = 'complex_default';
+
+    /**
+     * @param int|null $id
+     * @param string|null $created
+     * @param string|null $updated
+     * @return int
+     */
+    public function insert(
+        $id,
+        string $created = null,
+        string $updated = null
+    ): int {
+        $id  = $id ?: 'null';
+        $sql = <<<SQL
+insert into complex_default (
+    id, created, updated
+) values (
+    {$id}, {$created}, "{$updated}"
+)
+SQL;
+
+        return $this->connection->exec($sql);
+    }
+
+    protected function getSqlMysql(): array
+    {
+        return [
+            "
+drop table if exists `complex_default`;
+            ",
+            "
+create table complex_default
+(
+    `id`          int(10) auto_increment primary key,
+    `created`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+);
+            ",
+        ];
+    }
+
+    protected function getSqlSqlite(): array
+    {
+        return [];
+    }
+
+    protected function getSqlPgsql(): array
+    {
+        return [];
+    }
+
+    protected function getSqlSqlsrv(): array
+    {
+        return [];
+    }
+}
---
+++ tests/database/Db/Adapter/Pdo/DescribeColumnsCest.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Test\Database\Db\Adapter\Pdo;
+
+use Codeception\Example;
+use DatabaseTester;
+use Phalcon\Storage\Exception;
+use Phalcon\Test\Fixtures\Migrations\ComplexDefaultMigration;
+use Phalcon\Test\Fixtures\Traits\DiTrait;
+
+final class DescribeColumnsCest
+{
+    use DiTrait;
+
+    /**
+     * @param DatabaseTester $I
+     * @throws Exception
+     */
+    public function _before(DatabaseTester $I)
+    {
+        $this->setNewFactoryDefault();
+        $this->setDatabase($I);
+    }
+
+    /**
+     * Tests Phalcon\Db\Adapter\Pdo :: describeColumns()
+     *
+     * @dataProvider dataProvider
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-03-02
+     *
+     * @group mysql
+     */
+    public function dbAdapterPdoDescribeColumnsOnUpdate(DatabaseTester $I, Example $example)
+    {
+        $I->wantToTest('Db\Adapter\Pdo - describeColumns()');
+
+        $connection = $I->getConnection();
+        $db = $this->container->get('db');
+
+        $now = date('Y-m-d H:i:s');
+        $migration = new ComplexDefaultMigration($connection);
+        $migration->insert(1, $now, $now);
+
+        $columns = $db->describeColumns($migration->getTable());
+
+        $I->assertSame('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP', $columns[2]->getDefault());
+    }
+}
