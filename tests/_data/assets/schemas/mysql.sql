a199b1fa69615bdb36104c23764ae442b13c29d1
Anton Vasiliev <aionbase@gmail.com>
2020-03-02 12:18:51 +0000

    Fix MySQL syntax

--- tests/_data/assets/schemas/mysql.sql
+++ tests/_data/assets/schemas/mysql.sql
@@ -6,7 +6,7 @@ create table complex_default
 (
     `id`          int(10) auto_increment primary key,
     `created`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updated`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
             
 
--- tests/_data/fixtures/Migrations/ComplexDefaultMigration.php
+++ tests/_data/fixtures/Migrations/ComplexDefaultMigration.php
@@ -51,7 +51,7 @@ create table complex_default
 (
     `id`          int(10) auto_increment primary key,
     `created`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updated`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
             ",
         ];
