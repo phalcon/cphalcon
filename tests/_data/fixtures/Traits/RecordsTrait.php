0cf099f4381f3db04e989385501371ec4f0e8c6e
Jérémy PASTOURET <pastouret.jeremy@gmail.com>
2020-03-08 14:07:44 +0100

    test: resolves some errors - Closes #14874

--- tests/_data/fixtures/Traits/RecordsTrait.php
+++ tests/_data/fixtures/Traits/RecordsTrait.php
@@ -79,7 +79,7 @@ trait RecordsTrait
         $title = uniqid($prefix . '-');
         for ($counter = 1; $counter <= $count; $counter++) {
             $migration->insert(
-                'default',
+                null,
                 $custId,
                 1,
                 $title,
