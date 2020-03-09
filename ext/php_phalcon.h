82bb4d717ffc8222ed6ff08c720d915aa7a9f534
Jérémy PASTOURET <pastouret.jeremy@gmail.com>
2020-03-09 08:46:19 +0100

    fix: some minor error - Closes #14874

--- ext/php_phalcon.h
+++ ext/php_phalcon.h
@@ -14,7 +14,7 @@
 #define PHP_PHALCON_VERSION     "4.0.5"
 #define PHP_PHALCON_EXTNAME     "phalcon"
 #define PHP_PHALCON_AUTHOR      "Phalcon Team and contributors"
-#define PHP_PHALCON_ZEPVERSION  "0.12.16-12256a1"
+#define PHP_PHALCON_ZEPVERSION  "0.12.17-6724dbf"
 #define PHP_PHALCON_DESCRIPTION "Phalcon is a full stack PHP framework, delivered as a PHP extension, offering lower resource consumption and high performance."
 
 typedef struct _zephir_struct_db { 
