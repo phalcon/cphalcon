d0e7c0fa7fcd975f535136a6961eafd869fb0221
Nikolaos Dimopoulos <nikos@niden.net>
2020-03-19 19:37:07 -0400

    [#13655] - Added one more test for get/set

--- tests/unit/Assets/Manager/GetSetCest.php
+++ tests/unit/Assets/Manager/GetSetCest.php
@@ -13,12 +13,36 @@ declare(strict_types=1);
 
 namespace Phalcon\Test\Unit\Assets\Manager;
 
+use Countable;
+use Iterator;
+use Phalcon\Assets\Collection;
 use Phalcon\Assets\Exception;
 use Phalcon\Assets\Manager;
 use UnitTester;
 
-class GetCest
+class GetSetCest
 {
+    /**
+     * Tests Phalcon\Assets\Manager :: get()/set()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-03-19
+     */
+    public function assetsManagerGetSet(UnitTester $I)
+    {
+        $I->wantToTest('Assets\Manager - get()/set()');
+
+        $assets = new Manager();
+
+        $collection = new Collection();
+
+        $assets->set('hangout', $collection);
+        $actual = $assets->get('hangout');
+        $I->assertInstanceOf(Collection::class, $actual);
+        $I->assertInstanceOf(Countable::class, $actual);
+        $I->assertInstanceOf(Iterator::class, $actual);
+    }
+
     /**
      * Tests Phalcon\Assets\Manager :: get() - unknown
      *
