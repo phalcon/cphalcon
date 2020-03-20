7c9a5cb3f7e262cc8cd691720b8c991d6934a49b
Nikolaos Dimopoulos <nikos@niden.net>
2020-03-20 09:28:19 -0400

    PHPCS

--- tests/unit/Assets/Manager/AddInlineCodeByTypeCest.php
+++ tests/unit/Assets/Manager/AddInlineCodeByTypeCest.php
@@ -17,9 +17,11 @@ use Phalcon\Assets\Inline;
 use Phalcon\Assets\Manager;
 use Phalcon\Test\Fixtures\Traits\DiTrait;
 use UnitTester;
+
 use function ob_end_clean;
 use function ob_get_contents;
 use function ob_start;
+
 use const PHP_EOL;
 
 class AddInlineCodeByTypeCest
