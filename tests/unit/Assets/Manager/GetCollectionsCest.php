6d877234672cc1fae09d6e7714a9fe25be19b1fa
Nikolaos Dimopoulos <nikos@niden.net>
2020-03-19 22:18:24 -0400

    [#13655] - Added more tests

--- tests/unit/Assets/Manager/GetCollectionsCest.php
+++ tests/unit/Assets/Manager/GetCollectionsCest.php
@@ -13,6 +13,10 @@ declare(strict_types=1);
 
 namespace Phalcon\Test\Unit\Assets\Manager;
 
+use Countable;
+use Iterator;
+use Phalcon\Assets\Collection;
+use Phalcon\Assets\Manager;
 use UnitTester;
 
 class GetCollectionsCest
@@ -21,12 +25,24 @@ class GetCollectionsCest
      * Tests Phalcon\Assets\Manager :: getCollections()
      *
      * @author Phalcon Team <team@phalcon.io>
-     * @since  2018-11-13
+     * @since  2020-03-19
      */
     public function assetsManagerGetCollections(UnitTester $I)
     {
         $I->wantToTest('Assets\Manager - getCollections()');
 
-        $I->skipTest('Need implementation');
+        $assets = new Manager();
+
+        $assets->set('hangout1', new Collection());
+        $assets->set('hangout2', new Collection());
+
+        $collections = $assets->getCollections();
+
+        $I->assertCount(2, $collections);
+        foreach ($collections as $collection) {
+            $I->assertInstanceOf(Collection::class, $collection);
+            $I->assertInstanceOf(Countable::class, $collection);
+            $I->assertInstanceOf(Iterator::class, $collection);
+        }
     }
 }
