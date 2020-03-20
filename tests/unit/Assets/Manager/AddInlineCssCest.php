f6b90d6c3e8fa38c585f5a540d368bcd1fc9fe26
Nikolaos Dimopoulos <nikos@niden.net>
2020-03-19 22:18:24 -0400

    [#13655] - Added more tests

--- tests/unit/Assets/Manager/AddInlineCssCest.php
+++ tests/unit/Assets/Manager/AddInlineCssCest.php
@@ -13,20 +13,61 @@ declare(strict_types=1);
 
 namespace Phalcon\Test\Unit\Assets\Manager;
 
+use Phalcon\Assets\Manager;
+use Phalcon\Test\Fixtures\Traits\DiTrait;
 use UnitTester;
+
+use function ob_end_clean;
+use function ob_get_contents;
+use function ob_start;
+
+use const PHP_EOL;
 
 class AddInlineCssCest
 {
+    use DiTrait;
+
+    /**
+     * executed before each test
+     */
+    public function _before(UnitTester $I)
+    {
+        $this->newDi();
+        $this->setDiService('escaper');
+        $this->setDiService('url');
+    }
+
+    /**
+     * executed after each test
+     */
+    public function _after(UnitTester $I)
+    {
+        $this->resetDi();
+    }
+
     /**
      * Tests Phalcon\Assets\Manager :: addInlineCss()
      *
      * @author Phalcon Team <team@phalcon.io>
-     * @since  2018-11-13
+     * @since  2020-03-19
      */
     public function assetsManagerAddInlineCss(UnitTester $I)
     {
         $I->wantToTest('Assets\Manager - addInlineCss()');
 
-        $I->skipTest('Need implementation');
+        $manager = new Manager();
+
+        $css = ".h2:after, .h2:after { content: ''; display: none }";
+
+        $manager->addInlineCss($css);
+
+        $expected = "<style type=\"text/css\">{$css}</style>" . PHP_EOL;
+
+        ob_start();
+        $manager->outputInlineCss();
+        $actual = ob_get_contents();
+        ob_end_clean();
+
+        $I->assertSame($expected, $actual);
     }
 }
