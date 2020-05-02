9af3c294f7a4d363d8fc92fef3ef24c23b751f2d
Anton Vasiliev <aionbase@gmail.com>
2020-05-02 10:23:31 +0100

    Enhance Dialect\Mysql tests

--- tests/integration/Db/Dialect/Mysql/ModifyColumnCest.php
+++ tests/integration/Db/Dialect/Mysql/ModifyColumnCest.php
@@ -23,7 +23,7 @@ class ModifyColumnCest
      * Tests Phalcon\Db\Adapter\Pdo\Mysql :: modifyColumn()
      *
      * @author Phalcon Team <team@phalcon.io>
-     * @since  2020-02-27
+     * @since  2020-05-02
      */
     public function dbAdapterPdoMysqlModifyColumn(IntegrationTester $I)
     {
@@ -36,20 +36,42 @@ class ModifyColumnCest
                     [
                         'type'    => Column::TYPE_TIMESTAMP,
                         'default' => "CURRENT_TIMESTAMP",
-                        'notNull' => false,
+                        'notNull' => true,
                         'after'   => 'created_at',
                     ]
-                ), new Column(
+                ),
+                new Column(
                     'updated_at',
                     [
                         'type'    => Column::TYPE_TIMESTAMP,
                         'default' => "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
-                        'notNull' => false,
+                        'notNull' => true,
                         'after'   => 'created_at',
                     ]
                 ),
-                'ALTER TABLE `test` MODIFY `updated_at` TIMESTAMP ' .
+                'ALTER TABLE `test` MODIFY `updated_at` TIMESTAMP NOT NULL ' .
                 'DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP AFTER `created_at`',
+            ],
+            [
+                new Column(
+                    'deleted_at',
+                    [
+                        'type'    => Column::TYPE_TIMESTAMP,
+                        'default' => "CURRENT_TIMESTAMP",
+                        'notNull' => false,
+                        'after'   => 'updated_at',
+                    ]
+                ),
+                new Column(
+                    'deleted_at',
+                    [
+                        'type'    => Column::TYPE_TIMESTAMP,
+                        'default' => "NULL",
+                        'notNull' => false,
+                        'after'   => 'updated_at',
+                    ]
+                ),
+                'ALTER TABLE `test` MODIFY `deleted_at` TIMESTAMP NULL DEFAULT NULL AFTER `updated_at`',
             ],
             [
                 new Column(
@@ -59,7 +81,8 @@ class ModifyColumnCest
                         'notNull' => true,
                         'after'   => 'updated_at',
                     ]
-                ), new Column(
+                ),
+                new Column(
                     'numeric_val',
                     [
                         'type'    => Column::TYPE_FLOAT,
@@ -68,7 +91,7 @@ class ModifyColumnCest
                         'after'   => 'updated_at',
                     ]
                 ),
-                'ALTER TABLE `test` MODIFY `numeric_val` FLOAT DEFAULT 21.42 AFTER `updated_at`',
+                'ALTER TABLE `test` MODIFY `numeric_val` FLOAT NULL DEFAULT 21.42 AFTER `updated_at`',
             ],
         ];
 
