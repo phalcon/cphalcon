1a5835586e24f6b96f04b2bb9cba29c9a7b3add6
Balázs Németh <zsilbi@zsilbi.hu>
2020-10-07 23:12:31 +0200

    test(model): schema regen

--- tests/_data/assets/schemas/sqlite.sql
+++ tests/_data/assets/schemas/sqlite.sql
@@ -2,8 +2,29 @@
 
 
 
-drop table if exists co_customers;
+drop table if exists co_customers_defaults;
+            
+create table co_customers_defaults
+(
+    cst_id          integer constraint co_customers_defaults_pk primary key autoincrement,
+    cst_status_flag integer      null DEFAULT 1,
+    cst_name_last   text         null DEFAULT 'cst_default_lastName',
+    cst_name_first  text         null DEFAULT 'cst_default_firstName'
+);
+            
+create index co_customers_defaults_cst_status_flag_index
+    on co_customers_defaults (cst_status_flag);
+            
+create index co_customers_defaults_cst_name_last_index
+    on co_customers_defaults (cst_name_last);
+            
+create index co_customers_defaults_cst_name_first_index
+    on co_customers_defaults (cst_name_first);
+            
 
+
+drop table if exists co_customers;
+            
 create table co_customers
 (
     cst_id          integer constraint co_customers_pk primary key autoincrement,
@@ -11,22 +32,22 @@ create table co_customers
     cst_name_last   text         null,
     cst_name_first  text         null
 );
-
+            
 create index co_customers_cst_status_flag_index
     on co_customers (cst_status_flag);
-
+            
 create index co_customers_cst_name_last_index
     on co_customers (cst_name_last);
-
+            
 create index co_customers_cst_name_first_index
     on co_customers (cst_name_first);
-
+            
 
 
 
 
 drop table if exists co_invoices;
-
+            
 create table co_invoices
     (
     inv_id          integer constraint co_invoices_pk primary key autoincrement not null,
@@ -36,27 +57,27 @@ create table co_invoices
     inv_total       real,
     inv_created_at  text
 );
-
+            
 create index co_invoices_inv_cst_id_index
     on co_invoices (inv_cst_id);
-
+            
 create index co_invoices_inv_status_flag_index
     on co_invoices (inv_status_flag);
-
+            
 create index co_invoices_inv_created_at_index
     on co_invoices (inv_created_at);
-
+            
 
 
 drop table if exists `objects`;
-
+            
 create table objects
     (
     obj_id     integer constraint objects_pk primary key autoincrement,
     obj_name   text not null,
     obj_type   integer not null
 );
-
+            
 
 
 
@@ -66,35 +87,35 @@ create table objects
 
 
 drop table if exists co_sources;
-
+            
 create table co_sources
     (
     id          integer constraint co_sources_pk primary key autoincrement,
     username    text,
     source      text
 );
-
+            
 create index co_sources_username_index
     on co_sources (username);
-
+            
 
 
 drop table if exists table_with_uuid_primary;
-
+            
 create table table_with_uuid_primary
 (
     uuid        text constraint uuid_pk primary key,
     int_field   integer
 );
-
+            
 
 
 drop table if exists stuff;
-
+            
 create table stuff
 (
     stf_id      integer constraint stf_id_pk primary key autoincrement,
     stf_name    text not null,
     stf_type    integer not null
 );
-
+            
