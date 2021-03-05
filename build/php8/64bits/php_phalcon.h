d60903f03740f7c72ab13f8a6a7cd029aeef955b
Anton Vasiliev <aionbase@gmail.com>
2021-03-05 09:43:06 +0000

    #15186 - Regenerate build/php8/ folder

--- build/php8/64bits/php_phalcon.h
+++ build/php8/64bits/php_phalcon.h
@@ -115,7 +115,7 @@ typedef zend_function zephir_fcall_cache_entry;
 #define PHP_PHALCON_VERSION     "4.1.0"
 #define PHP_PHALCON_EXTNAME     "phalcon"
 #define PHP_PHALCON_AUTHOR      "Phalcon Team and contributors"
-#define PHP_PHALCON_ZEPVERSION  "0.12.20-5d0f025"
+#define PHP_PHALCON_ZEPVERSION  "0.12.20-f6197948"
 #define PHP_PHALCON_DESCRIPTION "Phalcon is a full stack PHP framework, delivered as a PHP extension, offering lower resource consumption and high performance."
 
 typedef struct _zephir_struct_db {
