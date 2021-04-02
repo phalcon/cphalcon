07e9ef4f2843a50e4116a07511fc0284ed530be7
AlexNDRmac <alexndr.mac@gmail.com>
2021-04-02 17:00:40 +0300

    Fix tests

--- tests/unit/Acl/Adapter/Memory/AllowCest.php
+++ tests/unit/Acl/Adapter/Memory/AllowCest.php
@@ -278,7 +278,7 @@ class AllowCest
     {
         $errorMessage = "You did not provide any parameters when 'Guests' can " .
                         "'update' 'Post'. We will use default action when no arguments. " .
-                        "at tests/unit/Acl/Adapter/Memory/AllowCest.php:329";
+                        "at tests/unit/Acl/Adapter/Memory/AllowCest.php:332";
 
         if (PHP_OS_FAMILY === 'Windows') {
             $errorMessage = str_replace('/', '\\', $errorMessage);
--- tests/unit/Acl/Adapter/Memory/IsAllowedCest.php
+++ tests/unit/Acl/Adapter/Memory/IsAllowedCest.php
@@ -205,7 +205,7 @@ class IsAllowedCest
         $errorMessage = 'Number of parameters in array is higher than the ' .
                         'number of parameters in defined function when checking if ' .
                         "'Members' can 'update' 'Post'. Extra parameters will be ignored. " .
-                        "at tests/unit/Acl/Adapter/Memory/IsAllowedCest.php:239";
+                        "at tests/unit/Acl/Adapter/Memory/IsAllowedCest.php:242";
 
         if (PHP_OS_FAMILY === 'Windows') {
             $errorMessage = str_replace('/', '\\', $errorMessage);
