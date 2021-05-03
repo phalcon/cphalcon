ac389a448313d5100b550e1c821c7f01b92de53b
Anton Vasiliev <aionbase@gmail.com>
2021-05-03 23:17:22 +0100

    #15456 - Add missed build/config directory

---
+++ build/config/phalcon_c_skip_files.php
@@ -0,0 +1,37 @@
+<?php
+// These are the files that must not be added to phalcon.c
+
+return [
+    // Included by phalcon.c header - see phalcon_c_header.php
+    'config.h',
+    'ext_config.h',
+    'php.h',
+    'ext.h',
+    'php_phalcon.h',
+    'phalcon.h',
+    'phalcon.c',
+
+    'phalcon/annotations/scanner.h',
+    'phalcon/annotations/annot.h',
+    'phalcon/url/utils.h',
+    'phalcon/mvc/model/orm.h',
+    'phalcon/mvc/model/query/scanner.h',
+    'phalcon/mvc/model/query/phql.h',
+    'phalcon/mvc/view/engine/volt/scanner.h',
+    'phalcon/mvc/view/engine/volt/volt.h',
+
+    'phalcon/annotations/base.c',
+    'phalcon/annotations/lempar.c',
+    'phalcon/annotations/lemon.c',
+    'phalcon/annotations/parser.php.c',
+
+    'phalcon/mvc/view/engine/volt/base.c',
+    'phalcon/mvc/view/engine/volt/lempar.c',
+    'phalcon/mvc/view/engine/volt/lemon.c',
+    'phalcon/mvc/view/engine/volt/parser.php.c',
+
+    'phalcon/mvc/model/query/base.c',
+    'phalcon/mvc/model/query/lempar.c',
+    'phalcon/mvc/model/query/lemon.c',
+    'phalcon/mvc/model/query/parser.php.c',
+];
