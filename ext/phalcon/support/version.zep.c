6e29971922be53b225aa2f58637d849317900a56
Nikolaos Dimopoulos <nikos@niden.net>
2021-05-05 18:09:44 -0400

    Corrected version/regenerated build and ext
    Refs [#.0.x]

--- ext/phalcon/support/version.zep.c
+++ ext/phalcon/support/version.zep.c
@@ -120,10 +120,10 @@ PHP_METHOD(Phalcon_Support_Version, getVersion)
 	ZVAL_LONG(&_0, 0);
 	zephir_array_fast_append(return_value, &_0);
 	ZEPHIR_INIT_NVAR(&_0);
-	ZVAL_LONG(&_0, 2);
+	ZVAL_LONG(&_0, 1);
 	zephir_array_fast_append(return_value, &_0);
 	ZEPHIR_INIT_NVAR(&_0);
-	ZVAL_LONG(&_0, 0);
+	ZVAL_LONG(&_0, 2);
 	zephir_array_fast_append(return_value, &_0);
 	RETURN_MM();
 }
