b0782dbf2e3e8ed37cfdb151e33050d4fc708b0d
Stijn Leenknegt <stijn@diagro.be>
2021-07-25 18:39:58 +0200

    Update FormEntityCest.php

--- tests/unit/Forms/FormEntityCest.php
+++ tests/unit/Forms/FormEntityCest.php
@@ -4,7 +4,7 @@ namespace Phalcon\Test\Unit\Forms;
 use Phalcon\Forms\Element\Text;
 use Phalcon\Forms\Form;
 use Phalcon\Test\Fixtures\Traits\DiTrait;
-use Phalcon\Test\Models\Users;
+use Phalcon\Test\Models\Products;
 use Phalcon\Validation;
 use Phalcon\Validation\Validator\StringLength\Max;
 use UnitTester;
@@ -22,27 +22,27 @@ class FormEntityCest
      * @author Stijn Leenknegt <stijn@diagro.be>
      * @since  2021-07-10
      */
-    public function helperArrIsUnique(UnitTester $I)
+    public function isValidEntity(UnitTester $I)
     {
         $I->wantToTest('Phalcon\Forms\Form  - isValid()');
 
         $validator = new Validation();
-        $validator->add('name', new Max(['max' => 15]));
+        $validator->add('prd_name', new Max(['max' => 15]));
 
-        $user = new Users();
+        $product = new Products();
 
-        $form = new Form($user);
+        $form = new Form($product);
         $form->setValidation($validator);
-        $name = new Text('name');
+        $name = new Text('prd_name');
         $form->add($name);
 
-        $isValid = $form->isValid(['name' => "Nikodem Tomlinson"]);
+        $isValid = $form->isValid(['prd_name' => "Nikodem Tomlinson"]); //normal this is from _POST
 
         $I->assertFalse($isValid);
 
         //Validation.zep line 456 is called, so the isValid method calls validate and sets the same entity as the form
         $I->assertNotNull($validator->getEntity());
-        $I->assertEquals($user->name, $validator->getEntity()->name);
+        $I->assertEquals($product->prd_name, $validator->getEntity()->prd_name);
     }
 
 }
