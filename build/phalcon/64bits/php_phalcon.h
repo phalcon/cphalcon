c3190e6f87abb5ffe26808fab827be47e7e1b33f
Nikolaos Dimopoulos <nikos@niden.net>
2021-09-05 12:34:11 -0400

    Generating optimized files
    Refs [#.0.x]

--- build/phalcon/64bits/php_phalcon.h
+++ build/phalcon/64bits/php_phalcon.h
@@ -103,7 +103,7 @@ typedef zend_function zephir_fcall_cache_entry;
 
 
 #define PHP_PHALCON_NAME        "phalcon"
-#define PHP_PHALCON_VERSION     "5.0.0alpha3"
+#define PHP_PHALCON_VERSION     "5.0.0alpha4"
 #define PHP_PHALCON_EXTNAME     "phalcon"
 #define PHP_PHALCON_AUTHOR      "Phalcon Team and contributors"
 #define PHP_PHALCON_ZEPVERSION  "0.13.5-1232784"
