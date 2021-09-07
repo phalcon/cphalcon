1833b563a8af192649b1d89d300e68b5a3e5a425
Anton Vasiliev <aionbase@gmail.com>
2021-09-07 20:46:04 +0100

    #15647 - Unify all arches into single
    This is because all arches contain identical code.

--- build/util/Generator.php
+++ build/util/Generator.php
@@ -1,59 +1,114 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Phalcon\Build;
 
 class Generator
 {
     /**
-     * Root dir of Phalcon
+     * Directory with Phalcon source code
      *
      * @var string
      */
-    protected string $rootDir;
+    protected string $sourceDir;
 
     /**
-     * Directory for 'safe' build
+     * Directory, where source code for building Phalcon will be put
      *
      * @var string
      */
-    protected string $safeDir;
+    protected string $outputDir;
 
     /**
-     * Directory for build, optimized for 32-bit platform
+     * Generator for phalcon.h
      *
-     * @var string
+     * @var Generator_File_PhalconH
      */
-    protected string $optimized32Dir;
+    protected Generator_File_PhalconH $phalconH;
 
     /**
-     * Directory for build, optimized for 64-bit platform
+     * Generator for phalcon.c
      *
-     * @var string
+     * @var Generator_File_PhalconC
      */
-    protected string $optimized64Dir;
+    protected Generator_File_PhalconC $phalconC;
+
+    /**
+     * Generator for config.m4
+     *
+     * @var Generator_File_ConfigM4
+     */
+    protected Generator_File_ConfigM4 $configM4;
+
+    /**
+     * Generator for config.w32
+     *
+     * @var Generator_File_ConfigW32
+     */
+    protected Generator_File_ConfigW32 $configW32;
 
     /**
      * @param string $rootDir
-     * @param string $buildDir
+     * @param string $outputDir
      */
-    public function __construct(string $rootDir, string $buildDir)
+    public function __construct(string $rootDir, string $outputDir)
     {
-        $this->rootDir = $rootDir;
-        $this->safeDir = $buildDir . '/safe';
+        $this->sourceDir = $rootDir . '/ext';
+        $this->outputDir = $outputDir;
 
-        $this->optimized32Dir = $buildDir . '/32bits';
-        $this->optimized64Dir = $buildDir . '/64bits';
+        $configDir = $rootDir . '/build/config';
+
+        $this->phalconH = new Generator_File_PhalconH($this->sourceDir, $outputDir);
+        $this->phalconC = new Generator_File_PhalconC($rootDir, $this->sourceDir, $configDir, $outputDir);
+        $this->configM4 = new Generator_File_ConfigM4($rootDir, $outputDir);
+        $this->configW32 = new Generator_File_ConfigW32($rootDir, $outputDir);
     }
 
     /**
-     * Generate safe and optimized builds
+     * Generator source code, from which Phalcon extension can be built
      */
     public function run()
     {
-        $generator = new Generator_Safe($this->rootDir, $this->safeDir);
-        $generator->run();
+        echo 'Generating build... ';
 
-        $generatorOptimized = new Generator_Optimized($this->safeDir, $this->optimized32Dir, $this->optimized64Dir);
-        $generatorOptimized->run();
+        Util::cleanDirectory($this->outputDir);
+        $this->generateFiles();
+
+        echo "OK\n";
+    }
+
+    /**
+     * Generate all the files in the output directory
+     */
+    protected function generateFiles()
+    {
+        $includedHeaderFiles = $this->phalconH->generate();
+        $this->phalconC->generate($includedHeaderFiles);
+
+        $this->configM4->generate();
+        $this->configW32->generate();
+
+        copy($this->sourceDir . '/php_phalcon.h', $this->outputDir . '/php_phalcon.h');
+        $this->processKernelGlobals();
+    }
+
+    /**
+     * Resolves headers in the php_phalcon.h file
+     */
+    protected function processKernelGlobals()
+    {
+        $lines = [];
+
+        foreach (file($this->outputDir . '/php_phalcon.h') as $line) {
+            if (preg_match('@^#include "(kernel/.+)"@', $line, $matches)) {
+                $content = file_get_contents($this->sourceDir . DIRECTORY_SEPARATOR . $matches[1]);
+                $lines[] = $content . PHP_EOL;
+            } else {
+                $lines[] = $line;
+            }
+        }
+
+        file_put_contents($this->outputDir . '/php_phalcon.h', join('', $lines));
     }
 }
