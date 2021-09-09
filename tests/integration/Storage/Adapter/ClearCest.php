30bc9c808ce0ec7c3270927d43eb80b8b8a96e10
Nikolaos Dimopoulos <nikos@niden.net>
2021-09-09 14:31:30 -0400

    refactored some of the tests for storage
    Refs [#15480]

---
+++ tests/integration/Storage/Adapter/ClearCest.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Test\Integration\Storage\Adapter;
+
+use Codeception\Example;
+use Phalcon\Storage\Adapter\Apcu;
+use Phalcon\Storage\Adapter\Libmemcached;
+use Phalcon\Storage\Adapter\Memory;
+use Phalcon\Storage\Adapter\Redis;
+use Phalcon\Storage\Adapter\Stream;
+use Phalcon\Storage\SerializerFactory;
+use IntegrationTester;
+
+use function array_merge;
+use function getOptionsLibmemcached;
+use function getOptionsRedis;
+use function outputDir;
+use function uniqid;
+
+class ClearCest
+{
+    /**
+     * Tests Phalcon\Storage\Adapter\* :: clear()
+     *
+     * @dataProvider getExamples
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-09-09
+     */
+    public function storageAdapterClear(IntegrationTester $I, Example $example)
+    {
+        $I->wantToTest(
+            'Storage\Adapter\'' . $example['className'] . ' - getPrefix()'
+        );
+
+        $extension  = $example['extension'];
+        $class      = $example['class'];
+        $options    = $example['options'];
+
+        if (!empty($extension)) {
+            $I->checkExtensionIsLoaded($extension);
+        }
+
+        $serializer = new SerializerFactory();
+        $adapter = new $class($serializer, $options);
+
+        $key1 = uniqid();
+        $key2 = uniqid();
+        $adapter->set($key1, 'test');
+        $actual = $adapter->has($key1);
+        $I->assertTrue($actual);
+
+        $adapter->set($key2, 'test');
+        $actual = $adapter->has($key2);
+        $I->assertTrue($actual);
+
+        $actual = $adapter->clear();
+        $I->assertTrue($actual);
+
+        $actual = $adapter->has($key1);
+        $I->assertFalse($actual);
+
+        $actual = $adapter->has($key2);
+        $I->assertFalse($actual);
+
+        /**
+         * Call clear twice to ensure it returns true
+         */
+        $actual = $adapter->clear();
+        $I->assertTrue($actual);
+    }
+
+    /**
+     * @return array[]
+     */
+    private function getExamples(): array
+    {
+        return [
+            [
+                'className' => 'Apcu',
+                'class'     => Apcu::class,
+                'options'   => [],
+                'extension' => 'apcu',
+            ],
+            [
+                'className' => 'Libmemcached',
+                'class'     => Libmemcached::class,
+                'options'   => getOptionsLibmemcached(),
+                'extension' => 'memcached',
+            ],
+            [
+                'className' => 'Memory',
+                'label'     => 'default',
+                'class'     => Memory::class,
+                'options'   => [],
+                'extension' => '',
+            ],
+            [
+                'className' => 'Redis',
+                'label'     => 'default',
+                'class'     => Redis::class,
+                'options'   => getOptionsRedis(),
+                'extension' => 'redis',
+            ],
+            [
+                'className' => 'Stream',
+                'label'     => 'default',
+                'class'     => Stream::class,
+                'options'   => [
+                    'storageDir' => outputDir(),
+                ],
+                'extension' => '',
+            ],
+        ];
+    }
+}
---
+++ tests/integration/Storage/Adapter/HasCest.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Test\Integration\Storage\Adapter;
+
+use Codeception\Example;
+use Phalcon\Storage\Adapter\Apcu;
+use Phalcon\Storage\Adapter\Libmemcached;
+use Phalcon\Storage\Adapter\Memory;
+use Phalcon\Storage\Adapter\Redis;
+use Phalcon\Storage\Adapter\Stream;
+use Phalcon\Storage\SerializerFactory;
+use IntegrationTester;
+
+use function array_merge;
+use function getOptionsLibmemcached;
+use function getOptionsRedis;
+use function outputDir;
+use function uniqid;
+
+class HasCest
+{
+    /**
+     * Tests Phalcon\Storage\Adapter\* :: has()
+     *
+     * @dataProvider getExamples
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-09-09
+     */
+    public function storageAdapterHas(IntegrationTester $I, Example $example)
+    {
+        $I->wantToTest(
+            'Storage\Adapter\'' . $example['className'] . ' - has()'
+        );
+
+        $extension  = $example['extension'];
+        $class      = $example['class'];
+        $options    = $example['options'];
+
+        if (!empty($extension)) {
+            $I->checkExtensionIsLoaded($extension);
+        }
+
+        $serializer = new SerializerFactory();
+        $adapter = new $class($serializer, $options);
+
+        $key = uniqid();
+
+        $actual = $adapter->has($key);
+        $I->assertFalse($actual);
+
+        $adapter->set($key, 'test');
+        $actual = $adapter->has($key);
+        $I->assertTrue($actual);
+    }
+
+    /**
+     * @return array[]
+     */
+    private function getExamples(): array
+    {
+        return [
+            [
+                'className' => 'Apcu',
+                'class'     => Apcu::class,
+                'options'   => [],
+                'extension' => 'apcu',
+            ],
+            [
+                'className' => 'Libmemcached',
+                'class'     => Libmemcached::class,
+                'options'   => getOptionsLibmemcached(),
+                'extension' => 'memcached',
+            ],
+            [
+                'className' => 'Memory',
+                'label'     => 'default',
+                'class'     => Memory::class,
+                'options'   => [],
+                'extension' => '',
+            ],
+            [
+                'className' => 'Redis',
+                'label'     => 'default',
+                'class'     => Redis::class,
+                'options'   => getOptionsRedis(),
+                'extension' => 'redis',
+            ],
+            [
+                'className' => 'Stream',
+                'label'     => 'default',
+                'class'     => Stream::class,
+                'options'   => [
+                    'storageDir' => outputDir(),
+                ],
+                'extension' => '',
+            ],
+        ];
+    }
+}
