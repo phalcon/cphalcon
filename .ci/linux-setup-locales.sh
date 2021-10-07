cbe1ff370d8c3c5b8d263f83bbbfd2e9ea8ee9ba
AlexNDRmac <alexndr.mac@gmail.com>
2021-10-07 13:51:53 +0300

    Add macOS build action

--- .ci/linux-setup-locales.sh
+++ .ci/linux-setup-locales.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+#
+# This file is part of the Phalcon Framework.
+#
+# (c) Phalcon Team <team@phalcon.io>
+#
+# For the full copyright and license information, please view the
+# LICENSE.txt file that was distributed with this source code.
 
-## Setup locales for Codeception tests with Extension 'gettext'
+# Setup locales for Codeception tests with Extension 'gettext'
 sudo sudo apt-get install tzdata locales -y
 sudo sed -i -e 's/# de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen
 sudo sed -i -e 's/# el_GR.UTF-8 UTF-8/el_GR.UTF-8 UTF-8/' /etc/locale.gen
