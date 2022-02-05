8ff6f9d514f210e1ad36f27a2c1fde50eb8ac2e2
Nikolaos Dimopoulos <nikos@niden.net>
2022-02-05 13:35:58 -0500

    corrected return type for findFirst()

--- tests/_data/fixtures/models/InvoicesExtended.php
+++ tests/_data/fixtures/models/InvoicesExtended.php
@@ -13,21 +13,20 @@ declare(strict_types=1);
 
 namespace Phalcon\Tests\Models;
 
+use Phalcon\Mvc\Model\Row;
 use Phalcon\Mvc\ModelInterface;
 
 use function is_string;
 
-/**
- * Class InvoicesExtended
- */
+
 class InvoicesExtended extends Invoices
 {
     /**
      * @param array|string|null $parameters
      *
-     * @return ModelInterface|null
+     * @return ModelInterface|Row|null
      */
-    public static function findFirst($parameters = null): ?ModelInterface
+    public static function findFirst($parameters = null)
     {
         if (is_string($parameters)) {
             return parent::findFirstByInvId($parameters);
