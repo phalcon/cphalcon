101cf146c1687457141df5879d7a90dee71bd774
Nikolaos Dimopoulos <nikos@niden.net>
2022-07-02 10:59:37 -0400

    Revert "[#15984] - adjusted tests for the new change (double quotes)"
    This reverts commit ceaeb6ec9f7d692533253c9ea3336e360cb6e64d.

--- tests/integration/Mvc/View/Engine/Volt/Compiler/ExpressionCest.php
+++ tests/integration/Mvc/View/Engine/Volt/Compiler/ExpressionCest.php
@@ -14,59 +14,21 @@ declare(strict_types=1);
 namespace Phalcon\Tests\Integration\Mvc\View\Engine\Volt\Compiler;
 
 use IntegrationTester;
-use Phalcon\Mvc\View\Engine\Volt\Compiler;
-use Phalcon\Tests\Fixtures\Traits\DiTrait;
 
 /**
  * Class ExpressionCest
  */
 class ExpressionCest
 {
-    use DiTrait;
-
     /**
      * Tests Phalcon\Mvc\View\Engine\Volt\Compiler :: expression()
      *
      * @author Phalcon Team <team@phalcon.io>
-     * @since  2022-07-01
+     * @since  2018-11-13
      */
     public function mvcViewEngineVoltCompilerExpression(IntegrationTester $I)
     {
         $I->wantToTest('Mvc\View\Engine\Volt\Compiler - expression()');
-
-        $this->setNewFactoryDefault();
-
-        $volt = new Compiler();
-        $volt->setDI($this->container);
-
-        // title("\r\n", "\n\n")
-        $source   = [
-            [
-                'expr' => [
-                    'type' => 260,
-                    'value' => "\t",
-                    'file' => 'eval code',
-                    'line' => 1,
-                ],
-                'file' => 'eval code',
-                'line' => 1,
-            ],
-            [
-                'expr' => [
-                    'type' => 260,
-                    'value' => "\n\n",
-                    'file' => 'eval code',
-                    'line' => 1,
-                ],
-                'file' => 'eval code',
-                'line' => 1,
-            ]
-        ];
-
-        $expected = "\"\t\", \"\n\n\"";
-        $actual   = $volt->expression($source);
-
-        $I->assertEquals($expected, $actual);
-
+        $I->skipTest('Need implementation');
     }
 }
