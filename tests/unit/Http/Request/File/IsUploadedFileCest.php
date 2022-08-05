b3dbcedaaab3a6a739913f502522dc065d68f63f
Nikolaos Dimopoulos <nikos@niden.net>
2022-08-05 12:17:18 -0400

    [#15977] - renaming of new assets

--- tests/unit/Http/Request/File/IsUploadedFileCest.php
+++ tests/unit/Http/Request/File/IsUploadedFileCest.php
@@ -35,7 +35,7 @@ class IsUploadedFileCest
             [
                 'name'     => 'test',
                 'type'     => 'text/plain',
-                'tmp_name' => dataDir('/assets/images/phalconphp.jpg'),
+                'tmp_name' => dataDir('/assets/images/example-jpg.jpg'),
                 'size'     => 1,
                 'error'    => 0,
             ]
