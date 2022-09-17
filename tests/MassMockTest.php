00f9f4d2d832e376bfd64b0c07d47c81be1a2db4
Anton Vasiliev <aionbase@gmail.com>
2022-09-17 13:44:16 +0100

    #16096 - Move MassMockTest outside unit/ directory

--- tests/MassMockTest.php
+++ tests/MassMockTest.php
@@ -11,18 +11,21 @@
 
 declare(strict_types=1);
 
-namespace Phalcon\Test\Unit;
+namespace Phalcon\Tests;
 
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
 
-class MassMockCest extends TestCase
+/**
+ * This test is executed manually.
+ * Its main purpose to check if there
+ * are no segmentation faults.
+ */
+class MassMockTest extends TestCase
 {
     public function testSegFaults(): void
     {
-        $this->markTestSkipped('Skip during CI execution.');
-
         $classes = array_filter(get_declared_classes(), function ($var) {
             return preg_match('/^Phalcon/', $var) === 1 && preg_match('/^Phalcon\\\\Test/', $var) === 0;
         });
@@ -41,7 +44,7 @@ class MassMockCest extends TestCase
             ob_flush();
 
             $mockBuilder = $this->createMock($class);
-            $this->assertInstanceOf(MockObject::class, $this->createMock($class));
+            $this->assertInstanceOf(MockObject::class, $mockBuilder);
 
             unset($reflector, $mockBuilder);
         }
