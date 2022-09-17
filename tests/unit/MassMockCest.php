27b6a8f2499d90b1b2a977061e7f9ec42d72f665
Anton Vasiliev <aionbase@gmail.com>
2022-09-17 10:41:47 +0100

    #16096 - Add unit test to mock whole Phalcon's codebase

---
+++ tests/unit/MassMockCest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Test\Unit;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+class MassMockCest extends TestCase
+{
+    public function testSegFaults(): void
+    {
+        $classes = array_filter(get_declared_classes(), function ($var) {
+            return preg_match('/^Phalcon/', $var) === 1;
+        });
+        sort($classes);
+
+        foreach ($classes as $class) {
+            $reflector = new ReflectionClass($class);
+            /**
+             * Final class could not be mocked.
+             */
+            if ($reflector->isFinal()) {
+                continue;
+            }
+
+            printf($class . PHP_EOL);
+            ob_flush();
+            $mockBuilder = $this->getMockBuilder($class)->disableOriginalConstructor();
+            $this->assertInstanceOf(MockObject::class, $mockBuilder->getMock());
+        }
+    }
+}
