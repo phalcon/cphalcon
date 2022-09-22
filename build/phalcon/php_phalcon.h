f2d57ec5725fad87a3c3fa7167cd7eea4f4ee5f7
Nikolaos Dimopoulos <nikos@niden.net>
2022-09-22 17:35:32 -0400

    [#.0.x] - optimized files

--- build/phalcon/php_phalcon.h
+++ build/phalcon/php_phalcon.h
@@ -103,10 +103,10 @@ typedef zend_function zephir_fcall_cache_entry;
 
 
 #define PHP_PHALCON_NAME        "phalcon"
-#define PHP_PHALCON_VERSION     "5.0.0RC4"
+#define PHP_PHALCON_VERSION     "5.0.0"
 #define PHP_PHALCON_EXTNAME     "phalcon"
 #define PHP_PHALCON_AUTHOR      "Phalcon Team and contributors"
-#define PHP_PHALCON_ZEPVERSION  "0.16.0-4fac47b"
+#define PHP_PHALCON_ZEPVERSION  "0.16.2-3e961ab"
 #define PHP_PHALCON_DESCRIPTION "Phalcon is a full stack PHP framework, delivered as a PHP extension, offering lower resource consumption and high performance."
 
 typedef struct _zephir_struct_db { 
