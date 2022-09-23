5dd2c95a70dd05b9223892bc3cd7d6bedaf69656
Nikolaos Dimopoulos <nikos@niden.net>
2022-09-23 09:29:19 -0400

    [#16115] - correcting tests

--- tests/unit/Encryption/Security/JWT/Token/Token/ValidateCest.php
+++ tests/unit/Encryption/Security/JWT/Token/Token/ValidateCest.php
@@ -11,11 +11,16 @@ declare(strict_types=1);
 
 namespace Phalcon\Tests\Unit\Encryption\Security\JWT\Token\Token;
 
+use Phalcon\Encryption\Security\JWT\Builder;
+use Phalcon\Encryption\Security\JWT\Signer\Hmac;
 use Phalcon\Encryption\Security\JWT\Token\Item;
 use Phalcon\Encryption\Security\JWT\Token\Signature;
 use Phalcon\Encryption\Security\JWT\Token\Token;
 use Phalcon\Encryption\Security\JWT\Validator;
 use UnitTester;
+
+use function strtotime;
+use function usleep;
 
 class ValidateCest
 {
@@ -31,20 +36,31 @@ class ValidateCest
     {
         $I->wantToTest('Encryption\Security\JWT\Token\Token - validate()');
 
-        $headers   = new Item(["typ" => "JWT"], "header-encoded");
-        $claims    = new Item(["aud" => ["valid-audience"]], "claim-encoded");
-        $signature = new Signature("signature-hash", "signature-encoded");
+        $signer     = new Hmac();
+        $builder    = new Builder($signer);
+        $expiry     = strtotime('+1 day');
+        $issued     = strtotime('now');
+        $notBefore  = strtotime('-1 day');
+        $passphrase = '&vsJBETaizP3A3VX&TPMJUqi48fJEgN7';
 
-        $token = new Token($headers, $claims, $signature);
+        $token = $builder
+            ->setAudience('my-audience')
+            ->setExpirationTime($expiry)
+            ->setIssuer('Phalcon JWT')
+            ->setIssuedAt($issued)
+            ->setId('PH-JWT')
+            ->setNotBefore($notBefore)
+            ->setSubject('Mary had a little lamb')
+            ->setPassphrase($passphrase)
+            ->getToken();
+
         $validator = new Validator($token);
 
         $errors = $token->validate($validator);
-        $I->assertCount(4, $errors);
+        $I->assertCount(2, $errors);
 
         $expected = [
-            "Validation: incorrect Id",
             "Validation: the token cannot be used yet (future)",
-            "Validation: incorrect issuer",
             "Validation: the token cannot be used yet (not before)"
         ];
         $actual = $errors;
