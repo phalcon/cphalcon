2743e7a0e3d6bd277d747beeed4e850085ba977d
Nikolaos Dimopoulos <nikos@niden.net>
2022-09-23 08:58:33 -0400

    [#16115] - adding more tests for validate()

--- tests/unit/Encryption/Security/JWT/Token/Token/ValidateCest.php
+++ tests/unit/Encryption/Security/JWT/Token/Token/ValidateCest.php
@@ -14,39 +14,40 @@ namespace Phalcon\Tests\Unit\Encryption\Security\JWT\Token\Token;
 use Phalcon\Encryption\Security\JWT\Token\Item;
 use Phalcon\Encryption\Security\JWT\Token\Signature;
 use Phalcon\Encryption\Security\JWT\Token\Token;
+use Phalcon\Encryption\Security\JWT\Validator;
 use UnitTester;
 
-/**
- * Class GetSignatureCest
- *
- * @package Phalcon\Tests\Unit\Encryption\Security\JWT\Token\Token
- */
-class GetSignatureCest
+class ValidateCest
 {
     /**
-     * Unit Tests Phalcon\Encryption\Security\JWT\Token\Token :: getSignature()
+     * Unit Tests Phalcon\Encryption\Security\JWT\Token\Token :: getToken()
      *
      * @param UnitTester $I
      *
      * @author Phalcon Team <team@phalcon.io>
-     * @since  2020-09-09
+     * @since  2022-09-23
      */
-    public function encryptionSecurityJWTTokenTokenGetSignature(UnitTester $I)
+    public function encryptionSecurityJWTTokenTokenValidate(UnitTester $I)
     {
-        $I->wantToTest('Encryption\Security\JWT\Token\Token - getSignature()');
+        $I->wantToTest('Encryption\Security\JWT\Token\Token - validate()');
 
         $headers   = new Item(["typ" => "JWT"], "header-encoded");
         $claims    = new Item(["aud" => ["valid-audience"]], "claim-encoded");
         $signature = new Signature("signature-hash", "signature-encoded");
 
         $token = new Token($headers, $claims, $signature);
+        $validator = new Validator($token);
 
-        $expected = Signature::class;
-        $actual   = $token->getSignature();
-        $I->assertInstanceOf($expected, $actual);
+        $errors = $token->validate($validator);
+        $I->assertCount(4, $errors);
 
-        $expected = "signature-encoded";
-        $actual   = $token->getSignature()->getEncoded();
+        $expected = [
+            "Validation: incorrect Id",
+            "Validation: the token cannot be used yet (future)",
+            "Validation: incorrect issuer",
+            "Validation: the token cannot be used yet (not before)"
+        ];
+        $actual = $errors;
         $I->assertSame($expected, $actual);
     }
 }
