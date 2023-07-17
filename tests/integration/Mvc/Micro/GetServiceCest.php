064ccd37bc3915d0fde6f87a358f72fe85d4da70
Nikolaos Dimopoulos <nikos@niden.net>
2023-07-17 12:27:52 -0400

    Adding test for php 8.2 deprecation warning

--- tests/integration/Mvc/Micro/GetServiceCest.php
+++ tests/integration/Mvc/Micro/GetServiceCest.php
@@ -13,59 +13,83 @@ declare(strict_types=1);
 
 namespace Phalcon\Tests\Integration\Mvc\Micro;
 
+use ErrorException;
 use IntegrationTester;
 use Phalcon\Di\Di;
+use Phalcon\Di\FactoryDefault;
 use Phalcon\Html\Escaper;
+use Phalcon\Http\Request;
 use Phalcon\Mvc\Dispatcher;
 use Phalcon\Mvc\Micro;
 use Phalcon\Mvc\Router;
+
+use function restore_error_handler;
+use function set_error_handler;
 
 class GetServiceCest
 {
     /**
      * Tests Phalcon\Mvc\Micro :: getService()
      *
-     * @author Sid Roberts <https://github.com/SidRoberts>
+     * @author Phalcon Team <team@phalcon.io>
      * @since  2019-05-22
      */
     public function mvcMicroGetService(IntegrationTester $I)
     {
         $I->wantToTest('Mvc\Micro - getService()');
 
-        $micro = new Micro();
-
-        $di = new Di();
-
-        $micro->setDi($di);
-
-
-        $escaper = new Escaper();
-
+        $micro     = new Micro();
+        $container = new Di();
+        $escaper   = new Escaper();
+        $micro->setDi($container);
         $micro->setService('escaper', $escaper);
 
-        $I->assertSame(
-            $escaper,
-            $micro->getService('escaper')
-        );
-
+        $expected = $escaper;
+        $actual   = $micro->getService('escaper');
+        $I->assertSame($expected, $actual);
 
         $dispatcher = new Dispatcher();
-
         $micro['dispatcher'] = $dispatcher;
 
-        $I->assertSame(
-            $dispatcher,
-            $micro->getService('dispatcher')
-        );
-
+        $expected = $dispatcher;
+        $actual   = $micro->getService('dispatcher');
+        $I->assertSame($expected, $actual);
 
         $router = new Router();
+        $container->set('router', $router);
 
-        $di->set('router', $router);
+        $expected = $router;
+        $actual   = $micro->getService('router');
+        $I->assertSame($expected, $actual);
+    }
+    /**
+     * Tests after binding event
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2023-07-03
+     */
+    public function mvcMicroGetServiceWithProperty(IntegrationTester $I)
+    {
+        $container = new FactoryDefault();
+        $micro     = new Micro($container);
+        $error     = '';
 
-        $I->assertSame(
-            $router,
-            $micro->getService('router')
+        set_error_handler(
+            function ($number, $message, $file, $line) {
+                throw new ErrorException($message, 0, $number, $file, $line);
+            }
         );
+
+        try {
+            $class   = Request::class;
+            $request = $micro->request;
+            $I->assertInstanceOf($class, $request);
+        } catch (ErrorException $ex) {
+            $error = $ex->getMessage();
+        }
+
+        restore_error_handler();
+
+        $I->assertEmpty($error);
     }
 }
