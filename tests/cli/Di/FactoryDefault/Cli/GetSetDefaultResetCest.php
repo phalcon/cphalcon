7effe5b966ac399be4d862ededa8c4bc9dbbb584
Nikolaos Dimopoulos <nikos@niden.net>
2023-09-29 15:59:41 -0500

    reorganizing cli tests

--- tests/cli/Di/FactoryDefault/Cli/GetSetDefaultResetCest.php
+++ tests/cli/Di/FactoryDefault/Cli/GetSetDefaultResetCest.php
@@ -16,31 +16,42 @@ namespace Phalcon\Tests\Cli\Di\FactoryDefault\Cli;
 use CliTester;
 use Phalcon\Di\FactoryDefault\Cli as Di;
 
-class GetDefaultCest
+use function spl_object_hash;
+
+class GetSetDefaultResetCest
 {
     /**
-     * Tests Phalcon\Di\FactoryDefault\Cli :: getDefault()
+     * Tests Phalcon\Di\FactoryDefault\Cli :: getDefault()/setDefault()/reset()
      *
      * @author Phalcon Team <team@phalcon.io>
      * @since  2018-11-13
      */
     public function diFactorydefaultCliGetDefault(CliTester $I)
     {
-        $I->wantToTest('Di\FactoryDefault\Cli - getDefault()');
+        $I->wantToTest('Di\FactoryDefault\Cli - getDefault()/setDefault()/reset()');
 
         // there is a DI container
         $I->assertInstanceOf(Di::class, Di::getDefault());
 
-        $di = Di::getDefault();
+        $container = Di::getDefault();
 
         // delete it
         Di::reset();
 
-        $I->assertNull(Di::getDefault());
+        // Get a new container
+        $new = Di::getDefault();
+
+        $I->assertNull($new);
 
         // set it again
-        Di::setDefault($di);
+        Di::setDefault($container);
 
-        $I->assertInstanceOf(Di::class, Di::getDefault());
+        $class  = Di::class;
+        $actual = Di::getDefault();
+        $I->assertInstanceOf($class, $actual);
+
+        $containerHash = spl_object_hash($container);
+        $currentHash   = spl_object_hash($actual);
+        $I->assertSame($containerHash, $currentHash);
     }
 }
