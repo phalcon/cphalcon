30558db39ef67e1f7ce1398930a2df269c7c80ea
Nikolaos Dimopoulos <nikos@niden.net>
2025-12-22 14:58:40 -0600

    refreshing ext

--- ext/phalcon/html/helper/doctype.zep.h
+++ ext/phalcon/html/helper/doctype.zep.h
@@ -6,21 +6,26 @@ ZEPHIR_INIT_CLASS(Phalcon_Html_Helper_Doctype);
 PHP_METHOD(Phalcon_Html_Helper_Doctype, __construct);
 PHP_METHOD(Phalcon_Html_Helper_Doctype, __invoke);
 PHP_METHOD(Phalcon_Html_Helper_Doctype, __toString);
+PHP_METHOD(Phalcon_Html_Helper_Doctype, getType);
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_html_helper_doctype___construct, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_phalcon_html_helper_doctype___invoke, 0, 0, Phalcon\\Html\\Helper\\Doctype, 0)
-	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, flag, IS_LONG, 0, "null")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, type, IS_LONG, 0, "null")
 	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, delimiter, IS_STRING, 0, "'\\n'")
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_phalcon_html_helper_doctype___tostring, 0, 0, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_phalcon_html_helper_doctype_gettype, 0, 0, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
 ZEPHIR_INIT_FUNCS(phalcon_html_helper_doctype_method_entry) {
 PHP_ME(Phalcon_Html_Helper_Doctype, __construct, arginfo_phalcon_html_helper_doctype___construct, ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
 	PHP_ME(Phalcon_Html_Helper_Doctype, __invoke, arginfo_phalcon_html_helper_doctype___invoke, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Html_Helper_Doctype, __toString, arginfo_phalcon_html_helper_doctype___tostring, ZEND_ACC_PUBLIC)
+	PHP_ME(Phalcon_Html_Helper_Doctype, getType, arginfo_phalcon_html_helper_doctype_gettype, ZEND_ACC_PUBLIC)
 	PHP_FE_END
 };
