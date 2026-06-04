92f056613bba13a5dc71cbe2e3c1d436d5eda15b
Nikolaos Dimopoulos <nikos@niden.net>
2026-06-03 22:11:51 -0500

    refreshing ext

--- ext/phalcon/events/manager.zep.h
+++ ext/phalcon/events/manager.zep.h
@@ -30,6 +30,8 @@ PHP_METHOD(Phalcon_Events_Manager, resume);
 PHP_METHOD(Phalcon_Events_Manager, setMethodExistsCacheLimit);
 PHP_METHOD(Phalcon_Events_Manager, setStopOnFalse);
 PHP_METHOD(Phalcon_Events_Manager, setStrict);
+PHP_METHOD(Phalcon_Events_Manager, afterFire);
+PHP_METHOD(Phalcon_Events_Manager, beforeFire);
 PHP_METHOD(Phalcon_Events_Manager, dispatch);
 PHP_METHOD(Phalcon_Events_Manager, insertHandlerEntry);
 PHP_METHOD(Phalcon_Events_Manager, processSubscriberEntry);
@@ -152,6 +154,21 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_phalcon_events_manager_setstrict
 	ZEND_ARG_TYPE_INFO(0, strict, _IS_BOOL, 0)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_events_manager_afterfire, 0, 0, 3)
+	ZEND_ARG_INFO(0, status)
+	ZEND_ARG_TYPE_INFO(0, eventType, IS_STRING, 0)
+	ZEND_ARG_INFO(0, source)
+	ZEND_ARG_INFO(0, data)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, cancelable, _IS_BOOL, 0, "true")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_phalcon_events_manager_beforefire, 0, 2, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, eventType, IS_STRING, 0)
+	ZEND_ARG_INFO(0, source)
+	ZEND_ARG_INFO(0, data)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, cancelable, _IS_BOOL, 0, "true")
+ZEND_END_ARG_INFO()
+
 ZEND_BEGIN_ARG_INFO_EX(arginfo_phalcon_events_manager_dispatch, 0, 0, 7)
 	ZEND_ARG_ARRAY_INFO(0, queue, 0)
 	ZEND_ARG_OBJ_INFO(0, event, Phalcon\\Events\\EventInterface, 0)
@@ -191,7 +208,7 @@ ZEPHIR_INIT_FUNCS(phalcon_events_manager_method_entry) {
 	PHP_ME(Phalcon_Events_Manager, detach, arginfo_phalcon_events_manager_detach, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Events_Manager, detachAll, arginfo_phalcon_events_manager_detachall, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Events_Manager, enablePriorities, arginfo_phalcon_events_manager_enablepriorities, ZEND_ACC_PUBLIC)
-	PHP_ME(Phalcon_Events_Manager, fire, arginfo_phalcon_events_manager_fire, ZEND_ACC_FINAL|ZEND_ACC_PUBLIC)
+	PHP_ME(Phalcon_Events_Manager, fire, arginfo_phalcon_events_manager_fire, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Events_Manager, fireAll, arginfo_phalcon_events_manager_fireall, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Events_Manager, fireQueue, arginfo_phalcon_events_manager_firequeue, ZEND_ACC_FINAL|ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Events_Manager, halt, arginfo_phalcon_events_manager_halt, ZEND_ACC_PUBLIC)
@@ -210,6 +227,8 @@ ZEPHIR_INIT_FUNCS(phalcon_events_manager_method_entry) {
 	PHP_ME(Phalcon_Events_Manager, setMethodExistsCacheLimit, arginfo_phalcon_events_manager_setmethodexistscachelimit, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Events_Manager, setStopOnFalse, arginfo_phalcon_events_manager_setstoponfalse, ZEND_ACC_PUBLIC)
 	PHP_ME(Phalcon_Events_Manager, setStrict, arginfo_phalcon_events_manager_setstrict, ZEND_ACC_PUBLIC)
+	PHP_ME(Phalcon_Events_Manager, afterFire, arginfo_phalcon_events_manager_afterfire, ZEND_ACC_PROTECTED)
+	PHP_ME(Phalcon_Events_Manager, beforeFire, arginfo_phalcon_events_manager_beforefire, ZEND_ACC_PROTECTED)
 	PHP_ME(Phalcon_Events_Manager, dispatch, arginfo_phalcon_events_manager_dispatch, ZEND_ACC_PRIVATE)
 	PHP_ME(Phalcon_Events_Manager, insertHandlerEntry, arginfo_phalcon_events_manager_inserthandlerentry, ZEND_ACC_PRIVATE)
 	PHP_ME(Phalcon_Events_Manager, processSubscriberEntry, arginfo_phalcon_events_manager_processsubscriberentry, ZEND_ACC_PRIVATE)
--- ext/php_phalcon.h
+++ ext/php_phalcon.h
@@ -11,7 +11,7 @@
 #include "kernel/globals.h"
 
 #define PHP_PHALCON_NAME        "phalcon"
-#define PHP_PHALCON_VERSION     "5.13.0"
+#define PHP_PHALCON_VERSION     "5.14.0"
 #define PHP_PHALCON_EXTNAME     "phalcon"
 #define PHP_PHALCON_AUTHOR      "Phalcon Team and contributors"
 #define PHP_PHALCON_ZEPVERSION  "0.22.0-$Id$"
